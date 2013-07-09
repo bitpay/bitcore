5c0db96180c9250e08611a664c6a3bdccd7efdb4
Jeff Garzik <jgarzik@bitpay.com>
2013-07-09 10:49:13 -0400

    Import BitcoinKey/eckey from bitcoinjs-server.

---
+++ src/common.h
@@ -0,0 +1,17 @@
+#ifndef BITCOINJS_SERVER_INCLUDE_COMMON_H_
+#define BITCOINJS_SERVER_INCLUDE_COMMON_H_
+
+#include <v8.h>
+
+#define REQ_FUN_ARG(I, VAR)                                                            \
+  if (args.Length() <= (I) || !args[I]->IsFunction())                                  \
+    return v8::ThrowException(v8::Exception::TypeError(                                \
+                              v8::String::New("Argument " #I " must be a function"))); \
+  v8::Local<v8::Function> VAR = v8::Local<v8::Function>::Cast(args[I]);
+
+static v8::Handle<v8::Value> VException(const char *msg) {
+    v8::HandleScope scope;
+    return v8::ThrowException(v8::Exception::Error(v8::String::New(msg)));
+}
+
+#endif
