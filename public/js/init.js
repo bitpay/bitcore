cf1deca94ff8ae5f2165662fdb717e05cd9df404
Gustavo Cortez <cmgustavo83@gmail.com>
2014-01-06 17:34:25 -0300

    angular js supported!

---
+++ public/js/init.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.element(document).ready(function() {
+    //Fixing facebook bug with redirect
+    if (window.location.hash === '#_=_') window.location.hash = '#!';
+
+    //Then init the app
+    angular.bootstrap(document, ['mystery']);
+});
