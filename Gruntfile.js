ade4053e010bbe6cd2395422cc6ffd8f61d9c55d
Gustavo Cortez <cmgustavo83@gmail.com>
2014-01-06 18:38:30 -0300

    mocha support!

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,6 +61,13 @@ module.exports = function(grunt) {
         logConcurrentOutput: true
       }
     },
+    mochaTest: {
+      options: {
+        reporter: 'spec',
+        require: 'server.js'
+      },
+      src: ['test/*.js']
+    },
     env: {
       test: {
         NODE_ENV: 'test'
@@ -71,6 +78,7 @@ module.exports = function(grunt) {
   //Load NPM tasks
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-env');
@@ -80,4 +88,7 @@ module.exports = function(grunt) {
 
   //Default task(s).
   grunt.registerTask('default', ['jshint', 'concurrent']);
+  
+  //Test task.
+  grunt.registerTask('test', ['env:test', 'mochaTest']);
 };
