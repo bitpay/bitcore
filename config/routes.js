513d8d88c95faa6a34fea3d54b2de4a786c14bf8
Mario Colque <mario.colque@globant.com>
2014-01-08 09:28:24 -0300

    added new route /blocks

--- config/routes.js
+++ config/routes.js
@@ -8,13 +8,14 @@ module.exports = function(app) {
 
   //Block routes
   var blocks = require('../app/controllers/blocks');
+  app.get('/blocks', blocks.list);
   app.get('/block/:blockHash', blocks.show);
   app.param('blockHash', blocks.block);
   app.get('/last_blocks', blocks.last_blocks);
 
   var transactions = require('../app/controllers/transactions');
   app.get('/tx/:txid', transactions.show);
- 
+
   app.param('txid', transactions.transaction);
-  
+
 };
