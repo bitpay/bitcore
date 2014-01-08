1ec2984bab77a4247b91fee1ff6baa667d76e850
Gustavo Cortez <cmgustavo83@gmail.com>
2014-01-08 09:32:38 -0300

    Frontend: Blocks controller for angularjs

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-angular.module('mystery', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mystery.system', 'mystery.index']);
+angular.module('mystery', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mystery.system', 'mystery.index', 'mystery.blocks']);
 
 angular.module('mystery.system', []);
 angular.module('mystery.index', []);
+angular.module('mystery.blocks', []);
