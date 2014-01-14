c3a6c0387cfb15e019bd51f116a22591d8467f4e
Mario Colque <colquemario@gmail.com>
2014-01-14 14:48:25 -0300

    added a interceptor in ther services to handle the server request error

--- public/js/services/blocks.js
+++ public/js/services/blocks.js
@@ -3,6 +3,20 @@
 angular.module('mystery.blocks').factory('Block', ['$resource', function($resource) {
   return $resource('/api/block/:blockHash', {
     blockHash: '@blockHash'
+  }, {
+    get: {
+      method: 'GET',
+      interceptor: {
+        response: function (res) {
+          return res.data;
+        },
+        responseError: function (res) {
+          if (res.status === 404) {
+            return res;
+          }
+        }
+      }
+    }
   });
 }]);
 
