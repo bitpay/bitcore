1c6b506dbb92f60a37514651f7372755b827632c
Matias Alejo Garcia <ematiu@gmail.com>
2014-01-20 16:53:54 -0300

    resolve merge conflits

--- public/js/controllers/transactions.js
+++ public/js/controllers/transactions.js
@@ -22,12 +22,7 @@ angular.module('insight.transactions').controller('transactionsController',
       txId: txid
     }, function(tx) {
       $scope.tx = tx;
-<<<<<<< HEAD
-      console.log(console.log(tx));
       $scope.txs.push(tx);
-    }, function() {
-      $rootScope.flashMessage = 'Transaction Not Found';
-=======
     }, function(e) {
       if (e.status === 400) {
         $rootScope.flashMessage = 'Invalid Transaction ID: ' + $routeParams.txId;
@@ -38,7 +33,6 @@ angular.module('insight.transactions').controller('transactionsController',
       else {
         $rootScope.flashMessage = 'Transaction Not Found';
       }
->>>>>>> matiu/feature/11showsyncstatus
       $location.path('/');
     });
   };
