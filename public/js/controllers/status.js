7ac3b5b1aa5afdedec49900bbd0e1004b8d06ee6
Matias Alejo Garcia <ematiu@gmail.com>
2014-01-21 19:40:41 -0300

    better header for sync

--- public/js/controllers/status.js
+++ public/js/controllers/status.js
@@ -23,19 +23,6 @@ function($scope, $routeParams, $location, $rootScope, Global, Status, Sync, get_
   };
 
   var on_sync_update = function(sync) {
-
-    if (!sync.error) {
-      if (sync.blocksToSync > sync.syncedBlocks) {
-        var p = parseInt(100*(sync.syncedBlocks) / sync.blocksToSync);
-        var delta = sync.blocksToSync - sync.syncedBlocks;
-        sync.message = 'Sync ' + p + '% ['+delta+' blocks remaining]';
-        sync.style = 'warn';
-      } else {
-        sync.message = 'On sync';
-        sync.style = 'success';
-      }
-      sync.tooltip = 'Synced blocks: '+sync.syncedBlocks;
-    }
     $scope.sync = sync;
   };
 
@@ -52,7 +39,6 @@ function($scope, $routeParams, $location, $rootScope, Global, Status, Sync, get_
   var socket = get_socket($scope);
   socket.emit('subscribe', 'sync');
   socket.on('status', function(sync) {
-
 console.log('[status.js.55::] sync status update received!');
     on_sync_update(sync);
   });
