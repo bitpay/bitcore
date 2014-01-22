13c9d0075a4c0c6b2397bcb52a97b4f8e3be7c4f
Gustavo Cortez <cmgustavo83@gmail.com>
2014-01-21 23:15:44 -0300

    fixed lasts conflicts in block/list

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -3,8 +3,24 @@
 var TRANSACTION_DISPLAYED = 5;
 var BLOCKS_DISPLAYED = 5;
 angular.module('insight.system').controller('IndexController',
-  function($scope, $rootScope, Global, get_socket, Blocks, Transactions) {
+  function($scope, $rootScope, Global, get_socket, Blocks, Block, Transactions, Transaction) {
   $scope.global = Global;
+
+  var getTransaction = function(txid) {
+    Transaction.get({
+      txId: txid
+    }, function(res) {
+      $scope.txs.unshift(res);
+    });
+  };
+
+  var getBlock = function(hash) {
+    Block.get({
+      blockHash: hash
+    }, function(res) {
+      $scope.blocks.unshift(res);
+    });
+  };
 
   var socket = get_socket($scope);
   socket.emit('subscribe', 'inv');
@@ -13,19 +29,21 @@ angular.module('insight.system').controller('IndexController',
   $scope.flashMessage = $rootScope.flashMessage || null;
 
   socket.on('tx', function(tx) {
+    var txStr = tx.txid.toString();
     console.log('Transaction received! ' + JSON.stringify(tx));
     if ($scope.txs.length === TRANSACTION_DISPLAYED) {
       $scope.txs.pop();
     }
-    $scope.txs.unshift(tx);
+    getTransaction(txStr);
   });
 
   socket.on('block', function(block) {
+    var blockHash = block.hash.toString();
     console.log('Block received! ' + JSON.stringify(block));
-      if ($scope.blocks.length === BLOCKS_DISPLAYED) {
-        $scope.blocks.pop();
-      }
-    $scope.blocks.unshift(block);
+    if ($scope.blocks.length === BLOCKS_DISPLAYED) {
+      $scope.blocks.pop();
+    }
+    getBlock(blockHash);
   });
 
   $scope.human_since = function(time) {
@@ -38,6 +56,7 @@ angular.module('insight.system').controller('IndexController',
       limit: BLOCKS_DISPLAYED
     }, function(res) {
       $scope.blocks = res.blocks;
+      $scope.blocksLength = res.lenght;
     });
 
     Transactions.get({
