0e29072cf2d871819a289c0a9a61d1324d7be4f1
Manuel Araoz <manuelaraoz@gmail.com>
2014-01-30 17:38:51 -0500

    migrating base58

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 //var Enc = require('./util/EncodedData').class();
-//require('base58-native');
 
 var bignum = require('bignum');
-
+bignum.config({EXPONENTIAL_AT: 999999999});
+console.log(bignum('98320').toNumber());
 var b = bignum('782910138827292261791972728324982')
           .sub('182373273283402171237474774728373')
           .div(8);
-console.log(b);
+console.log(b.toString());
 
+/*
+var base58 = require('base58-native');
+var m = 'mqqa8xSMVDyf9QxihGnPtap6Mh6qemUkcu';
+var d = base58.decode(m);
+var m2 = base58.encode(d);
+console.log(m);
+console.log(d);
+/*
+var m = base58.encode(base58.decode('mqqa8xSMVDyf9QxihGnPtap6Mh6qemUkcu'));
+console.log(typeof(m));
+
+var base58Check = require('base58-native').base58Check;
+console.log(base58Check.encode(base58Check.decode('mqqa8xSMVDyf9QxihGnPtap6Mh6qemUkcu')));
+*/
