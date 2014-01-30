cb762eb284c49f173749487ab9ea96ff6bcedbc9
Manuel Araoz <manuelaraoz@gmail.com>
2014-01-30 18:09:54 -0500

    making base58 work

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 //var Enc = require('./util/EncodedData').class();
 
 var bignum = require('bignum');
-bignum.config({EXPONENTIAL_AT: 999999999});
-console.log(bignum('98320').toNumber());
+bignum.config({EXPONENTIAL_AT: 9999999});
+console.log(bignum('9832087987979879879879879879879879879879879879').toString());
 var b = bignum('782910138827292261791972728324982')
           .sub('182373273283402171237474774728373')
           .div(8);
 console.log(b.toString());
 
-/*
+
 var base58 = require('base58-native');
 var m = 'mqqa8xSMVDyf9QxihGnPtap6Mh6qemUkcu';
 var d = base58.decode(m);
-var m2 = base58.encode(d);
+//var m2 = base58.encode(d);
 console.log(m);
 console.log(d);
 /*
