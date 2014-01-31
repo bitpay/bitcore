9a6c89e89dfcc31516ee8a8cbc406721504d52cc
Mario Colque <colquemario@gmail.com>
2014-01-31 14:20:56 -0300

    improve code without config files

--- config/config.js
+++ config/config.js
@@ -1,9 +1,28 @@
 'use strict';
 
-var _ = require('lodash');
+/**
+ * Module dependencies.
+ */
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..');
 
-// Load app configuration
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-module.exports = _.extend(
-  require(__dirname + '/../config/env/all.js'),
-  require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.js') || {});
+module.exports = {
+  root: rootPath,
+  appName: 'Insight',
+  port: process.env.PORT || 3000,
+  db: 'mongodb://localhost/insight-test',
+  bitcoind: {
+    protocol:  process.env.BITCOIND_PROTO || 'http',
+    user: process.env.BITCOIND_USER || 'user',
+    pass: process.env.BITCOIND_PASS || 'pass',
+    host: process.env.BITCOIND_HOST || '127.0.0.1',
+    port: process.env.BITCOIND_PORT || '18332',
+    p2pPort: process.env.BITCOIND_P2P_PORT || '18333',
+
+    // DO NOT CHANGE THIS!
+    disableAgent: true
+  },
+  network: process.env.INSIGHT_NETWORK || 'testnet',
+  disableP2pSync: false,
+  disableHistoricSync: false,
+};
