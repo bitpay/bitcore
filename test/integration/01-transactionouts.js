339f6f79fc8e151284d71fdf3a3dd78d70d81258
Matias Alejo Garcia <ematiu@gmail.com>
2014-02-04 03:22:58 -0300

    mocha 01-txout passing

--- test/integration/01-transactionouts.js
+++ test/integration/01-transactionouts.js
@@ -5,60 +5,57 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 
 
-var mongoose      = require('mongoose'),
+var 
   assert          = require('assert'),
   fs              = require('fs'),
   util            = require('util'),
   config          = require('../../config/config'),
-  TransactionOut = require('../../app/models/TransactionOut');
+  TransactionDb = require('../../lib/TransactionDb').class();
 
-var txItemsValid = JSON.parse(fs.readFileSync('test/model/txitems.json'));
-
-mongoose.connection.on('error', function(err) { console.log(err); });
+var txItemsValid = JSON.parse(fs.readFileSync('test/integration/txitems.json'));
 
 describe('TransactionOut', function(){
 
-  before(function(done) {
-    mongoose.connect(config.db);
-    done();
-  });
-
-  after(function(done) {
-    mongoose.connection.close();
-    done();
-  });
+  var tdb = new TransactionDb();
 
   txItemsValid.forEach( function(v) {
     if (v.disabled) return;
-    it('test a exploding tx ' + v.txid, function(done) {
+    it('test a processing tx ' + v.txid, function(done) {
+      this.timeout(60000);
 
       // Remove first
-      TransactionOut.removeFromTxId(v.txid, function(err) {
-        TransactionOut._explodeTransactionOuts(v.txid, function(err, tx) {
-          if (err) done(err);
+      tdb.removeFromTxId(v.txid, v.toRm, function() {
 
-          TransactionOut
-            .fromTxId( v.txid, function(err, readItems) {
+        tdb.fromTxId( v.txid, function(err, readItems) {
+          assert.equal(readItems.length,0);
 
-            var unmatch={};
+          var unmatch=[];
+          tdb.createFromArray([v.txid], null, function(err) {
+            if (err) return done(err);
 
-            v.items.forEach(function(validItem){ 
-              unmatch[validItem.addr] =1;
+            tdb.fromTxId( v.txid, function(err, readItems) {
+
+              v.items.forEach(function(validItem){ 
+                unmatch[validItem.addr] =1;
+              });
+              assert.equal(readItems.length,v.items.length);
+
+              v.items.forEach(function(validItem){ 
+                var readItem = readItems.shift();
+
+                assert.equal(readItem.addr,validItem.addr);
+                assert.equal(readItem.value_sat,validItem.value_sat);
+                assert.equal(readItem.index,validItem.index);
+                assert.equal(readItem.spendIndex, null);
+                assert.equal(readItem.spendTxIdBuf, null);
+                delete unmatch[validItem.addr];
+              });
+
+              var valid = util.inspect(v.items, { depth: null });
+              assert(!Object.keys(unmatch).length,'\n\tUnmatchs:' + Object.keys(unmatch) + "\n\n" +valid + '\nvs.\n' + readItems);
+              return done();
+
             });
-            v.items.forEach(function(validItem){ 
-              var readItem = readItems.shift();
-              assert.equal(readItem.addr,validItem.addr);
-              assert.equal(readItem.value_sat,validItem.value_sat);
-              assert.equal(readItem.index,validItem.index);
-              assert.equal(readItem.spendIndex, null);
-              assert.equal(readItem.spendTxIdBuf, null);
-              delete unmatch[validItem.addr];
-            });
-
-            var valid = util.inspect(v.items, { depth: null });
-            assert(!Object.keys(unmatch).length,'\n\tUnmatchs:' + Object.keys(unmatch) + "\n\n" +valid + '\nvs.\n' + readItems);
-            done();
-
           });
         });
       });
--- test/integration/block.js
+++ test/integration/block.js
@@ -7,68 +7,38 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var TESTING_BLOCK = '000000000185678d3d7ecc9962c96418174431f93fe20bf216d5565272423f74';
 
 var
-  mongoose= require('mongoose'),
   assert  = require('assert'),
   config      = require('../../config/config'),
-  Block       = require('../../app/models/Block');
+  BlockDb     = require('../../lib/BlockDb').class();
 
 
-mongoose.connection.on('error', function(err) { console.log(err); });
+describe('BlockDb fromHashWithInfo', function(){
 
-describe('Block fromHashWithInfo', function(){
-
-  before(function(done) {
-    mongoose.connect(config.db);
-    done();
-  });
-
-  after(function(done) {
-    mongoose.connection.close();
-    done();
-  });
-
-
-  it('should poll block\'s info from mongoose', function(done) {
-    Block.fromHashWithInfo(TESTING_BLOCK, function(err, b2) {
-        if (err) done(err);
-
-
-        var h = new Buffer(TESTING_BLOCK,'hex');
-        assert(b2.hashStr === TESTING_BLOCK);
-        assert.equal(b2.hashStr, TESTING_BLOCK);
-        done();
-      });
-  });
-
+  var bdb = new BlockDb();
   it('should poll block\'s info from bitcoind', function(done) {
-    Block.fromHashWithInfo(TESTING_BLOCK, function(err, b2) {
+    bdb.fromHashWithInfo(TESTING_BLOCK, function(err, b2) {
         if (err) done(err);
+        assert.equal(b2.hash, TESTING_BLOCK);
         assert.equal(b2.info.hash, TESTING_BLOCK);
         assert.equal(b2.info.chainwork, '000000000000000000000000000000000000000000000000001b6dc969ffe847');
         done();
     });
   });
-
-
-  it('hash Virtuals SET', function(done) {
-    var b = new Block();
-    b.hashStr = 'a1a2';
-    assert.equal(b.hash.toString('hex'),'a1a2');
-    b.nextBlockHashStr = 'a1a3';
-    assert.equal(b.nextBlockHash.toString('hex'),'a1a3');
-    done();
+  it('return true in has', function(done) {
+    bdb.has(TESTING_BLOCK, function(err, has) {
+      assert.equal(has, true);
+console.log('[block.js.29:has:]',has); //TODO
+      done();
+    });
+  });
+  it('return false in has', function(done) {
+    bdb.has('111', function(err, has) {
+      assert.equal(has, false);
+      done();
+    });
   });
 
 
-  it('hash Virtuals GET', function(done) {
-    var b = new Block();
-    b.hash = new Buffer('a1a2','hex');
-    assert.equal(b.hashStr,'a1a2');
 
-
-    b.nextBlockHash = new Buffer('b2b1','hex');
-    assert.equal(b.nextBlockHashStr,'b2b1');
-    done();
-  });
 });
 
