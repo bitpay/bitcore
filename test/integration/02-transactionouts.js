65aad6d0ad4e8799e94d93cc846350955d3fbc48
Matias Alejo Garcia <ematiu@gmail.com>
2014-02-04 03:41:32 -0300

    02 transaction tests passing

--- test/integration/02-transactionouts.js
+++ test/integration/02-transactionouts.js
@@ -5,31 +5,27 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 
 
-var mongoose      = require('mongoose'),
+var 
   assert          = require('assert'),
   fs              = require('fs'),
   util            = require('util'),
   async           = require('async'),
   config          = require('../../config/config'),
-  TransactionOut = require('../../app/models/TransactionOut');
+  TransactionDb = require('../../lib/TransactionDb').class();
 
-var spentValid   = JSON.parse(fs.readFileSync('test/model/spent.json'));
+var spentValid   = JSON.parse(fs.readFileSync('test/integration/spent.json'));
 
-mongoose.connection.on('error', function(err) { console.log(err); });
-
-
-
-describe('TransactionOut Expenses', function(){
+describe('TransactionDb Expenses', function(){
+  var tdb = new TransactionDb();
 
   before(function(done) {
-    mongoose.connect(config.db);
 
     // lets spend!
     async.each(Object.keys(spentValid), 
       function(txid,c_out) {
         async.each(spentValid[txid], 
                   function(i,c_in) {
-                    TransactionOut._explodeTransactionOuts(i.txid, function(err) {
+                    tdb.createFromArray([i.txid], null, function(err) {
                       return c_in();
                     });
                   }, 
@@ -40,24 +36,18 @@ describe('TransactionOut Expenses', function(){
         );
       },
       function(err) {
-        console.log('[transactionouts.js.88]'); //TODO
         return done();
       }
     );
-  });
-
-  after(function(done) {
-    mongoose.connection.close();
-    done();
   });
 
   Object.keys(spentValid).forEach( function(txid) {
     it('test result of spending tx ' + txid, function(done) {
       var s = spentValid[txid];
       var c=0;
-      TransactionOut.fromTxId( txid, function(err, readItems) {
+      tdb.fromTxId( txid, function(err, readItems) {
         s.forEach( function(v) {
-          assert.equal(readItems[c].spendTxIdBuf.toString('hex'),v.txid);
+          assert.equal(readItems[c].spendTxId,v.txid);
           assert.equal(readItems[c].spendIndex,v.n);
           c++;
         });
