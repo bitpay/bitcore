71b27aede00676d72129dceb6d2b865170e48556
Matias Alejo Garcia <ematiu@gmail.com>
2014-02-05 02:48:54 -0300

    better remove in txDB

--- test/integration/01-transactionouts.js
+++ test/integration/01-transactionouts.js
@@ -11,18 +11,18 @@ var assert          = require('assert'),
   TransactionDb = require('../../lib/TransactionDb').class();
 
 var txItemsValid = JSON.parse(fs.readFileSync('test/integration/txitems.json'));
-var tdb;
+var txDb;
 
 describe('TransactionDb fromIdWithInfo', function(){
 
   before(function(c) {
-    tdb = new TransactionDb();
+    txDb = new TransactionDb();
     return c();
   });
 
   var txid = '7e621eeb02874ab039a8566fd36f4591e65eca65313875221842c53de6907d6c';
   it('tx info ' + txid, function(done) {
-    tdb.fromIdWithInfo(txid, function(err, tx) {
+    txDb.fromIdWithInfo(txid, function(err, tx) {
 
       if (err) done(err);
       assert.equal(tx.txid, txid);
@@ -38,7 +38,7 @@ describe('TransactionDb fromIdWithInfo', function(){
 
   it('tx info', function(done) {
     var txid = '21798ddc9664ac0ef618f52b151dda82dafaf2e26d2bbef6cdaf55a6957ca237';
-    tdb.fromIdWithInfo(txid, function(err, tx) {
+    txDb.fromIdWithInfo(txid, function(err, tx) {
       if (err) done(err);
       assert.equal(tx.txid, txid);
       assert(!tx.info.isCoinBase);
@@ -48,7 +48,7 @@ describe('TransactionDb fromIdWithInfo', function(){
 
   it('should pool tx\'s info from bitcoind', function(done) {
     var txid = '21798ddc9664ac0ef618f52b151dda82dafaf2e26d2bbef6cdaf55a6957ca237';
-    tdb.fromIdWithInfo(txid, function(err, tx) {
+    txDb.fromIdWithInfo(txid, function(err, tx) {
       if (err) done(err);
       assert.equal(tx.info.txid, txid);
       assert.equal(tx.info.blockhash, '000000000185678d3d7ecc9962c96418174431f93fe20bf216d5565272423f74');
@@ -62,7 +62,7 @@ describe('TransactionDb fromIdWithInfo', function(){
 
   var txid1 = '2a104bab1782e9b6445583296d4a0ecc8af304e4769ceb64b890e8219c562399';
   it('test a coinbase TX ' + txid1, function(done) {
-    tdb.fromIdWithInfo(txid1, function(err, tx) {
+    txDb.fromIdWithInfo(txid1, function(err, tx) {
       if (err) done(err);
       assert(tx.info.isCoinBase);
       assert.equal(tx.info.txid, txid1);
@@ -72,7 +72,7 @@ describe('TransactionDb fromIdWithInfo', function(){
   });
   var txid22 = '666';
   it('test invalid TX ' + txid22, function(done) {
-    tdb.fromIdWithInfo(txid22, function(err, tx) {
+    txDb.fromIdWithInfo(txid22, function(err, tx) {
       if (err && err.message.match(/must.be.hexadecimal/))  {
           return done();
       }
@@ -85,7 +85,7 @@ describe('TransactionDb fromIdWithInfo', function(){
   var txid23 = '21798ddc9664ac0ef618f52b151dda82dafaf2e26d2bbef6cdaf55a6957ca227';
   it('test unexisting TX ' + txid23, function(done) {
 
-    tdb.fromIdWithInfo(txid23, function(err, tx) {
+    txDb.fromIdWithInfo(txid23, function(err, tx) {
       assert(!err);
       assert(!tx);
       return done();
@@ -96,7 +96,7 @@ describe('TransactionDb fromIdWithInfo', function(){
 
   var txid2 = '64496d005faee77ac5a18866f50af6b8dd1f60107d6795df34c402747af98608';
   it('create TX on the fly ' + txid2, function(done) {
-    tdb.fromIdWithInfo(txid2, function(err, tx) {
+    txDb.fromIdWithInfo(txid2, function(err, tx) {
       if (err) return done(err);
       assert.equal(tx.info.txid, txid2);
       done();
@@ -105,7 +105,7 @@ describe('TransactionDb fromIdWithInfo', function(){
 
   txid2 = '64496d005faee77ac5a18866f50af6b8dd1f60107d6795df34c402747af98608';
   it('test a broken TX ' + txid2, function(done) {
-    tdb.fromIdWithInfo(txid2, function(err, tx) {
+    txDb.fromIdWithInfo(txid2, function(err, tx) {
       if (err) return done(err);
       assert.equal(tx.info.txid, txid2);
       assert.equal(tx.info.vin[0].addr, 'n1JagbRWBDi6VMvG7HfZmXX74dB9eiHJzU');
@@ -113,11 +113,8 @@ describe('TransactionDb fromIdWithInfo', function(){
     });
   });
 });
-/*
 
 describe('TransactionDb Outs', function(){
-
-  var tdb = new TransactionDb();
 
   txItemsValid.forEach( function(v) {
     if (v.disabled) return;
@@ -125,16 +122,16 @@ describe('TransactionDb Outs', function(){
       this.timeout(60000);
 
       // Remove first
-      tdb.removeFromTxId(v.txid, v.toRm, function() {
+      txDb.removeFromTxId(v.txid, function() {
 
-        tdb.fromTxId( v.txid, function(err, readItems) {
+        txDb.fromTxId( v.txid, function(err, readItems) {
           assert.equal(readItems.length,0);
 
           var unmatch=[];
-          tdb.createFromArray([v.txid], null, function(err) {
+          txDb.createFromArray([v.txid], null, function(err) {
             if (err) return done(err);
 
-            tdb.fromTxId( v.txid, function(err, readItems) {
+            txDb.fromTxId( v.txid, function(err, readItems) {
 
               v.items.forEach(function(validItem){ 
                 unmatch[validItem.addr] =1;
@@ -164,5 +161,4 @@ describe('TransactionDb Outs', function(){
   });
 });
 
-*/
 
