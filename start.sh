b0ed88eddd87d48341818a6d87b6f0030dfcc8b2
Gustavo Cortez <cmgustavo83@gmail.com>
2014-02-13 11:47:49 -0300

    delete start.sh (old script for start server. Documented on readme)

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-NODE_ENV='production' INSIGHT_NETWORK='livenet' ./node_modules/pm2/bin/pm2 -f start insight.js --name insight-livenet &
-sleep 10;
-NODE_ENV='production' INSIGHT_NETWORK='testnet' ./node_modules/pm2/bin/pm2 -f start insight.js --name insight-testnet &
-
