0f393cb98964453d3d4886d67c0d3d243e3a5e3f
Manuel Araoz <manuelaraoz@gmail.com>
2014-02-19 16:54:41 -0300

    ECDSA key browser work

--- test/test.Key.js
+++ test/test.Key.js
@@ -5,10 +5,14 @@ var bitcore = require('../bitcore');
 
 var should = chai.should();
 
-var Key = bitcore.Key;
-
-describe.skip('Key', function() {
+var KeyModule = bitcore.KeyModule;
+var Key;
+describe('Key', function() {
   it('should initialze the main object', function() {
+    should.exist(KeyModule);
+  });
+  it('should be able to create class', function() {
+    Key = KeyModule.Key;
     should.exist(Key);
   });
   it('should be able to create instance', function() {
