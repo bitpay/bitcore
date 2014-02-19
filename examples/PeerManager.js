16426d84bd123c3086dc9dfef70fed7fb1193713
Matias Alejo Garcia <ematiu@gmail.com>
2014-02-19 16:48:52 -0300

    fix peermanager example

--- examples/PeerManager.js
+++ examples/PeerManager.js
@@ -1,3 +1,4 @@
+'use strict';
 
 // Replace path '..' to 'bitcore' if you are using this example
 // in a different project
@@ -7,18 +8,26 @@ var PeerManager = require('../PeerManager').createClass({
   network: networks.testnet
 });
 
+var util= require('util');
+
 
 var handleBlock = function(b) {
-  console.log('block received:', b);
+  console.log('block received:', util.inspect(b.message,{depth:null}));
 };
 
 var handleTx = function(b) {
-  console.log('block tx:', b);
+  console.log('block tx:',  util.inspect(b.message,{depth:null}));
 };
+
+var handleInv = function(b) {
+  console.log('block inv:',  util.inspect(b.message,{depth:null}));
+};
+
 
 var peerman = new PeerManager();
 peerman.addPeer( new Peer('127.0.0.1',18333) );
-peerman.on('connect', function(conn) {
+peerman.on('connection', function(conn) {
+  conn.on('inv',  handleInv);
   conn.on('block', handleBlock);
   conn.on('tx', handleTx);
 });
