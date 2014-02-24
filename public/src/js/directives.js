151d6b7d75fecf365efefdd93feebd2b42a26013
Gustavo Cortez <cmgustavo83@gmail.com>
2014-02-24 19:42:17 -0300

    added currency selector to navbar. secondary navbar on blockpage when scroll

--- public/src/js/directives.js
+++ public/src/js/directives.js
@@ -3,6 +3,18 @@
 var ZeroClipboard = window.ZeroClipboard;
 
 angular.module('insight')
+  .directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
+      angular.element($window).bind("scroll", function() {
+        if (this.pageYOffset >= 200) {
+          scope.secondaryNavbar = true;
+        } else {
+          scope.secondaryNavbar = false;
+        }
+        scope.$apply();
+      });
+    };
+  })
   .directive('whenScrolled', function($window) {
     return {
       restric: 'A',
