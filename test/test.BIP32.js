47fe12ea19e1d051095158e62b50b9a4ed7d466b
Ryan X. Charles <ryan@bitpay.com>
2014-03-22 14:25:11 -0700

    all vector 2 tests work

--- test/test.BIP32.js
+++ test/test.BIP32.js
@@ -21,6 +21,19 @@ describe('BIP32', function() {
   var vector1_m0h12h2_private = 'xprvA2JDeKCSNNZky6uBCviVfJSKyQ1mDYahRjijr5idH2WwLsEd4Hsb2Tyh8RfQMuPh7f7RtyzTtdrbdqqsunu5Mm3wDvUAKRHSC34sJ7in334';
   var vector1_m0h12h21000000000_public = 'xpub6H1LXWLaKsWFhvm6RVpEL9P4KfRZSW7abD2ttkWP3SSQvnyA8FSVqNTEcYFgJS2UaFcxupHiYkro49S8yGasTvXEYBVPamhGW6cFJodrTHy';
   var vector1_m0h12h21000000000_private = 'xprvA41z7zogVVwxVSgdKUHDy1SKmdb533PjDz7J6N6mV6uS3ze1ai8FHa8kmHScGpWmj4WggLyQjgPie1rFSruoUihUZREPSL39UNdE3BBDu76';
+  var vector2_master = 'fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542';
+  var vector2_m_public = 'xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB';
+  var vector2_m_private = 'xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U';
+  var vector2_m0_public = 'xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH';
+  var vector2_m0_private = 'xprv9vHkqa6EV4sPZHYqZznhT2NPtPCjKuDKGY38FBWLvgaDx45zo9WQRUT3dKYnjwih2yJD9mkrocEZXo1ex8G81dwSM1fwqWpWkeS3v86pgKt';
+  var vector2_m02147483647h_public = 'xpub6ASAVgeehLbnwdqV6UKMHVzgqAG8Gr6riv3Fxxpj8ksbH9ebxaEyBLZ85ySDhKiLDBrQSARLq1uNRts8RuJiHjaDMBU4Zn9h8LZNnBC5y4a';
+  var vector2_m02147483647h_private = 'xprv9wSp6B7kry3Vj9m1zSnLvN3xH8RdsPP1Mh7fAaR7aRLcQMKTR2vidYEeEg2mUCTAwCd6vnxVrcjfy2kRgVsFawNzmjuHc2YmYRmagcEPdU9';
+  var vector2_m02147483647h1_public = 'xpub6DF8uhdarytz3FWdA8TvFSvvAh8dP3283MY7p2V4SeE2wyWmG5mg5EwVvmdMVCQcoNJxGoWaU9DCWh89LojfZ537wTfunKau47EL2dhHKon';
+  var vector2_m02147483647h1_private = 'xprv9zFnWC6h2cLgpmSA46vutJzBcfJ8yaJGg8cX1e5StJh45BBciYTRXSd25UEPVuesF9yog62tGAQtHjXajPPdbRCHuWS6T8XA2ECKADdw4Ef';
+  var vector2_m02147483647h12147483646h_public = 'xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL';
+  var vector2_m02147483647h12147483646h_private = 'xprvA1RpRA33e1JQ7ifknakTFpgNXPmW2YvmhqLQYMmrj4xJXXWYpDPS3xz7iAxn8L39njGVyuoseXzU6rcxFLJ8HFsTjSyQbLYnMpCqE2VbFWc';
+  var vector2_m02147483647h12147483646h2_public = 'xpub6FnCn6nSzZAw5Tw7cgR9bi15UV96gLZhjDstkXXxvCLsUXBGXPdSnLFbdpq8p9HmGsApME5hQTZ3emM2rnY5agb9rXpVGyy3bdW6EEgAtqt';
+  var vector2_m02147483647h12147483646h2_private = 'xprvA2nrNbFZABcdryreWet9Ea4LvTJcGsqrMzxHx98MMrotbir7yrKCEXw7nadnHM8Dq38EGfSh6dqA9QWTyefMLEcBYJUuekgW4BYPJcr9E7j';
 
 
   it('should initialize the class', function() {
@@ -37,7 +50,7 @@ describe('BIP32', function() {
     should.exist(bip32);
   });
 
-  it('should get the extended public key from the extended private key', function() {
+  it('should get the extended public key from the extended private key for test vector 1', function() {
     var bip32 = new BIP32(vector1_m_private);
     bip32.extended_public_key_string().should.equal(vector1_m_public);
   });
@@ -110,6 +123,91 @@ describe('BIP32', function() {
     var child = bip32.derive("m/0'/1/2'/2/1000000000");
     should.exist(child);
     child.extended_public_key_string().should.equal(vector1_m0h12h21000000000_public);
+  });
+
+  it('should initialize test vector 2 from the extended public key', function() {
+    var bip32 = new BIP32(vector2_m_public);
+    should.exist(bip32);
+  });
+
+  it('should initialize test vector 2 from the extended private key', function() {
+    var bip32 = new BIP32(vector2_m_private);
+    should.exist(bip32);
+  });
+
+  it('should get the extended public key from the extended private key for test vector 2', function() {
+    var bip32 = new BIP32(vector2_m_private);
+    bip32.extended_public_key_string().should.equal(vector2_m_public);
+  });
+
+  it("should get m/0 ext. private key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0");
+    should.exist(child);
+    child.extended_private_key_string().should.equal(vector2_m0_private);
+  });
+
+  it("should get m/0 ext. public key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0");
+    should.exist(child);
+    child.extended_public_key_string().should.equal(vector2_m0_public);
+  });
+
+  it("should get m/0/2147483647h ext. private key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0/2147483647'");
+    should.exist(child);
+    child.extended_private_key_string().should.equal(vector2_m02147483647h_private);
+  });
+
+  it("should get m/0/2147483647h ext. public key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0/2147483647'");
+    should.exist(child);
+    child.extended_public_key_string().should.equal(vector2_m02147483647h_public);
+  });
+
+  it("should get m/0/2147483647h/1 ext. private key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0/2147483647'/1");
+    should.exist(child);
+    child.extended_private_key_string().should.equal(vector2_m02147483647h1_private);
+  });
+
+  it("should get m/0/2147483647h/1 ext. public key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0/2147483647'/1");
+    should.exist(child);
+    child.extended_public_key_string().should.equal(vector2_m02147483647h1_public);
+  });
+
+  it("should get m/0/2147483647h/1/2147483646h ext. private key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0/2147483647'/1/2147483646'");
+    should.exist(child);
+    child.extended_private_key_string().should.equal(vector2_m02147483647h12147483646h_private);
+  });
+
+  it("should get m/0/2147483647h/1/2147483646h ext. public key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0/2147483647'/1/2147483646'");
+    should.exist(child);
+    child.extended_public_key_string().should.equal(vector2_m02147483647h12147483646h_public);
+  });
+
+  it("should get m/0/2147483647h/1/2147483646h/2 ext. private key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0/2147483647'/1/2147483646'/2");
+    should.exist(child);
+    child.extended_private_key_string().should.equal(vector2_m02147483647h12147483646h2_private);
+  });
+
+  it("should get m/0/2147483647h/1/2147483646h/2 ext. public key from test vector 2", function() {
+    var bip32 = new BIP32(vector2_m_private);
+    var child = bip32.derive("m/0/2147483647'/1/2147483646'/2");
+    should.exist(child);
+    child.extended_public_key_string().should.equal(vector2_m02147483647h12147483646h2_public);
   });
 
 });
