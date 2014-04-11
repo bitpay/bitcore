a5ba4577873ac86623eec6432f92e666122ca65a
Gordon Hall <gordon@gordonwritescode.com>
2014-04-11 14:05:53 -0400

    update example to discover a peer instead of hardcoding arbitrary IP

--- examples/ConnectionTor.js
+++ examples/ConnectionTor.js
@@ -1,32 +1,27 @@
 var Peer       = require('../Peer');
 var Connection = require('../Connection');
+var dns        = require('dns');
 
-// create a peer instance from a know peer
-// (later we can use built-in peer discovery)
-// to get a peer to connect to you can run:
-//
-//  ~# dig dnsseed.bluematt.me
-//
-// (or use a different dns seed)
-var peer = new Peer('108.13.10.109', 8333);
+// get a peer from dns seed
+dns.resolve('dnsseed.bluematt.me', function(err, seeds) {
+  // use the first peer
+  var peer = new Peer(seeds[0], 8333);
+  
+  // create a connection without an existing socket
+  // but specify a socks5 proxy to create a socket 
+  // that's bound to that proxy in it's place
+  var connection = new Connection(null, peer, {
+    proxy: { host: '127.0.0.1', port: 9050 }
+  });
 
-// create a connection without an existing socket
-// but specify a socks5 proxy to create a socket 
-// that's bound to that proxy in it's place
-var connection = new Connection(null, peer, {
-  proxy: { host: '127.0.0.1', port: 9050 }
-});
+  connection.open();
 
-// open the connection
-connection.open();
+  connection.on('connect', function(data) {
+    console.log('connected through socks5!');
+  });
 
-// you can listen for the connect event
-connection.on('connect', function(data) {
-  // we are connected!
-  console.log('connected');
-});
+  connection.on('error', function(err) {
+    console.log(err);
+  });
 
-connection.on('error', function(err) {
-  // boo! :(
-  console.log(err);
 });
