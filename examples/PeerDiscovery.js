8b17591661dadbb02e3e9ab252482cc3d5d41617
Gordon Hall <gordon@gordonwritescode.com>
2014-04-19 16:46:00 -0400

    fix example

--- examples/PeerDiscovery.js
+++ examples/PeerDiscovery.js
@@ -5,7 +5,7 @@ peerman.discoverPeers(function(err, peers) {
   // we can use this array of peers to add to the manager
   // but let's limit it to 6 connections for this example
   var p = 0;
-  do { peerman.addPeer(peers[p]); p++; } while (p <= 6);
+  do { peerman.addPeer(peers[p]); p++; } while (p < 6);
   // then we can start the manager
   peerman.start();
 });
