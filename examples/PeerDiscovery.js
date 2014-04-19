e82867007ec54c8a384202df42a9742f9045ed76
Gordon Hall <gordon@gordonwritescode.com>
2014-04-19 15:12:44 -0400

    simplified example for peer discovery, added some debug output to discoverPeers(), made Peer reference the connection created from createConnection()

--- examples/PeerDiscovery.js
+++ examples/PeerDiscovery.js
@@ -2,14 +2,10 @@ var PeerManager = require('../lib/PeerManager');
 var peerman     = new PeerManager();
 
 peerman.discoverPeers(function(err, peers) {
-  // we get an array of peer instances
-  console.log(peers);
-  // the peer manager caches the tried seeds and any results
-  console.log(peerman.seeds);
   // we can use this array of peers to add to the manager
-  peers.forEach(function(p) {
-    peerman.addPeer(p);
-  });
+  // but let's limit it to 6 connections for this example
+  var p = 0;
+  do { peerman.addPeer(peers[p]); p++; } while (p <= 6);
   // then we can start the manager
   peerman.start();
 });
