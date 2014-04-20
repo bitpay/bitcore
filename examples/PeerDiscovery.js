def036b79f27aba66d6fc2517b181cc3bb3ab481
Gordon Hall <gordon@gordonwritescode.com>
2014-04-20 14:04:52 -0400

    setup connection pool and auto handling of bad seeds

--- examples/PeerDiscovery.js
+++ examples/PeerDiscovery.js
@@ -1,11 +1,4 @@
 var PeerManager = require('../lib/PeerManager');
 var peerman     = new PeerManager();
 
-peerman.discoverPeers(function(err, peers) {
-  // we can use this array of peers to add to the manager
-  // but let's limit it to 6 connections for this example
-  var p = 0;
-  do { peerman.addPeer(peers[p]); p++; } while (p < 6);
-  // then we can start the manager
-  peerman.start();
-});
+peerman.discover({ limit: 6 }).start();
