468ebddc1e49881ed3a352fecb450f64feae1d45
olalonde <olalonde@gmail.com>
2014-05-05 17:15:39 +0800

    Implemented Armory initializing from seed

--- examples/Armory.js
+++ examples/Armory.js
@@ -17,6 +17,8 @@ var chaincode = '84ac14bc4b388b33da099a0b4ee3b507284d99e1476639e36e5ca5e6af86481
 //var PublicX = '9df5 23e7 18b9 1f59 a790 2d46 999f 9357 ccf8 7208 24d4 3076 4516 b809 f7ab ce4e'
 //var PublicY = '66ba 5d21 4682 0dae 401d 9506 8437 2516 79f9 0c56 4186 cc50 07df c6d0 6989 1ff4';
 //var pubkey = '04' + PublicX.split(' ').join('') + PublicY.split(' ').join('');
+//console.log(pubkey);
+
 
 // mmm... can't figure out how to arrive at same pubkey as brainwallet
 var pubkey = '045a09a3286873a72f164476bde9d1d8e5c2bc044e35aa47eb6e798e325a86417f7c35b61d9905053533e0b4f2a26eca0330aadf21c638969e45aaace50e4c0c87';
@@ -28,3 +30,13 @@ for (var i = 0; i < 5; i++) {
   armory = armory.next();
   console.log(Address.fromPubKey(armory.pubkey).as('base58'));
 }
+
+// Derive stuff from root cod
+var seed = [
+  'aagh hjfj sihk ietj giik wwai awtd uodh hnji',
+  'soss uaku egod utai itos fijj ihgi jhau jtoo'
+].join('\n');
+
+console.log(seed);
+
+console.log(Armory.fromSeed(seed));
