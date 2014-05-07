f132dcd2eea5ecdb3cab53ceedba366fe9299d3d
Ryan X. Charles <ryan@bitpay.com>
2014-05-07 18:17:17 -0400

    add and expose sjcl

--- bitcore.js
+++ bitcore.js
@@ -61,5 +61,6 @@ requireWhenAccessed('PeerManager', './lib/PeerManager');
 requireWhenAccessed('Message', './lib/Message');
 requireWhenAccessed('Electrum', './lib/Electrum');
 requireWhenAccessed('Armory', './lib/Armory');
+requireWhenAccessed('sjcl', 'sjcl');
 module.exports.Buffer = Buffer;
 
