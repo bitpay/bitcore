9e46334d2747907a6d4fcac36e047ad684137bdd
Manuel Araoz <manuelaraoz@gmail.com>
2014-06-11 18:15:26 -0300

    wierdest way of fixing the problem

--- test/test.HierarchicalKey.js
+++ test/test.HierarchicalKey.js
@@ -307,13 +307,17 @@ describe('HierarchicalKey', function() {
   });
 
   describe('derivation in linux', function() {
-    it('should not be non-deterministic', function(){
+    it.only('should not be non-deterministic', function(){
+      var hp = 'm/45\'';
+      var sp = 'm/45';
+
       var hk = new HierarchicalKey('tprv8ZgxMBicQKsPdSF1avR6mXyDj5Uv1XY2UyUHSDpAXQ5TvPN7prGeDppjy4562rBB9gMMAhRfFdJrNDpQ4t69kkqHNEEen3PX1zBJqSehJDH');
-      console.log(hk.derive('m/45/0/0/0').extendedPrivateKeyString());
-      console.log(hk.derive('m/45/0/0/0').extendedPrivateKeyString());
-      console.log(hk.derive('m/45\'/0/0/0').extendedPrivateKeyString());
-      console.log(hk.derive('m/45\'/0/0/0').extendedPrivateKeyString());
-      hk.derive('m/45\'/0/0/0').extendedPrivateKeyString().should.equal(hk.derive('m/45\'/0/0/0').extendedPrivateKeyString());
+      hk.derive(sp).extendedPrivateKeyString().should.equal(
+        'tprv8cSDV3fVD6wqGoLKykTPhRwWLiwD6WBHvYHYkFvp8PJvApm7HCfY9HH9P6Q6iPaCGNsU3LEqh7iJMN7478TqjkLFnf71f9zBXXd7XoiL7dw');
+      hk.derive(sp).extendedPrivateKeyString().should.equal(hk.derive(sp).extendedPrivateKeyString());
+      hk.derive(hp).extendedPrivateKeyString().should.equal(hk.derive(hp).extendedPrivateKeyString());
+      hk.derive(hp).extendedPrivateKeyString().should.equal(
+        'tprv8cSDV3fdYmUoTNGu4xRTm6qh3DPrNxPZzukM5FPdWoa9m22ALFJVGbjnU7J4TC5t3MJp293GtZWssAPuV1PNWGjXavQTnXy9xW6Lee2X6rd');
     });
   });
 
