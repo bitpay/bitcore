fb3cc380460bbfba4dd8d92571e81c19e9d02dbb
Ryan X. Charles <ryan@bitpay.com>
2014-07-10 18:17:11 -0700

    make bignum tests run in node, not just browser

---
+++ test/test.Bignum.js
@@ -0,0 +1,65 @@
+var chai = chai || require('chai');
+var bitcore = bitcore || require('../bitcore');
+var coinUtil = coinUtil || bitcore.util;
+var should = chai.should();
+var assert = chai.assert;
+
+var Bignum = bitcore.Bignum;
+
+describe('Bignum', function() {
+  it('should create a bignum', function() {
+    var bn = new Bignum(50);
+    should.exist(bn);
+    bn.toString().should.equal('50');
+  });
+
+  it('should parse this number', function() {
+    var bn = new Bignum(999970000);
+    bn.toString().should.equal('999970000');
+  });
+  
+  describe('#add', function() {
+
+    it('should add two small numbers together', function() {
+      var bn1 = new Bignum(50);
+      var bn2 = new Bignum(75);
+      var bn3 = bn1.add(bn2);
+      bn3.toString().should.equal('125');
+    });
+
+  });
+
+  describe('#sub', function() {
+
+    it('should subtract a small number', function() {
+      var bn1 = new Bignum(50);
+      var bn2 = new Bignum(25);
+      var bn3 = bn1.sub(bn2);
+      bn3.toString().should.equal('25');
+    });
+
+  });
+
+  describe('#gt', function() {
+
+    it('should say 1 is greater than 0', function() {
+      var bn1 = new Bignum(1);
+      var bn0 = new Bignum(0);
+      bn1.gt(bn0).should.equal(true);
+    });
+
+    it('should say a big number is greater than a small big number', function() {
+      var bn1 = new Bignum('24023452345398529485723980457');
+      var bn0 = new Bignum('34098234283412341234049357');
+      bn1.gt(bn0).should.equal(true);
+    });
+
+    it('should say a big number is great than a standard number', function() {
+      var bn1 = new Bignum('24023452345398529485723980457');
+      var bn0 = new Bignum(5);
+      bn1.gt(bn0).should.equal(true);
+    });
+
+  });
+
+});
