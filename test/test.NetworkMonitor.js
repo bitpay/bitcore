a0a730b3d81b28a4617cedd16cf2bd4bab6dbb8b
Manuel Araoz <manuelaraoz@gmail.com>
2014-07-21 11:10:35 -0300

    add tx basic event test

--- test/test.NetworkMonitor.js
+++ test/test.NetworkMonitor.js
@@ -91,6 +91,20 @@ describe('NetworkMonitor', function() {
   var unrelatedRaw = '010000000114bae675546f758e0dbab95aa88d4db0c63e26f8fd6cbbce3a4827446d4937cf00000000700048304502201da760691f18a0ab140de1437e4bd29767b74add8cca8e38d46a2f37d9a8188f022100cbf4e121d97b4db846d236957da7fc17fd706ad47b41ae63adf953982e34f70901255121022f58491a833933a9bea80d8e820e66bee91bd8c71bfa972fe70482360b48129951aeffffffff01706f9800000000001976a91400a26ff8123593e10d0a9eba2a74db33cd69299288ac00000000';
   var outgoingRaw = '0100000001613b50ef601ac068b7805afb8615bb06371881321a478b62d1f52d21f2a8529c000000006b483045022100e3c38e6da99bc8e4b6150404d3afc9ee74b5b48a245311e8fb0e019a3f69570102201eda167b14d675f7b9cf60cf1b0c65b1d66efa4a339743aaec047ce90b92e52e0121031915a253ead0da95c46ff64d07fe4d562a29b7fc211c6a8f49764ac85c039de4ffffffff01f0a29a3b000000001976a914c69536a7d60748bb1953e5e186edf920efa823e388ac00000000';
 
+  describe('tx event', function() {
+    it('should be called on network transactions', function() {
+      var nm = createConnectedNM();
+      var spy = sinon.spy();
+      nm.on('tx', spy);
+      var tx1 = simulateNetworkTx(incomingRaw, nm);
+      var tx2 = simulateNetworkTx(unrelatedRaw, nm);
+      var tx3 = simulateNetworkTx(outgoingRaw, nm);
+      spy.calledWith(tx1).should.equal(true);
+      spy.calledWith(tx2).should.equal(true);
+      spy.calledWith(tx3).should.equal(true);
+      spy.callCount.should.equal(3);
+    });
+  });
   describe('incoming tx event', function() {
     it('should be called on incoming transactions', function() {
       var nm = createConnectedNM();
