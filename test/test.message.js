15f9a99e65166a9222db069cc4e6b28a6b4ef62d
Ryan X. Charles <ryan@bitpay.com>
2014-08-21 15:50:38 -0700

    message signing

---
+++ test/test.message.js
@@ -0,0 +1,49 @@
+var Address = require('../lib/address');
+var Message = require('../lib/message');
+var Key = require('../lib/key');
+var should = require('chai').should();
+
+describe('Message', function() {
+  
+  it('should make a new message', function() {
+    var message = new Message();
+    should.exist(message);
+  });
+
+  it('should make a new message when called without "new"', function() {
+    var message = Message();
+    should.exist(message);
+  });
+
+  describe('#sign', function() {
+    var messagebuf = new Buffer('this is my message');
+    var key = Key().fromRandom();
+
+    it('should sign a message', function() {
+      var message = new Message();
+      message.messagebuf = messagebuf;
+      message.key = key;
+      message.sign();
+      var sig = message.sig;
+      should.exist(sig);
+    });
+
+  });
+
+  describe('#verify', function() {
+    var messagebuf = new Buffer('this is my message');
+    var key = Key().fromRandom();
+
+    it('should verify a message that was just signed', function() {
+      var message = new Message();
+      message.messagebuf = messagebuf;
+      message.key = key;
+      message.address = Address().fromPubkey(key.pubkey);
+      message.sign();
+      message.verify();
+      message.verified.should.equal(true);
+    });
+
+  });
+
+});
