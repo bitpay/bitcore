38d9ab65af0984d3f0ab0bcc5c27586a1bec42e2
Ryan X. Charles <ryan@bitpay.com>
2014-08-24 20:51:56 -0700

    xor buffers ... will be useful for CBC

--- test/test.cbc.js
+++ test/test.cbc.js
@@ -2,6 +2,16 @@ var should = require('chai').should();
 var CBC = require('../lib/expmt/cbc');
 
 describe('CBC', function() {
+
+  it('should return a new CBC', function() {
+    var cbc = new CBC();
+    should.exist(cbc);
+  })
+
+  it('should return a new CBC when called without "new"', function() {
+    var cbc = new CBC();
+    should.exist(cbc);
+  });
   
   describe('@pkcs7pad', function() {
     
@@ -14,6 +24,24 @@ describe('CBC', function() {
       padbuf[32 / 8 + 1].should.equal(128 / 8 - 32 / 8);
       // ...
       padbuf[32 / 8 + 128 / 8 - 32 / 8 - 1].should.equal(128 / 8 - 32 / 8);
+    });
+
+  });
+
+  describe('@xorbufs', function() {
+    
+    it('should xor 1 and 0', function() {
+      var buf1 = new Buffer([1]);
+      var buf2 = new Buffer([0]);
+      var buf = CBC.xorbufs(buf1, buf2);
+      buf[0].should.equal(1);
+    });
+
+    it('should xor 1 and 1', function() {
+      var buf1 = new Buffer([1]);
+      var buf2 = new Buffer([1]);
+      var buf = CBC.xorbufs(buf1, buf2);
+      buf[0].should.equal(0);
     });
 
   });
