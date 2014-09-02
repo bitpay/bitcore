6375941ef818977570b7a777bc99e8366380cd09
Ryan X. Charles <ryan@bitpay.com>
2014-09-01 18:31:02 -0700

    script.toString()

--- test/script.js
+++ test/script.js
@@ -151,4 +151,22 @@ describe('Script', function() {
 
   });
 
+  describe('#toString', function() {
+
+    it('should output this buffer an OP code, data, and another OP code', function() {
+      var buf = new Buffer([0, 0, 0, 0, 0, 0, 1, 2, 3, 0]);
+      buf[0] = Opcode('OP_0').toNumber();
+      buf[1] = Opcode('OP_PUSHDATA4').toNumber();
+      buf.writeUInt16LE(3, 2);
+      buf[buf.length - 1] = Opcode('OP_0').toNumber();
+      var script = Script().fromBuffer(buf);
+      script.chunks.length.should.equal(3);
+      script.chunks[0].should.equal(buf[0]);
+      script.chunks[1].buf.toString('hex').should.equal('010203');
+      script.chunks[2].should.equal(buf[buf.length - 1]);
+      script.toString().toString('hex').should.equal('OP_0 OP_PUSHDATA4 3 0x010203 OP_0');
+    });
+
+  });
+
 });
