2ecf1cdcdffe0850fb64201a7452ef913086d101
Ryan X. Charles <ryan@bitpay.com>
2014-09-18 15:20:23 -0700

    Block parsing example
    bitcoind saves blocks in files called blk*****.dat. Those files can be piped
    into this example, which will parse them and spit out a nice looking string of
    all the blocks, which also includes parsed transactions.

---
+++ examples/blockreader.js
@@ -0,0 +1,20 @@
+var Block = require('../lib/block');
+var BufferReader = require('../lib/bufferreader');
+var BufferWriter = require('../lib/bufferwriter');
+
+//This example will parse the blocks in a block file.
+//To use, pipe in a blk*****.dat file. e.g.:
+//cat blk00000.dat | node blockreader.js
+
+var bw = new BufferWriter();
+
+process.stdin.on('data', function(buf) {
+  bw.write(buf);
+});
+
+process.stdin.on('end', function(buf) {
+  var blocksbuf = bw.concat();
+  var br = new BufferReader(blocksbuf);
+  while (!br.eof())
+    console.log(Block().fromBufferReader(br));
+});
