4dfe9fd13d178322f597238d858f70346729e49b
Eric Martindale <eric@ericmartindale.com>
2014-10-02 22:33:22 -0400

    Update example code.

--- examples/identity.js
+++ examples/identity.js
@@ -4,7 +4,8 @@ var Keypair  = require('../lib/keypair');
 var pubkeyhash = new Buffer('3c3fa3d4adcaf8f52d5b1843975e122548269937', 'hex');
 var buf = Buffer.concat([new Buffer([0]), pubkeyhash]);
 
-var keypair  = new Keypair().fromString( buf.toString('hex') );
+var keypair  = new Keypair();
+//var identity = new Identity().fromPubkey( keypair.pubkey );
 var identity = new Identity().fromPubkey( keypair.pubkey );
 
 console.log( 'pubkey',  keypair.pubkey.toString() );
