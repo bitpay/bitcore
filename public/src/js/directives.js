beee5a3003314372604b36a26cd0b1e101ca5699
Andrew Hart <andrew@datafluency.com>
2014-10-21 16:42:40 -0700

    Add focus directive to be used to give input elements focus

--- public/src/js/directives.js
+++ public/src/js/directives.js
@@ -72,4 +72,20 @@ angular.module('insight')
         });
       }
     };
+  })
+  .directive('focus', function ($timeout) {
+    return {
+      scope: {
+        trigger: '@focus'
+      },
+      link: function (scope, element) {
+        scope.$watch('trigger', function (value) {
+          if (value === "true") {
+            $timeout(function () {
+              element[0].focus();
+            });
+          }
+        });
+      }
+    };
   });
