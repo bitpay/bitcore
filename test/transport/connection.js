738b56f5a51a17c4e0559c5a2b01b9684e7980a4
Yemel Jardi <angel.jardi@gmail.com>
2014-12-05 11:39:30 -0300

    Add transport/connection class

---
+++ test/transport/connection.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var chai = require('chai');
+var bitcore = require('../..');
+
+var should = chai.should();
+
+var ConnectionModule = bitcore.transport.Connection;
+var Connection;
+var nop = function() {};
+
+describe.only('Connection', function() {
+  it('should initialze the main object', function() {
+    should.exist(ConnectionModule);
+  });
+  it('should be able to create class', function() {
+    Connection = ConnectionModule;
+    should.exist(Connection);
+  });
+  it('should be able to create instance', function() {
+    var mSocket = {server: null, addListener: nop},
+      mPeer;
+    var c = new Connection(mSocket, mPeer);
+    should.exist(c);
+  });
+
+  if (typeof process !== 'undefined' && process.versions) { //node-only tests
+    it('should create a proxied socket if instructed', function() {
+      var mPeer;
+      var c = new Connection(null, mPeer, {
+        proxy: { host: 'localhost', port: 9050 }
+      });
+      should.exist(c.socket);
+    });
+  };
+});
+
+
+
+
+
