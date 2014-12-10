c085f166b4e2c155b3206bd044cc2197e442bc5b
Manuel Araoz <manuelaraoz@gmail.com>
2014-12-10 16:54:30 -0300

    add sorting to Script#buildMutlisigOut()

--- test/script.js
+++ test/script.js
@@ -400,7 +400,7 @@ describe('Script', function() {
     });
   });
 
-  describe.only('#buildMultisigOut', function() {
+  describe('#buildMultisigOut', function() {
     var pubkey_hexs = [
       '022df8750480ad5b26950b25c7ba79d3e37d75f640f8e5d9bcd5b150a0f85014da',
       '03e3818b65bcc73a7d64064106a859cc1a5a728c4345ff0b641209fba0d90de6e9',
