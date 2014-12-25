9c02a4b677597ce5633a08fc9f6acc8c8b73c09a
Braydon Fuller <braydon@bitpay.com>
2014-12-25 19:04:03 -0300

    Tests: Use karma to launch browsers locally if installed.

--- karma.conf.js
+++ karma.conf.js
@@ -2,12 +2,35 @@
 
 // karma.conf.js
 module.exports = function(config) {
+
   config.set({
-    frameworks: ['mocha'],
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Firefox'],
+    frameworks: ['mocha', 'detectBrowsers'],
+    detectBrowsers: {
+      enabled: true,
+      usePhantomJS: false,
+      postDetection: function(availableBrowser) {
+        // modify to enable additional browsers if available
+        var runBrowsers = ['Firefox', 'Chrome'];
+        var browsers = [];
+        for(var i = 0; i < runBrowsers.length; i++) {
+          if(~availableBrowser.indexOf(runBrowsers[i])) {
+            browsers.push(runBrowsers[i]);
+          }
+        }
+        return browsers;
+      }
+    },
     singleRun: true,
     files: [
       'browser/tests.js'
+    ],
+    plugins: [
+      'karma-mocha',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-detect-browsers'
     ]
   });
+
 };
