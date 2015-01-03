a1ee393dce56539df8dbe14d11a855e503311dae
Braydon Fuller <braydon@bitpay.com>
2015-01-02 19:38:22 -0500

    JSUtil: Return result quicker for isValidJSON and add additional tests.

--- test/util/js.js
+++ test/util/js.js
@@ -13,6 +13,7 @@ describe('js utils', function() {
     
     var hexa = '8080808080808080808080808080808080808080808080808080808080808080';
     var json = '{"key": ["value", "value2"]}';
+    var json2 = '["value", "value2", {"key": "value"}]';
 
     it('does not mistake an integer as valid json object', function() {
       var valid = JSUtil.isValidJSON(hexa);
@@ -20,6 +21,11 @@ describe('js utils', function() {
     });
 
     it('correctly validates a json object', function() {
+      var valid = JSUtil.isValidJSON(json);
+      valid.should.equal(true);
+    });
+
+    it('correctly validates an array json object', function() {
       var valid = JSUtil.isValidJSON(json);
       valid.should.equal(true);
     });
