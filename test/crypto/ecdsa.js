97b6e8b9897b8a3d3d3d1558e0e7c42679625255
Ryan X. Charles <ryanxcharles@gmail.com>
2015-01-04 19:37:45 -0800

    add deterministicK test vectors
    These vectors were produced by @bip32JP and tested against a similar
    implementation of the "badrs" approach to RFC 6979 in python-ecdsa as described
    in this related bitcoinjs-lib PR:
    
    https://github.com/bitcoinjs/bitcoinjs-lib/pull/337

--- test/crypto/ecdsa.js
+++ test/crypto/ecdsa.js
@@ -296,6 +296,21 @@ describe('ECDSA', function() {
           ecdsa.sigError().should.equal(obj.exception);
         });
       });
+      
+      vectors.deterministicK.forEach(function(obj, i) {
+        it('should validate deterministicK vector ' + i, function() {
+          var hashbuf = Hash.sha256(new Buffer(obj.message));
+          var privkey = Privkey(BN.fromBuffer(new Buffer(obj.privkey, 'hex')), 'mainnet');
+          var ecdsa = ECDSA({
+            privkey: privkey,
+            hashbuf: hashbuf
+          });
+          ecdsa.deterministicK(0).k.toString('hex').should.equal(obj.k_bad00);
+          ecdsa.deterministicK(1).k.toString('hex').should.equal(obj.k_bad01);
+          ecdsa.deterministicK(15).k.toString('hex').should.equal(obj.k_bad15);
+        });
+      });
+
     });
   });
 });
