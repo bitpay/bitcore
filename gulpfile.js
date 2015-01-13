e8cd70c28aec42b26b2c43a89d4b864e5105aab1
Esteban Ordano <eordano@gmail.com>
2015-01-13 19:12:14 -0300

    Use gulp-bitcore for releases

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@
 var gulp = require('gulp');
 var gulp_bitcore = require('gulp-bitcore');
 
-gulp_bitcore('p2p');
+gulp_bitcore('p2p', {skipBrowser: true});
 
 gulp.task('default', ['lint', 'coverage']);
