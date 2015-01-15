eddffcc682722a5d613a8a94333629eb513865db
Yemel Jardi <angel.jardi@gmail.com>
2015-01-15 17:53:16 -0300

    Add karma config

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function startGulp(name, opts) {
   };
 
   var testkarma = shell.task([
-    './node_modules/karma/bin/karma start'
+    './node_modules/karma/bin/karma start ./node_modules/gulp-bitcore/karma.conf.js'
   ]);
 
   gulp.task('test:node', testmocha);
