6aa0d14bb998ceba60aeee6fe266d30d0b7b7055
Braydon Fuller <braydon@bitpay.com>
2015-01-16 23:23:02 -0500

    Removed magicnumber and size as properties of a block.

--- test/block.js
+++ test/block.js
@@ -31,7 +31,7 @@ describe('Block', function() {
   });
   var json = dataJson;
 
-  var genesishex = 'f9beb4d91d0100000100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c0101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000';
+  var genesishex = '0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c0101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000';
   var genesisbuf = new Buffer(genesishex, 'hex');
   var genesisidhex = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f';
 
@@ -50,20 +50,16 @@ describe('Block', function() {
 
     it('should set these known values', function() {
       var b = new Block({
-        magicnum: magicnum,
-        size: size,
         header: bh,
         transactions: txs
       });
-      should.exist(b.magicnum);
-      should.exist(b.size);
       should.exist(b.header);
       should.exist(b.transactions);
     });
 
     it('should properly deserialize blocks', function() {
       dataBlocks.forEach(function(block){
-        var b = Block.fromBuffer(new Buffer(block.data, 'hex'), {skipMagic: true});
+        var b = Block.fromBuffer(new Buffer(block.data, 'hex'));
         b.transactions.length.should.equal(block.transactions);
       });
     });
@@ -90,8 +86,6 @@ describe('Block', function() {
 
     it('should set these known values', function() {
       var block = Block.fromJSON(json);
-      should.exist(block.magicnum);
-      should.exist(block.size);
       should.exist(block.header);
       should.exist(block.transactions);
     });
@@ -99,8 +93,6 @@ describe('Block', function() {
     it('should set these known values', function() {
 
       var block = Block(json);
-      should.exist(block.magicnum);
-      should.exist(block.size);
       should.exist(block.header);
       should.exist(block.transactions);
     });
@@ -117,8 +109,6 @@ describe('Block', function() {
     it('should recover these known values', function() {
       var block = Block.fromJSON(json);
       var b = JSON.parse(block.toJSON());
-      should.exist(b.magicnum);
-      should.exist(b.size);
       should.exist(b.header);
       should.exist(b.transactions);
     });
