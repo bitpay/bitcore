6fa7e7603a0a1adb3b84cc5067fc9bb324a4733a
Braydon Fuller <braydon@bitpay.com>
2015-01-22 19:19:40 -0500

    Networks: Added the ability to define a custom network.

--- test/networks.js
+++ test/networks.js
@@ -13,6 +13,34 @@ describe('Networks', function() {
     should.exist(networks.defaultNetwork);
   });
 
+  it('should be able to define a custom Network', function() {
+    var custom = {
+      name: 'customnet',
+      alias: 'mynet',
+      pubkeyhash: 0x10,
+      privatekey: 0x90,
+      scripthash: 0x08,
+      xpubkey:  0x0278b20e,
+      xprivkey: 0x0278ade4,
+      networkMagic: 0xe7beb4d4,
+      port: 20001,
+      dnsSeeds: [
+        'localhost',
+        'mynet.localhost'
+      ]
+    };
+    networks.add(custom);
+    var customnet = networks.get('customnet');
+    for (var key in custom) {
+      if (key !== 'networkMagic') {
+        customnet[key].should.equal(custom[key]);
+      } else {
+        var expected = new Buffer('e7beb4d4', 'hex');
+        customnet[key].should.deep.equal(expected);
+      }
+    }
+  });
+
   var constants = ['name', 'alias', 'pubkeyhash', 'scripthash', 'xpubkey', 'xprivkey'];
 
   constants.forEach(function(key){
