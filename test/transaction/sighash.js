da90ff8617567dbfa51b976d036ce9977bc5f21e
Esteban Ordano <eordano@gmail.com>
2015-01-27 11:18:21 -0300

    Use the exposed interface to sighash on tests

--- test/transaction/sighash.js
+++ test/transaction/sighash.js
@@ -4,9 +4,10 @@ var buffer = require('buffer');
 
 var chai = require('chai');
 var should = chai.should();
-var Script = require('../../lib/script');
-var Transaction = require('../../lib/transaction');
-var sighash = require('../../lib/transaction/sighash');
+var bitcore = require('../../');
+var Script = bitcore.Script;
+var Transaction = bitcore.Transaction;
+var sighash = Transaction.sighash;
 
 var vectors_sighash = require('../data/sighash.json');
 
