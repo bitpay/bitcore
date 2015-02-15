7ecdea11210d6929c8f3dfed6eee62645d536201
Matias Alejo Garcia <ematiu@gmail.com>
2015-02-15 13:18:12 -0300

    Merge pull request #13 from isocolsky/reject
    reject & rm

--- packages/bitcore-wallet-cli/bit-wallet/common.js
+++ packages/bitcore-wallet-cli/bit-wallet/common.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var _ = require('lodash');
+
 var common = function() {};
 
 
@@ -9,14 +13,14 @@ var die = common.die = function(err) {
 };
 
 common.parseMN = function(MN) {
-  if (!MN) 
+  if (!MN)
     die('No m-n parameter');
   var mn = MN.split('-');
 
-  var m = parseInt(mn[0]); 
+  var m = parseInt(mn[0]);
   var n = parseInt(mn[1]);
 
-  if (!m || ! n) {
+  if (!m || !n) {
     die('Bad m-n parameter');
   }
 
@@ -26,6 +30,22 @@ common.parseMN = function(MN) {
 
 common.shortID = function(id) {
   return id.substr(id.length - 4);
+};
+
+common.findOneTxProposal = function(txps, id) {
+  var matches = _.filter(txps, function(tx) {
+    return _.endsWith(common.shortID(tx.id), id);
+  });
+
+  if (!matches.length)
+    common.die('Could not find TX Proposal:' + id);
+
+  if (matches.length > 1)
+    common.die('More than one TX Proposals match:' + id + ' : ' + _.map(matches, function(tx) {
+      return tx.id;
+    }).join(' '));;
+
+  return matches[0];
 };
 
 module.exports = common;
