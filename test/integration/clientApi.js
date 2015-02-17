196ae2a4481b5c47965f8028f48a55a5decd3503
Matias Alejo Garcia <ematiu@gmail.com>
2015-02-17 02:20:04 -0300

    add test

--- test/integration/clientApi.js
+++ test/integration/clientApi.js
@@ -7,51 +7,95 @@ var should = chai.should();
 var Client = require('../../lib/client');
 var API = Client.API;
 var Bitcore = require('bitcore');
+var TestData = require('./clienttestdata');
 
-var wallet11 = {
-  "m": 1,
-  "n": 1,
-  "walletPrivKey": "{\"bn\":\"6b862ffbfc90a37a2fedbbcfea91c6a4e49f49b6aaa322b6e16c46bfdbe71a38\",\"compressed\":true,\"network\":\"livenet\"}",
-  "network": "testnet",
-  "xPrivKey": "tprv8ZgxMBicQKsPeisyNJteQXZnb7CnhYc4TVAyxxicXuxMjK1rmaqVq1xnXtbSTPxUKKL9h5xJhUvw1AKfDD3i98A82eJWSYRWYjmPksewFKR",
-  "copayerId": "a84daa08-17b5-45ad-84cd-e275f3b07123",
-  "signingPrivKey": "42798f82c4ed9ace4d66335165071edf180e70bc0fc08dacb3e35185a2141d5b",
-  "publicKeyRing": ["tpubD6NzVbkrYhZ4YBumFxZEowDuA8iirsny2nmmFUkuxBkkZoGdPyf61Waei3tDYvVa1yqW82Xhmmd6oiibeDyM1MS3zTiky7Yg75UEV9oQhFJ"]
-};
-
-var incompleteWallet22 = {
-  "m": 2,
-  "n": 2,
-  "walletPrivKey": "L3XSE3KNjQM1XRP1h5yMCSKsN4hs3D6eK7Vwn5M88Bs6jpCnXR3R",
-  "network": "testnet",
-  "secret": "d9cf45a1-6793-4df4-94df-c99d2c2e1fe9:bc2488c1b83e455a4b908a0d0aeaf70351efc48fbcaa454bffefdef419a5ee6a:T",
-  "xPrivKey": "tprv8ZgxMBicQKsPdoC5DGtnXx7fp7YnUtGv8b7fU2oDQfDpHFQh1QCgpKc8GHpdsBN5THaHYMV5LgD5cP5NYaacGVr786p3mVLSZff9berTV8h",
-  "copayerId": "c3a33ca0-37cf-4e80-b745-71272683835c",
-  "signingPrivKey": "6e129c4996666e5ecdf78aed626c01977fa19eacce6659738ebe065f86523e9b",
-  "publicKeyRing": []
-};
-
-
-describe('client API', function() {
+describe(' client API ', function() {
 
   var client;
 
   beforeEach(function() {
 
     var fsmock = {};;
-    fsmock.readFile = sinon.mock().yields(null, JSON.stringify(wallet11));
+    fsmock.readFile = sinon.mock().yields(null, JSON.stringify(TestData.storage.wallet11));
     fsmock.writeFile = sinon.mock().yields();
     var storage = new Client.FileStorage({
       filename: 'dummy',
       fs: fsmock,
     });
     client = new Client({
-      storage: storage,
+      storage: storage
     });
   });
 
-  describe('createAddress', function() {
-    it('should check address', function(done) {
+  describe(' _tryToComplete ', function() {
+    it('should complete a wallet ', function(done) {
+      var request = sinon.stub();
+
+      // Wallet request
+      request.onCall(0).yields(null, {
+        statusCode: 200,
+      }, TestData.serverResponse.completeWallet);
+      request.onCall(1).yields(null, {
+        statusCode: 200,
+      }, "pepe");
+
+      client.request = request;
+      client.storage.fs.readFile = sinon.stub().yields(null, JSON.stringify(TestData.storage.incompleteWallet22));
+      client.getBalance(function(err, x) {
+        should.not.exist(err);
+        done();
+      });
+    })
+
+
+    it('should complain wallet is not complete ', function(done) {
+      var request = sinon.stub();
+
+      // Wallet request
+      request.onCall(0).yields(null, {
+        statusCode: 200,
+      }, TestData.serverResponse.incompleteWallet);
+
+      client.request = request;
+      client.storage.fs.readFile = sinon.stub().yields(null, JSON.stringify(TestData.storage.incompleteWallet22));
+      client.createAddress(function(err, x) {
+        err.should.contain('Incomplete');
+        done();
+      });
+    })
+
+    it(' should reject wallets with bad signatures', function(done) {
+      var request = sinon.stub();
+      // Wallet request
+      request.onCall(0).yields(null, {
+        statusCode: 200,
+      }, TestData.serverResponse.corruptWallet22);
+
+      client.request = request;
+      client.storage.fs.readFile = sinon.stub().yields(null, JSON.stringify(TestData.storage.incompleteWallet22));
+      client.createAddress(function(err, x) {
+        err.should.contain('verified');
+        done();
+      });
+    })
+    it(' should reject wallets with missing signatures ', function(done) {
+      var request = sinon.stub();
+      // Wallet request
+      request.onCall(0).yields(null, {
+        statusCode: 200,
+      }, TestData.serverResponse.corruptWallet222);
+
+      client.request = request;
+      client.storage.fs.readFile = sinon.stub().yields(null, JSON.stringify(TestData.storage.incompleteWallet22));
+      client.createAddress(function(err, x) {
+        err.should.contain('verified');
+        done();
+      });
+    })
+  });
+
+  describe(' createAddress ', function() {
+    it(' should check address ', function(done) {
 
       var response = {
         createdOn: 1424105995,
@@ -71,7 +115,7 @@ describe('client API', function() {
         done();
       });
     })
-    it('should detect fake addresses', function(done) {
+    it(' should detect fake addresses ', function(done) {
       var response = {
         createdOn: 1424105995,
         address: '2N3fA6wDtnebzywPkGuNK9KkFaEzgbPRRTq',
@@ -88,20 +132,5 @@ describe('client API', function() {
         done();
       });
     })
-
-    it('should complain wallet is not complete', function(done) {
-      var request = sinon.mock().yields(null, {
-        statusCode: 200
-      }, {
-        dummy: true
-      });
-      client.request = request;
-      client.storage.fs.readFile = sinon.mock().yields(null, JSON.stringify(incompleteWallet22));
-      client.createAddress(function(err, x) {
-        err.should.contain('Incomplete');
-        done();
-      });
-    })
-
-  });
+  })
 });
