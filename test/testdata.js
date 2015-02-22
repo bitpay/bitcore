e012f334f34d189ee3dd44a3de82478248d9b3e9
Ivan Socolsky <jungans@gmail.com>
2015-02-21 22:35:12 -0300

    tx history

--- test/testdata.js
+++ test/testdata.js
@@ -62,6 +62,82 @@ var copayers = [{
 }, ];
 
 
+var history = [{
+  txid: "0279ef7b21630f859deb723e28beac9e7011660bd1346c2da40321d2f7e34f04",
+  vin: [{
+    txid: "c8e221141e8bb60977896561b77fa59d6dacfcc10db82bf6f5f923048b11c70d",
+    vout: 0,
+    n: 0,
+    addr: "2N6Zutg26LEC4iYVxi7SHhopVLP1iZPU1rZ",
+    valueSat: 485645,
+    value: 0.00485645,
+  }, {
+    txid: "6e599eea3e2898b91087eb87e041c5d8dec5362447a8efba185ed593f6dc64c0",
+    vout: 1,
+    n: 1,
+    addr: "2MyqmcWjmVxW8i39wdk1CVPdEqKyFSY9H1S",
+    valueSat: 885590,
+    value: 0.0088559,
+  }],
+  vout: [{
+    value: "0.00045753",
+    n: 0,
+    scriptPubKey: {
+      addresses: [
+        "2NAVFnsHqy5JvqDJydbHPx393LFqFFBQ89V"
+      ]
+    },
+  }, {
+    value: "0.01300000",
+    n: 1,
+    scriptPubKey: {
+      addresses: [
+        "mq4D3Va5mYHohMEHrgHNGzCjKhBKvuEhPE"
+      ]
+    }
+  }],
+  confirmations: 2,
+  time: 1424471041,
+  blocktime: 1424471041,
+  valueOut: 0.01345753,
+  valueIn: 0.01371235,
+  fees: 0.00025482
+}, {
+  txid: "fad88682ccd2ff34cac6f7355fe9ecd8addd9ef167e3788455972010e0d9d0de",
+  vin: [{
+    txid: "0279ef7b21630f859deb723e28beac9e7011660bd1346c2da40321d2f7e34f04",
+    vout: 0,
+    n: 0,
+    addr: "2NAVFnsHqy5JvqDJydbHPx393LFqFFBQ89V",
+    valueSat: 45753,
+    value: 0.00045753,
+  }],
+  vout: [{
+    value: "0.00011454",
+    n: 0,
+    scriptPubKey: {
+      addresses: [
+        "2N7GT7XaN637eBFMmeczton2aZz5rfRdZso"
+      ]
+    }
+  }, {
+    value: "0.00020000",
+    n: 1,
+    scriptPubKey: {
+      addresses: [
+        "mq4D3Va5mYHohMEHrgHNGzCjKhBKvuEhPE"
+      ]
+    }
+  }],
+  confirmations: 1,
+  time: 1424472242,
+  blocktime: 1424472242,
+  valueOut: 0.00031454,
+  valueIn: 0.00045753,
+  fees: 0.00014299
+}];
+
 module.exports.keyPair = keyPair;
 module.exports.message = message;
 module.exports.copayers = copayers;
+module.exports.history = history;
