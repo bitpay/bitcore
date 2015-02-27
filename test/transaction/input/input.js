b92fd915ebedfb5e11efe93cc87d5a7d9a058671
Manuel Araoz <manuelaraoz@gmail.com>
2015-02-27 18:27:56 -0300

    increase coverage for Input and Output, fix some bugs

--- test/transaction/input/input.js
+++ test/transaction/input/input.js
@@ -30,6 +30,14 @@ describe('Transaction.Input', function() {
     script: new Script(),
     satoshis: 1000000
   };
+  var coinbaseJSON = '{"prevTxId":"0000000000000000000000000000000000000000000000000000000000000000"' +
+    ',"outputIndex":4294967295,"script":""}';
+  var otherJSON = '{"txidbuf":"a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458"' +
+    ',"txoutnum":0,"seqnum":4294967295,"script":"71 0x3044022006553276ec5b885ddf5cc1d7' +
+    '9e1e3dadbb404b60ad4cc00318e215654f13242102200757c17b36e3d0492fb9cf597032e5afbea67a59274e64af' +
+    '5a05d12e5ea2303901 33 0x0223078d2942df62c45621d209fab84ea9a7a23346201b7727b9b45a29c4e76f5e",' +
+    '"output":{"satoshis":100000,"script":"OP_DUP OP_HASH160 20 0x88d9931ea73d60eaf7e5671efc0552b' +
+    '912911f2a OP_EQUALVERIFY OP_CHECKSIG"}}';
 
   it('has abstract methods: "getSignatures", "isFullySigned", "addSignature", "clearSignatures"', function() {
     var input = new Input(output);
@@ -41,6 +49,39 @@ describe('Transaction.Input', function() {
   });
   it('detects coinbase transactions', function() {
     new Input(output).isNull().should.equal(false);
-    new Input(coinbase).isNull().should.equal(true);
+    var ci = new Input(coinbase);
+    ci.isNull().should.equal(true);
+  });
+
+  describe('instantiation', function() {
+    it('works without new', function() {
+      var input = Input();
+      should.exist(input);
+    });
+    it('fails with no script info', function() {
+      expect(function() {
+        var input = new Input({});
+        input.toString();
+      }).to.throw('Need a script to create an input');
+    });
+    it('fromJSON', function() {
+      var input = Input.fromJSON(coinbaseJSON);
+      var otherInput = Input.fromJSON(otherJSON);
+      should.exist(input);
+      should.exist(otherInput);
+    });
+    it('fromObject', function() {
+      var input = Input.fromJSON(coinbaseJSON);
+      var obj = input.toObject();
+      obj.script = new Buffer(obj.script, 'hex');
+      Input.fromObject(obj).should.deep.equal(input);
+      obj.script = 42;
+      Input.fromObject.bind(null, obj).should.throw('Invalid argument type: script');
+    });
+  });
+
+  it('_estimateSize', function() {
+    var input = new Input(output);
+    input._estimateSize().should.equal(66);
   });
 });
--- test/transaction/output.js
+++ test/transaction/output.js
@@ -17,10 +17,16 @@ var errors = bitcore.errors;
 
 describe('Output', function() {
 
-  var output = new Output({satoshis: 0, script: Script.empty()});
+  var output = new Output({
+    satoshis: 0,
+    script: Script.empty()
+  });
 
   it('can be assigned a satoshi amount in big number', function() {
-    var newOutput = new Output({satoshis: new BN(100), script: Script.empty()});
+    var newOutput = new Output({
+      satoshis: new BN(100),
+      script: Script.empty()
+    });
     newOutput.satoshis.should.equal(100);
   });
 
@@ -37,7 +43,10 @@ describe('Output', function() {
   });
 
   it('roundtrips to/from object', function() {
-    var newOutput = new Output({satoshis: 50, script: new Script().add(0)});
+    var newOutput = new Output({
+      satoshis: 50,
+      script: new Script().add(0)
+    });
     var otherOutput = new Output(newOutput.toObject());
     expectEqualOutputs(newOutput, otherOutput);
   });
@@ -47,8 +56,30 @@ describe('Output', function() {
     newOutput.setScript(Script().add(0).toBuffer());
     newOutput.inspect().should.equal('<Output (0 sats) <Script: OP_0>>');
   });
-  
+
   it('has a inspect property', function() {
     output.inspect().should.equal('<Output (0 sats) <Script: >>');
+  });
+
+  var output2 = new Output({
+    satoshis: 1100000000,
+    script: new Script('OP_2 21 0x038282263212c609d9ea2a6e3e172de238d8c39' +
+      'cabd5ac1ca10646e23fd5f51508 21 0x038282263212c609d9ea2a6e3e172de23' +
+      '8d8c39cabd5ac1ca10646e23fd5f51508 OP_2 OP_CHECKMULTISIG OP_EQUAL')
+  });
+  it('toBufferWriter', function() {
+    output2.toBufferWriter().toBuffer().toString('hex')
+      .should.equal('00ab904100000000485215038282263212c609d9ea2a6e3e172de2' +
+        '38d8c39cabd5ac1ca10646e23fd5f5150815038282263212c609d9ea2a6e3e172d' +
+        'e238d8c39cabd5ac1ca10646e23fd5f5150852ae87');
+  });
+  it('to/from JSON', function() {
+    var json = output2.toJSON();
+    var o3 = new Output(json);
+    o3.toJSON().should.equal(json);
+  });
+  it('setScript fails with invalid input', function() {
+    var out = new Output(output2.toJSON());
+    out.setScript.bind(out, 45).should.throw('Invalid argument type: script');
   });
 });
