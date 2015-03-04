8864dd3619e1fd2ef3b97b422ba342a5344e04e6
Matias Alejo Garcia <ematiu@gmail.com>
2015-03-04 01:22:26 -0300

    fix tests + dependencies + coveralls

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./lib');
+node.js: 815
+var cwd = process.cwd(); ^
+Error: ENOENT, no such file or directory
+at Function.startup.resolveArgv0(node.js: 815: 23)
+at startup(node.js: 58: 13)
+at node.js: 906: 3
