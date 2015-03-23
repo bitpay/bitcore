221431d6cb2ca1a2fdafec2a58784769b5d47f3c
Ivan Socolsky <jungans@gmail.com>
2015-03-23 18:26:47 -0300

    implement ws handshake

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var ExpressApp = require('./lib/expressapp');
+var WsApp = require('./lib/wsapp');
 
 var basePath = process.env.BWS_BASE_PATH || '/bws/api';
 var port = process.env.BWS_PORT || 3001;
@@ -11,14 +12,9 @@ var app = ExpressApp.start({
 //app.listen(port);
 
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+
+var ws = WsApp.start(server);
 
 server.listen(port);
-
-io.sockets.on('connection', function(socket) {
-  socket.emit('message', {
-    'message': 'hello world'
-  });
-});
 
 console.log('Bitcore Wallet Service running on port ' + port);
