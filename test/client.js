ef12ec8b3b7f952e1001ade1288ed683552e07ba
Ivan Socolsky <jungans@gmail.com>
2015-04-06 18:23:32 -0300

    use 'copayerName' instead of 'name' in pkr

--- test/client.js
+++ test/client.js
@@ -1298,6 +1298,7 @@ describe('client API', function() {
                 should.not.exist(err);
                 recoveryClient.getStatus(function(err, status) {
                   should.not.exist(err);
+                  _.difference(_.pluck(status.wallet.copayers, 'name'), ['creator', 'copayer 1']).length.should.equal(0);
                   recoveryClient.createAddress(function(err, addr2) {
                     should.not.exist(err);
                     should.exist(addr2);
