55d8c4156a747c5442da56a68ddbd7e4b381256e
Braydon Fuller <braydon@bitpay.com>
2015-07-06 20:24:15 -0400

    Add tests for script.toASM() and new method script.fromASM()

--- test/script/script.js
+++ test/script/script.js
@@ -159,6 +159,19 @@ describe('Script', function() {
 
   });
 
+  describe('#fromASM', function() {
+    it('should parse this known script in ASM', function() {
+      var asm = 'OP_DUP OP_HASH160 f4c03610e60ad15100929cc23da2f3a799af1725 OP_EQUALVERIFY OP_CHECKSIG';
+      var script = Script.fromASM(asm);
+      script.chunks[0].opcodenum.should.equal(Opcode.OP_DUP);
+      script.chunks[1].opcodenum.should.equal(Opcode.OP_HASH160);
+      script.chunks[2].opcodenum.should.equal(20);
+      script.chunks[2].buf.toString('hex').should.equal('f4c03610e60ad15100929cc23da2f3a799af1725');
+      script.chunks[3].opcodenum.should.equal(Opcode.OP_EQUALVERIFY);
+      script.chunks[4].opcodenum.should.equal(Opcode.OP_CHECKSIG);
+    });
+  });
+
   describe('#fromString', function() {
 
     it('should parse these known scripts', function() {
@@ -189,6 +202,11 @@ describe('Script', function() {
       script.chunks[1].buf.toString('hex').should.equal('010203');
       script.chunks[2].opcodenum.should.equal(buf[buf.length - 1]);
       script.toString().toString('hex').should.equal('OP_0 OP_PUSHDATA4 3 0x010203 OP_0');
+    });
+
+    it('should output this known script as ASM', function() {
+      var script = Script.fromHex('76a914f4c03610e60ad15100929cc23da2f3a799af172588ac');
+      script.toASM().should.equal('OP_DUP OP_HASH160 f4c03610e60ad15100929cc23da2f3a799af1725 OP_EQUALVERIFY OP_CHECKSIG');
     });
 
   });
