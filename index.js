17460095465741519808fc1bd7e128111e571255
Braydon Fuller <braydon@bitpay.com>
2015-10-15 15:58:44 -0400

    Name changes for version guard message.

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore found with versions: ' + bitcore.version +
-      ' and ' + version + '. Please make sure to require bitcore and check that submodules do' +
-      ' not also include their own bitcore dependency.';
+    var message = 'More than one instance of bitcore-lib found. ' + 
+      'Please make sure to require bitcore-lib and check that submodules do' +
+      ' not also include their own bitcore-lib dependency.';
     throw new Error(message);
   }
 };
