685df45e36a9f30576982a5b22c69933ee524402
Ivan Socolsky <jungans@gmail.com>
2015-10-15 16:30:14 -0300

    express endpoint + tests

--- test/expressapp.js
+++ test/expressapp.js
@@ -38,6 +38,22 @@ describe('ExpressApp', function() {
     describe('Routes', function() {
       var testPort = 3239;
       var testHost = 'http://127.0.0.1';
+      var httpServer;
+
+      function start(ExpressApp, done) {
+        var app = new ExpressApp();
+        httpServer = http.Server(app.app);
+
+        app.start(config, function(err) {
+          should.not.exist(err);
+          httpServer.listen(testPort);
+          done();
+        });
+      };
+
+      afterEach(function() {
+        httpServer.close();
+      });
 
       it('/v2/wallets', function(done) {
         var server = {
@@ -49,23 +65,53 @@ describe('ExpressApp', function() {
             getInstanceWithAuth: sinon.stub().callsArgWith(1, null, server),
           }
         });
-        var app = new TestExpressApp();
-        var httpServer = http.Server(app.app);
-
-        app.start(config, function(err) {
-          should.not.exist(err);
-          httpServer.listen(testPort);
+        start(TestExpressApp, function() {
           var requestOptions = {
-            url: testHost + ':' + testPort + config.basePath + '/v1/wallets',
+            url: testHost + ':' + testPort + config.basePath + '/v2/wallets',
             headers: {
               'x-identity': 'identity',
               'x-signature': 'signature'
             }
           };
-          request(requestOptions, function(err, response, body){
+          request(requestOptions, function(err, response, body) {
             should.not.exist(err);
             response.statusCode.should.equal(200);
             body.should.equal('{}');
+            done();
+          });
+        });
+      });
+
+      it('/v1/notifications', function(done) {
+        var clock = sinon.useFakeTimers(1234000, 'Date');
+
+        var server = {
+          getNotifications: sinon.stub().callsArgWith(1, null, {})
+        };
+        var TestExpressApp = proxyquire('../lib/expressapp', {
+          './server': {
+            initialize: sinon.stub().callsArg(1),
+            getInstanceWithAuth: sinon.stub().callsArgWith(1, null, server),
+          }
+        });
+        start(TestExpressApp, function() {
+          var requestOptions = {
+            url: testHost + ':' + testPort + config.basePath + '/v1/notifications' + '?notificationId=123&minTs=0',
+            headers: {
+              'x-identity': 'identity',
+              'x-signature': 'signature'
+            }
+          };
+          request(requestOptions, function(err, response, body) {
+            should.not.exist(err);
+            response.statusCode.should.equal(200);
+            body.should.equal('{}');
+            server.getNotifications.calledWith({
+              notificationId: '123',
+              minTs: 1234000 - 60000, // override minTs argument with a hardcoded 60 seconds span
+            }).should.be.true;
+
+            clock.restore();
             done();
           });
         });
