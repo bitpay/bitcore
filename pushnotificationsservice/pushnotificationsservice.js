b7f98bb752269bcbb2bcd3c6a9c5ebf1103d5089
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-01-04 15:56:14 -0300

    refactor

--- pushnotificationsservice/pushnotificationsservice.js
+++ pushnotificationsservice/pushnotificationsservice.js
@@ -7,10 +7,10 @@ var log = require('npmlog');
 log.debug = log.verbose;
 
 var config = require('../config');
-var PushNotificationService = require('../lib/pushnotificationsservice');
+var PushNotificationsService = require('../lib/pushnotificationsservice');
 
-var PushNotificationsService = new PushNotificationsService();
-PushNotificationsService.start(config, function(err) {
+var pushNotificationsService = new PushNotificationsService();
+pushNotificationsService.start(config, function(err) {
   if (err) throw err;
 
   console.log('Push Notification Service started');
