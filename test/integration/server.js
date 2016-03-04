446f35426fd5c0756fe54193b22a23f2e54fbdbf
Ivan Socolsky <jungans@gmail.com>
2016-03-04 10:30:05 -0300

    remove explicit final fee for tx creation

--- test/integration/server.js
+++ test/integration/server.js
@@ -2742,61 +2742,6 @@ describe('Wallet service', function() {
         });
       });
 
-      it('should be able to specify the final fee', function(done) {
-        helpers.stubUtxos(server, wallet, [1, 2], function() {
-          var txOpts = {
-            outputs: [{
-              toAddress: '18PzpUFkFZE8zKWUPvfykkTxmB9oMR8qP7',
-              amount: 0.8 * 1e8,
-            }],
-            fee: 123400,
-          };
-          server.createTx(txOpts, function(err, tx) {
-            should.not.exist(err);
-            should.exist(tx);
-            tx.fee.should.equal(123400);
-            var t = tx.getBitcoreTx();
-            t.getFee().should.equal(123400);
-            done();
-          });
-        });
-      });
-
-      it('should not be able to specify both final fee & fee per kb', function(done) {
-        helpers.stubUtxos(server, wallet, [1, 2], function() {
-          var txOpts = {
-            outputs: [{
-              toAddress: '18PzpUFkFZE8zKWUPvfykkTxmB9oMR8qP7',
-              amount: 0.8 * 1e8,
-            }],
-            fee: 123400,
-            feePerKb: 123400,
-          };
-          server.createTx(txOpts, function(err, tx) {
-            should.exist(err);
-            err.message.should.contain('fee');
-            done();
-          });
-        });
-      });
-
-      it('should check explicit fee to be below max', function(done) {
-        helpers.stubUtxos(server, wallet, [1, 2], function() {
-          var txOpts = {
-            outputs: [{
-              toAddress: '18PzpUFkFZE8zKWUPvfykkTxmB9oMR8qP7',
-              amount: 0.8 * 1e8,
-            }],
-            fee: 1e8,
-          };
-          server.createTx(txOpts, function(err, tx) {
-            should.exist(err);
-            err.message.should.contain('fee');
-            done();
-          });
-        });
-      });
-
       it('should be able to publish a temporary tx proposal', function(done) {
         helpers.stubUtxos(server, wallet, [1, 2], function() {
           var txOpts = {
