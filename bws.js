d13fe55b6697ba394bbdeea938fec14f5cbedb2e
Matias Alejo Garcia <ematiu@gmail.com>
2017-04-05 12:03:45 -0300

    .

--- bws.js
+++ bws.js
@@ -51,7 +51,7 @@ function startInstance(cb) {
   var server = config.https ? serverModule.createServer(serverOpts, expressApp.app) : serverModule.Server(expressApp.app);
 
   server.on('connection', function(socket) {
-    socket.setTimeout(2 * 1000);
+    socket.setTimeout(300 * 1000);
   })
 
   expressApp.start(config, function(err) {
