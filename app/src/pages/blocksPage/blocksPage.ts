9334b13a0d6f6672ae6581a706b845038844c7a4
Darren Nelsen <darren@bitpay.com>
2017-07-25 10:00:51 -0400

    enabled search in blocksPage; removed package-lock.json from source control

--- app/src/pages/blocksPage/blocksPage.ts
+++ app/src/pages/blocksPage/blocksPage.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { Observable } from 'rxjs';
 import { Block } from '../../models';
 import { BlocksService } from '../../services';
+import { Http } from '@angular/http';
 
 @Component({
   templateUrl: './blocksPage.html'
@@ -12,8 +13,10 @@ export class BlocksPage {
 
   public title: string;
   public blocks: Observable<Block[]>;
+  public q: string;
+  public badQuery: boolean = false;
 
-  constructor(private nav: NavController, private blocksService: BlocksService) {
+  constructor(private nav: NavController, private http: Http, private blocksService: BlocksService) {
     this.nav = nav;
     this.title = 'Blocks';
     this.blocks = blocksService.latestBlocks;
@@ -24,6 +27,78 @@ export class BlocksPage {
   }
 
   public search(event) {
-    console.log(event.target.value);
+    console.log('q is', this.q);
+    let apiPrefix = 'http://insight.bitpay.com/api/';
+    this.http.get(apiPrefix + 'block/' + this.q).subscribe(
+      (data) => {
+        this.resetSearch();
+        console.log('block', data);
+        //this.router.navigate(['./block/' + q]);
+      },
+      () => {
+        this.http.get(apiPrefix + 'tx/' + this.q).subscribe(
+          (data) => {
+            this.resetSearch();
+            console.log('tx', data);
+            //this.router.navigate(['./tx/' + q]);
+          },
+          function (err) { this.reportBadQuery() }.bind(this)
+        );
+      }
+    );
+
+    /*
+    Block.get({
+      blockHash: q
+    }, function() {
+      _resetSearch();
+      $location.path('block/' + q);
+    }, function() { //block not found, search on TX
+      Transaction.get({
+        txId: q
+      }, function() {
+        _resetSearch();
+        $location.path('tx/' + q);
+      }, function() { //tx not found, search on Address
+        Address.get({
+          addrStr: q
+        }, function() {
+          _resetSearch();
+          $location.path('address/' + q);
+        }, function() { // block by height not found
+          if (isFinite(q)) { // ensure that q is a finite number. A logical height value.
+            BlockByHeight.get({
+              blockHeight: q
+            }, function(hash) {
+              _resetSearch();
+              $location.path('/block/' + hash.blockHash);
+            }, function() { //not found, fail :(
+              $scope.loading = false;
+              _badQuery();
+            });
+          }
+          else {
+            $scope.loading = false;
+            _badQuery();
+          }
+        });
+      });
+    });
+     */
   }
+
+  resetSearch = function() {
+    this.q = '';
+    this.loading = false;
+  };
+
+  reportBadQuery() {
+    this.badQuery = true;
+    console.log('badQuery', this.badQuery);
+
+    setTimeout(function() {
+      this.badQuery = false;
+      console.log('badQuery', this.badQuery);
+    }.bind(this), 2000);
+  };
 }
