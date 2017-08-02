ee160d9e7becaa6cdca29463040bd2905c1afb6b
tenthirtyone <Spactigus@gmail.com>
2017-08-02 17:02:01 -0400

    Added Utils, db, and updated block schema

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const FullNode = require('bcoin/lib/node/fullnode');
-const mongoose = require('mongoose');
-const config = require('./config/config.js');
+const config = require(`${__dirname}/config/config.js`);
 const logger = require('./lib/logger');
 const Block = require('./models/block');
 const Server = require('./lib/server');
-
-mongoose.connect(config.mongodb.uri, config.mongodb.options);
+const db = require('./lib/db');
+const util = require('./lib/util');
+const node = new FullNode(config.bcoin);
 
 logger.log('debug',
   'Debug mode started');
 
-const node = new FullNode(config.bcoin);
-
 (async () => {
+  logger.log('debug',
+    'Starting Full Node');
   await node.open();
   await node.connect();
 
@@ -29,38 +29,38 @@ const node = new FullNode(config.bcoin);
 })();
 
 function processBlock(block) {
-  block.hash = revHex(block.hash().toString('hex'));
+  block.hash = util.revHex(block.hash().toString('hex'));
   logger.log('debug',
     `New Block Height: ${block.height}, Hash: ${block.hash}`);
 
-  let b = new Block({
-    mainChain: true,
-    height: block.height,
+  const b = new Block({
     hash: block.hash,
+    size: block.size,
+    height: block.height,
     version: block.version,
     merkleRoot: block.merkleRoot,
+    tx: block.txs.map(tx => util.revHex(tx.hash().toString('hex'))),
     time: block.ts,
-    timeNormalized: block.ts,
     nonce: block.nonce,
-    previousBlockHash: block.prevBlock,
+    bits: block.bits,
+    difficulty: block.bits,
+    chainwork: 0,
+    confirmations: 0,
+    previousBlockHash: block.previousBlockHash,
+    nextBlockHash: 0,
+    reward: 0,
+    timeNormalized: block.ts,
+    isMainChain: true,
+    poolInfo: Object,
     transactionCount: block.txs.length,
   });
   b.save((err) => {
     if (err) {
       console.log(err);
     }
-  })
+  });
 }
 
-function revHex(hexString) {
-  let out = '';
-  for (let i = 0; i < hexString.length; i += 2) {
-    out = hexString.slice(i, i + 2) + out;
-  }
-
-  return out;
-}
-
-Server.listen(3000, function() {
+Server.listen(3000, () => {
   console.log('listening on port 3000');
-})
+});
