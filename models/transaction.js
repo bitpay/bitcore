8c5f29698007a7660e247eda2bd36247d792ec8b
tenthirtyone <Spactigus@gmail.com>
2017-08-03 11:52:23 -0400

    Input/Output moved back inside Transaction model file

--- models/transaction.js
+++ models/transaction.js
@@ -1,4 +1,20 @@
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const InputSchema = new Schema({
+  utxo: String,
+  vout: Number,
+  address: String,
+  amount: Number,
+  wallets: { type: [Schema.Types.ObjectId] },
+});
+
+const OutputSchema = new Schema({
+  address: String,
+  amount: Number,
+  vout: Number,
+  wallets: { type: [Schema.Types.ObjectId] },
+});
 
 const TransactionSchema = new Schema({
   txid: String,
@@ -7,8 +23,8 @@ const TransactionSchema = new Schema({
   blockHash: String,
   blockTime: Date,
   blockTimeNormalized: Date,
-  inputs: [Input],
-  outputs: [Output],
+  inputs: [InputSchema],
+  outputs: [OutputSchema],
   coinbase: Boolean,
   fee: Number,
   inputsProcessed: Boolean,
@@ -27,7 +43,12 @@ TransactionSchema.index({ wallets: 1 }, { sparse: true });
 TransactionSchema.index({ 'inputs.wallets': 1 }, { sparse: true });
 TransactionSchema.index({ 'outputs.wallets': 1 }, { sparse: true });
 
-const Block = mongoose.model('Transaction', TransactionSchema);
+const Transaction = mongoose.model('Transaction', TransactionSchema);
+const Input = mongoose.model('Input', InputSchema);
+const Output = mongoose.model('Output', OutputSchema);
 
-module.exports = Block;
-
+module.exports = {
+  Transaction,
+  Input,
+  Output
+};
