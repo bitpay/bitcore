4a1f51c9fe89f4fced4c56dd0b4a7f92e1c73950
Justin Langston <nitsujlangston@gmail.com>
2017-08-06 02:12:12 -0400

    * bcoin * p2p * reorgs

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-var cluster = require('cluster');
 var async = require('async');
 
 var express = require('express');
@@ -9,22 +8,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({limit: 100000000}));
 
 var storageService = require('./lib/services/storage');
-var workerService = require('./lib/services/worker');
+var p2pService = require('./lib/services/p2p');
 var syncService = require('./lib/services/sync');
 
 async.series([
   storageService.start.bind(storageService),
-  workerService.start.bind(workerService),
+  p2pService.start.bind(p2pService),
   syncService.start.bind(syncService)
 ], function(){
-  // TODO should be cluster.isWorker, or better yet a subset of workers
-  if(cluster.isMaster) {
-    var router = require('./lib/routes');
-    app.use(router);
-    var server = app.listen(3000, function() {
-      console.log('api server listening on port 3000!');
-    });
-    server.timeout = 600000;
-  }
+  var router = require('./lib/routes');
+  app.use(router);
+  var server = app.listen(3000, function() {
+    console.log('api server listening on port 3000!');
+  });
+  server.timeout = 600000;
 });
 
