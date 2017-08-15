90adbf3ece750550ebfaea79bb7dff6c03d59c84
tenthirtyone <Spactigus@gmail.com>
2017-08-14 22:10:00 -0400

    remove Block from status

--- server/lib/api/status.js
+++ server/lib/api/status.js
@@ -1,27 +1,6 @@
-const Block = require('../../models/block.js');
 const pkg = require('../../package.json');
 
 const logger = require('../logger');
-
-const MAX_BLOCKS = 200;
-
-// Not dry, in multiple APIs. Refactor to db api
-function getBlock(params, options, limit, cb) {
-  const defaultOptions = { _id: 0 };
-
-  if (!Number.isInteger(limit)) {
-    limit = MAX_BLOCKS;
-  }
-
-  Object.assign(defaultOptions, options);
-
-  Block.find(
-    params,
-    defaultOptions,
-    cb)
-    .sort({ height: -1 })
-    .limit(limit);
-}
 
 module.exports = function statusAPI(router) {
   router.get('/status', (req, res) => {
