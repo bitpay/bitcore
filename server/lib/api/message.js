1777be323c8b2ade576cf1b8c97ce883c9e92861
tenthirtyone <Spactigus@gmail.com>
2017-08-15 20:29:38 -0400

    Message verification and bitcore-message added

--- server/lib/api/message.js
+++ server/lib/api/message.js
@@ -1,10 +1,36 @@
+const Message = require('bitcore-message');
+
+function verifyMessage(req, res) {
+  const address   = req.body.address || req.query.address;
+  const signature = req.body.signature || req.query.signature;
+  const message   = req.body.message || req.query.message;
+  if (!address || !signature || !message) {
+    res.json({
+      message: 'Missing parameters (expected "address", "signature" and "message")',
+      code: 1,
+    });
+    return;
+  }
+  let valid;
+  try {
+    valid = new Message(message).verify(address, signature);
+  } catch (err) {
+    res.json({
+      message: `Unexpected error: ${err.message}`,
+      code: 1,
+    });
+    return;
+  }
+  res.json({ result: valid });
+}
+
 module.exports = function messageAPI(router) {
   router.get('/messages/verify', (req, res) => {
-    res.send('messages verify');
+    verifyMessage(req, res);
   });
 
   router.post('/messages/verify', (req, res) => {
-    res.send('post messages verify');
+    verifyMessage(req, res);
   });
 
   router.get('/utils/estimatefee', (req, res) => {
