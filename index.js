1b844080ffbd81ce6d583bf63855e30caf07ccc9
matiu <ematiu@gmail.com>
2017-09-13 10:30:54 -0300

    change versionGuard message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib found. ' +
+    var message = 'More than one instance of bitcore-lib-cash found. ' +
       'Please make sure to require bitcore-lib and check that submodules do' +
       ' not also include their own bitcore-lib dependency.';
     throw new Error(message);
