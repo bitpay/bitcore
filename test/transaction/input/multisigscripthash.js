244534512b0a3edb00652fbb34cf9fc8027462cf
matiu <ematiu@gmail.com>
2017-09-12 21:28:29 -0300

    fix tx/input tests

--- test/transaction/input/multisigscripthash.js
+++ test/transaction/input/multisigscripthash.js
@@ -21,10 +21,10 @@ describe('MultiSigScriptHashInput', function() {
   var public1 = privateKey1.publicKey;
   var public2 = privateKey2.publicKey;
   var public3 = privateKey3.publicKey;
-  var address = new Address('33zbk2aSZYdNbRsMPPt6jgy6Kq1kQreqeb');
+  var address = new Address('H8piCq1XQrr3DbkPF5YFi5VdMV2mCQEnKW');
 
   var output = {
-    address: '33zbk2aSZYdNbRsMPPt6jgy6Kq1kQreqeb',
+    address: 'H8piCq1XQrr3DbkPF5YFi5VdMV2mCQEnKW',
     txId: '66e64ef8a3b384164b78453fa8c8194de9a473ba14f89485a0e433699daec140',
     outputIndex: 0,
     script: new Script(address),
