73a8c83b5ef75889a0fdce220a30b02da3f1c5d5
Matias Alejo Garcia <ematiu@gmail.com>
2017-10-26 13:39:06 -0300

    add versionGuard tests

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var bitcore = module.exports;
 
 // module information
 bitcore.version = 'v' + require('./package.json').version;
+bitcore.versionGuard = function(version) {
+  if (version !== undefined) {
+    var message = 'More than one instance of bitcore-lib found. ' +
+      'Please make sure to require bitcore-lib and check that submodules do' +
+      ' not also include their own bitcore-lib dependency.';
+    throw new Error(message);
+  }
+};
+bitcore.versionGuard(global._bitcore);
+global._bitcore = bitcore.version;
 
 // crypto
 bitcore.crypto = {};
