8bebc4e47fbf4e320567be20bbe3d8fa70a08777
Matias Alejo Garcia <ematiu@gmail.com>
2018-01-12 16:01:22 -0300

    lint

--- test/base32.js
+++ test/base32.js
@@ -7,8 +7,6 @@ var base32 = bitcore.util.base32;
 
 describe('base32', function() {
 
-  var customnet;
-
   it('should encode 0-5', function() {
     var a = base32.encode([0,1,2,3,4,5]);
     a.should.equal('qpzry9');
@@ -17,29 +15,35 @@ describe('base32', function() {
 
   it('should encode 0-31', function() {
     var all=[];
-    for(var i = 0; i<32; i++) 
+    for(var i = 0; i<32; i++) {
       all.push(i);
+    }
     var a = base32.encode(all);
     a.should.equal('qpzry9x8gf2tvdw0s3jn54khce6mua7l');
   });
 
   it('should fail to encode 35', function() {
-    (function() {base32.encode([35])}).should.throw('Invalid Argument: value 35');
+    (function() {base32.encode([35]);}).should.throw('Invalid Argument: value 35');
   });
 
   it('should decode 0-31', function() {
     var a=  'qpzry9x8gf2tvdw0s3jn54khce6mua7lqpzry9x8gf2tvdw0s3jn54khce6mua7l';
     var all=[];
-    for(var i = 0; i<32; i++) 
+    for(var i = 0; i<32; i++) {
       all.push(i);
+    }
     all=all.concat(all);
     base32.decode(a).should.deep.equal(all);
   });
 
-
   it('should fail decode abc', function() {
     var a=  'abc';
     (function() {base32.decode(a);}).should.throw('Invalid Argument: value b');
+  });
+
+  it('should fail decode Q', function() {
+    var a=  'aqQ';
+    (function() {base32.decode(a);}).should.throw('Invalid Argument: value Q');
   });
 
 });
