4e9c9f54518fe13318c76fe2c1fdfead354d1e24
Darren Nelsen <darren@bitpay.com>
2018-02-08 17:47:59 -0500

    added rudimentary blockchain switcher

--- app/src/components/denomination/denomination.ts
+++ app/src/components/denomination/denomination.ts
@@ -9,17 +9,33 @@ import { ViewController } from 'ionic-angular';
 export class DenominationComponent {
 
   public text: string;
+  public switcherOn: boolean;
+  public currencies: any = [];
   public units: any = [];
 
   constructor(
-    public currency: CurrencyProvider,
+    public currencyProvider: CurrencyProvider,
     public viewCtrl: ViewController
   ) {
     this.text = 'Hello World';
+    this.switcherOn = true;
+    this.currencies = [
+      {
+        name: 'Bitcoin',
+        ticker: 'BTC',
+        url: 'https://insight.bitpay.com'
+      },
+      {
+        name: 'Bitcoin Cash',
+        ticker: 'BCH',
+        url: 'https://bch-insight.bitpay.com'
+      }
+    ];
+
     this.units = [
       'USD',
-      this.currency.defaultCurrency,
-      'm' + this.currency.defaultCurrency,
+      this.currencyProvider.defaultCurrency,
+      'm' + this.currencyProvider.defaultCurrency,
       'bits'
     ];
   }
@@ -28,4 +44,11 @@ export class DenominationComponent {
     this.viewCtrl.dismiss();
   }
 
+  public changeCurrency(currency: any): void {
+    console.log('selected currency is', currency.name, currency.ticker);
+    this.close();
+    let theUrl: string = currency.url;
+    console.log('theUrl is', theUrl);
+    window.location.href = theUrl;
+  }
 }
