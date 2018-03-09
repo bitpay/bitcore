3b2e65e5ade13d2862be0a2b4e1a9e6d2c8e0a4d
Darren Nelsen <darren@bitpay.com>
2018-03-09 13:48:45 -0500

    removed console logs; added jscs and jshint resource files

--- bitcore-node/index.js
+++ bitcore-node/index.js
@@ -7,7 +7,6 @@ var exec = require('child_process').exec;
 var pkg = require('../app/package.json');
 
 var InsightUI = function(options) {
-  console.log('options are', options);
   BaseService.call(this, options);
   this.apiPrefix = options.apiPrefix || 'api';
   this.routePrefix = options.routePrefix || '';
@@ -20,14 +19,12 @@ inherits(InsightUI, BaseService);
 InsightUI.prototype.start = function(callback) {
 
   var self = this;
-  console.log('pkg before', pkg);
   pkg.insightConfig.apiPrefix = self.apiPrefix;
   pkg.insightConfig.routePrefix = self.routePrefix;
-  console.log('pkg after', pkg);
-  console.log('dirname', __dirname);
 
   fs.writeFileSync(__dirname + '/../app/package.json', JSON.stringify(pkg, null, 2));
   /*
+   * TODO implement properly with this version of insight
   exec('cd ' + __dirname + '/../;' +
     ' npm run install-and-build', function(err) {
     if (err) {
