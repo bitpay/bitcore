6d6494332a9bbec798f731751a30f759e274005d
Micah Riggan <mriggan@bitpay.com>
2018-04-23 16:17:12 -0400

    Removing console.log

--- packages/bitcore-client/lib/providers/tx-provider/btc/index.js
+++ packages/bitcore-client/lib/providers/tx-provider/btc/index.js
@@ -2,7 +2,6 @@ const bitcoreLib = require('bitcore-lib');
 
 class BTCTxProvder {
   create({addresses, amount, utxos, change}) {
-    console.log(addresses, amount);
     let tx = new bitcoreLib.Transaction()
       .from(utxos)
       .to(addresses, Number(amount))
