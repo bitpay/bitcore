c2433326f9c6db0da117f43fa84c33ad8343575b
Micah Riggan <mriggan@bitpay.com>
2018-05-17 22:30:32 -0400

    Changing hash to sha256. Encryption and decryption work

--- packages/bitcore-client/lib/encryption.js
+++ packages/bitcore-client/lib/encryption.js
@@ -1,24 +1,25 @@
 const crypto = require('crypto');
 
 function encrypt(data, password, algo = 'aes-256-cbc') {
-  let password_hash = crypto
-    .createHash('md5')
-    .update(password, 'utf-8')
-    .digest('hex')
-    .toUpperCase();
-
-  let iv = new Buffer.alloc(16);
-
-  let cipher = crypto.createCipheriv(algo, password_hash, iv);
+  let password_hash = mdfHash(password);
+  let cipher = crypto.createCipher(algo, password_hash);
   let encryptedData = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
-
   return encryptedData;
 }
 
+function mdfHash(password) {
+  let password_hash = crypto
+  .createHash('sha256')
+  .update(password, 'utf-8')
+  .digest('hex')
+  .toUpperCase();
+  return password_hash;
+}
+
 function decrypt(data, password, algo = 'aes-256-cbc') {
-  const decipher = crypto.createDecipher(algo, password);
-  let decrypted = decipher.update(data, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
+  const password_hash = mdfHash(password);
+  const decipher = crypto.createDecipher(algo, password_hash);
+  let decrypted = decipher.update(data, 'hex', 'utf8') + decipher.final('utf8');
   return decrypted;
 }
 
