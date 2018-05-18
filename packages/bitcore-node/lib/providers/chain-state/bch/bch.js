aef0bfa3cd30f0b150d1367366a3d4adba88d0d3
Micah Riggan <mriggan@bitpay.com>
2018-05-18 12:42:10 -0400

    Removing lib folder

--- packages/bitcore-node/lib/providers/chain-state/bch/bch.js
+++
@@ -1,9 +0,0 @@
-const BTCStateProvider = require('../btc/btc');
-
-class BCHStateProvider extends BTCStateProvider {
-  constructor() {
-    super('BCH');
-  }
-}
-
-module.exports = BCHStateProvider;
