5aa8da7a47eda428ab6f207606dcf9c7b6fb537f
Micah Riggan <mriggan@bitpay.com>
2018-05-17 23:06:56 -0400

    Switching to IV based encrypt and decrypt

--- packages/bitcore-client/lib/encryption.js
+++ packages/bitcore-client/lib/encryption.js
@@ -1,25 +1,29 @@
 const crypto = require('crypto');
 
 function encrypt(data, password, algo = 'aes-256-cbc') {
-  let password_hash = mdfHash(password);
-  let cipher = crypto.createCipher(algo, password_hash);
-  let encryptedData = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
-  return encryptedData;
+  const password_hash = shaHash(password).slice(0, 32);
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algo, password_hash, iv);
+  const encData = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
+  return iv.toString('hex') + ':' + encData;
 }
 
-function mdfHash(password) {
+function shaHash(password) {
   let password_hash = crypto
-  .createHash('sha256')
-  .update(password, 'utf-8')
-  .digest('hex')
-  .toUpperCase();
+    .createHash('sha256')
+    .update(password, 'utf-8')
+    .digest('hex')
+    .toUpperCase();
   return password_hash;
 }
 
 function decrypt(data, password, algo = 'aes-256-cbc') {
-  const password_hash = mdfHash(password);
-  const decipher = crypto.createDecipher(algo, password_hash);
-  let decrypted = decipher.update(data, 'hex', 'utf8') + decipher.final('utf8');
+  const password_hash = shaHash(password).slice(0, 32);
+  const [iv, encData] = data.split(':');
+  const ivBuffer = Buffer.from(iv, 'hex');
+  const decipher = crypto.createDecipheriv(algo, password_hash, ivBuffer);
+  const decrypted =
+    decipher.update(encData, 'hex', 'utf8') + decipher.final('utf8');
   return decrypted;
 }
 
