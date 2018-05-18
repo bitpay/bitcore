7758695c70887e62b35a501c9f041d0adfaea509
Micah Riggan <mriggan@bitpay.com>
2018-05-18 09:48:13 -0400

    WIP Converting to a buffer

--- packages/bitcore-client/lib/encryption.js
+++ packages/bitcore-client/lib/encryption.js
@@ -1,11 +1,11 @@
 const crypto = require('crypto');
 
 function encrypt(data, password, algo = 'aes-256-cbc') {
-  const password_hash = shaHash(password).slice(0, 32);
+  const password_hash = Buffer.from(shaHash(password));
   const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv(algo, password_hash, iv);
   const encData = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
-  return iv.toString('hex') + ':' + encData;
+  return iv.toString() + ':' + encData;
 }
 
 function shaHash(password) {
@@ -18,7 +18,7 @@ function shaHash(password) {
 }
 
 function decrypt(data, password, algo = 'aes-256-cbc') {
-  const password_hash = shaHash(password).slice(0, 32);
+  const password_hash = Buffer.form(shaHash(password));
   const [iv, encData] = data.split(':');
   const ivBuffer = Buffer.from(iv, 'hex');
   const decipher = crypto.createDecipheriv(algo, password_hash, ivBuffer);
