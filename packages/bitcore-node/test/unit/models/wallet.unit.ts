4b7ce6ae12e273833d221d77ef04da7f46b6d0a7
Harshit Joshi <harshit@bitpay.com>
2018-05-24 19:24:14 -0400

    WIP: add wallet unit tests

--- packages/bitcore-node/test/unit/models/wallet.unit.ts
+++ packages/bitcore-node/test/unit/models/wallet.unit.ts
@@ -1,7 +1,59 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { WalletModel, IWalletModel } from '../../../src/models/wallet';
+import { BlockModel } from '../../../src/models/block';
+import { WalletAddressModel } from '../../../src/models/walletAddress';
 
-describe('Wallet Model', function(){
-    it('should have a test which runs', function(){
-        expect(true).to.equal(true);
+describe('Wallet Model', function () {
+
+  describe('_apiTransform', () => {
+    it('should return the transform object with wallet info', () => {
+      let wallet = {
+        name: 'Wallet1',
+        singleAddress: true,
+        pubKey: 'xpub661MyMwAqRbcFa63vSTa3vmRiVWbpLWhgUsyvjfMFP7ePR5osC1rtPUkgJrB94V1YEQathfWLm9U5zaZttYPDPWhASwJGUvYvPGtofqnTGN',
+        path: 'm/44\'/0\'/0\''
+      }
+
+      const result = WalletModel._apiTransform(new WalletModel(wallet), {
+        object: false
+      });
+      const parseResult = JSON.parse(result);
+
+      expect(parseResult.name).to.be.equal(wallet.name);
+      expect(parseResult.pubKey).to.be.equal(wallet.pubKey);
+
     });
+  });
+
+  describe('updateCoins', () => {
+    let sandbox;
+    beforeEach(() => {
+      sandbox = sinon.sandbox.create();
+    });
+    afterEach(() => {
+      sandbox.restore();
+    });
+    it('should return wallet address model update coins', async () => {
+      sandbox.stub(WalletAddressModel, 'find').returns(['test', 'run']);
+      let walletAddressModelSpy = sandbox.stub(WalletAddressModel, 'updateCoins').returns({
+        wallet: sandbox.stub().returnsThis(),
+        addresses: sandbox.stub().returnsThis()
+      });
+
+      let wallet = {
+        name: 'Wallet1',
+        singleAddress: true,
+        pubKey: 'xpub661MyMwAqRbcFa63vSTa3vmRiVWbpLWhgUsyvjfMFP7ePR5osC1rtPUkgJrB94V1YEQathfWLm9U5zaZttYPDPWhASwJGUvYvPGtofqnTGN',
+        path: 'm/44\'/0\'/0\'',
+        chain: 'BTC',
+        network: 'regtest'
+      } as IWalletModel;
+
+      await WalletModel.updateCoins(wallet);
+      expect(walletAddressModelSpy.calledOnce).to.be.true;
+
+    });
+  });
 });
+
