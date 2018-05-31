c2b1907087b934c3b134db1df41b7e0fcb5a0220
Michael Eden <themichaeleden@gmail.com>
2018-05-31 19:19:41 -0400

    Use existing RPC library in p2p end-to-end test.

--- packages/bitcore-node/test/integration/services/p2p.unit.ts
+++ packages/bitcore-node/test/integration/services/p2p.unit.ts
@@ -1,5 +1,4 @@
 import logger from '../../../src/logger';
-import fetch from 'node-fetch';
 import config from '../../../src/config';
 import { expect } from 'chai';
 import { build, P2pRunner, P2pEvents } from '../../../src/services/p2p';
@@ -7,12 +6,15 @@ import { BlockModel } from '../../../src/models/block';
 import { TransactionModel } from '../../../src/models/transaction';
 import { Storage } from '../../../src/services/storage';
 import { sleep } from '../../../src/utils/async';
+import { RPC } from '../../../src/rpc';
 
 describe('P2P Service', () => {
   it('should sync to latest and listen for new blocks', async function() {
     this.timeout(15000);
     await Storage.start({});
 
+    const creds = config.chains.BTC.regtest.rpc;
+    const rpc = new RPC(creds.username, creds.password, creds.host, creds.port);
     const chain = 'BTC';
     const network = 'regtest';
     const service = build(chain, Object.assign(config.chains.BTC.regtest, {
@@ -23,7 +25,7 @@ describe('P2P Service', () => {
     }));
 
     // add some blocks to sync on startup
-    await btcrpc('generate', [10]);
+    await rpc.generate(10);
 
     // start and connect the service
     const runner = new P2pRunner(chain, network, BlockModel, TransactionModel, service);
@@ -33,11 +35,11 @@ describe('P2P Service', () => {
     await new Promise(r => runner.events.once(P2pEvents.SYNC_COMPLETE, r));
 
     // check DB that all the new blocks are synced correctly
-    await verify(10);
+    await verify(rpc, 10);
 
     // slowly add some blocks
     for (let i = 0; i < 5; i += 1) {
-      await btcrpc('generate', [1]);
+      await rpc.generate(1);
       await sleep(100);
     }
 
@@ -49,54 +51,23 @@ describe('P2P Service', () => {
       }
       recent = pair.block.hash;
     }));
-    const added = (await btcrpc('generate', [1]))[0];
+    const added = (await rpc.generate(1))[0];
     if (added !== recent) {
       recent = added;
       await stored;
     }
 
     // check that blocks got updated when not explicitly syncing
-    await verify(16);
+    await verify(rpc, 16);
   });
 });
 
-const RPC_CONFIG = config.chains.BTC.regtest.rpc;
-
-async function btcrpc(method: string, params: any[] = [], config: {
-  host: string,
-  port: number,
-  username: string,
-  password: string,
-} = RPC_CONFIG) {
-  const auth = Buffer.from(`${config.username}:${config.password}`).toString('base64');
-  return fetch(`http://${config.host}:${config.port}`, {
-    method: 'POST',
-    body: JSON.stringify({
-      jsonrpc: '1.0',
-      id: Date.now().toString(),
-      method,
-      params,
-    }),
-    headers: {
-      'Content-Type': 'text/plain',
-      'Authorization': `Basic ${auth}`,
-    }
-  })
-  .then(res => res.json())
-  .then(res => {
-    if (res.error) {
-      throw new Error(res.error);
-    }
-    return res.result;
-  });
-}
-
-async function verify(tail: number) {
+async function verify(rpc: RPC, tail: number) {
   const myTip = await BlockModel.getLocalTip({
     chain: 'BTC',
     network: 'regtest',
   });
-  const poolTip = await btcrpc('getbestblockhash');
+  const poolTip = await rpc.bestBlockHashAsync();
 
   // check that we're at the right tip
   expect(myTip.hash, 'local tip').to.equal(poolTip);
@@ -108,7 +79,7 @@ async function verify(tail: number) {
     logger.info(`Checking block ${hash}`);
 
     // check block is correct
-    const truth = await btcrpc('getblock', [ hash ]);
+    const truth = await rpc.blockAsync(hash);
     const ours = await BlockModel.find({ hash });
     expect(ours.length, 'number of blocks').to.equal(1);
     expect(ours[0].previousBlockHash, 'previous block hash').to.equal(truth.previousblockhash);
