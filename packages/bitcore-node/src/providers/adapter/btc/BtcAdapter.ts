be7b5959e588354954dde01539e0acf3e02e3901
Michael Eden <themichaeleden@gmail.com>
2018-06-01 15:30:29 -0400

    Updated adapters with intermediate representation.

--- packages/bitcore-node/src/providers/adapter/btc/BtcAdapter.ts
+++ packages/bitcore-node/src/providers/adapter/btc/BtcAdapter.ts
@@ -1,52 +1,34 @@
-import { IBlock } from '../../../models/block';
-import { ITransaction } from '../../../models/transaction';
-import { IChainAdapter } from '../../../types/namespaces/ChainAdapter';
 import { Bitcoin } from "../../../types/namespaces/Bitcoin";
-import { ChainNetwork } from '../../../types/ChainNetwork';
+import {
+  IChainAdapter,
+  CoreBlock,
+  ChainInfo,
+  CoreTransaction,
+} from '../../../types/namespaces/ChainAdapter';
 
 export class BTCAdapter implements IChainAdapter<Bitcoin.Block, Bitcoin.Transaction> {
-  convertBlock(chainnet: ChainNetwork, block: Bitcoin.Block): IBlock {
-    let header = block.header.toObject();
-    const converted: IBlock = {
-      chain: chainnet.chain,
-      network: chainnet.network,
-      height,
-      hash: block.hash,
-      previousBlockHash: header.prevHash,
-      merkleRoot: header.merkleRoot,
-      version: Number(header.version),
-      bits: Number(header.bits),
-      nonce: Number(header.nonce),
-      time: new Date(header.time * 1000),
-      timeNormalized: new Date(header.time * 1000),
-      transactionCount: block.transactions.length,
+  convertBlock(info: ChainInfo, block: Bitcoin.Block): CoreBlock {
+    const header = block.header.toObject();
+    return Object.assign(info, {
+      header,
       size: block.toBuffer().length,
       reward: block.transactions[0].outputAmount,
-      nextBlockHash: '',
-      processed: false
-    };
-    return converted;
+      transactions: block.transactions.map(tx => this.convertTx(info, tx)),
+    });
   }
 
-  convertTx(chainnet: ChainNetwork, transaction: Bitcoin.Transaction, block?: Bitcoin.Block) {
-    let convertedBlock;
-    if (block) {
-      convertedBlock = this.convertBlock(block);
-    }
-    const converted: ITransaction = {
-      chain: chainnet.chain,
-      network: chainnet.network,
-      txid: transaction.hash,
+  convertTx(info: ChainInfo, transaction: Bitcoin.Transaction): CoreTransaction {
+    return Object.assign(info, {
+      hash: transaction.hash,
       coinbase: transaction.isCoinbase(),
-      fee: 0,
-      size: transaction.toBuffer().length,
-      locktime: transaction.nLockTime,
-      wallets: [],
-      blockHash: convertedBlock? convertedBlock.hash : undefined,
-      blockTime: convertedBlock? convertedBlock.blockTime : Date.now(),
-      blockHeight: convertedBlock? convertedBlock.height : -1,
-      blockTimeNormalized: convertedBlock? convertedBlock.blockTimeNormalized : Date.now(),
-    };
-    return converted;
+      nLockTime: transaction.nLockTime,
+      inputs: transaction.inputs.map(input => input.toObject()),
+      outputs: transaction.outputs.map(out => {
+        return {
+          satoshis: out.satoshis,
+          script: out.script.toBuffer(),
+        };
+      }),
+    });
   }
 }
--- packages/bitcore-node/src/types/namespaces/ChainAdapter.ts
+++ packages/bitcore-node/src/types/namespaces/ChainAdapter.ts
@@ -1,8 +1,42 @@
-import { IBlock } from "../../models/block";
-import { ITransaction } from "../../models/transaction";
-import { ChainNetwork } from "../ChainNetwork";
-
 export interface IChainAdapter<B, T> {
-  convertBlock(chainnet: ChainNetwork, block: B): IBlock;
-  convertTx(chainnet: ChainNetwork, transaction: T, block?: B): ITransaction;
+  convertBlock(info: ChainInfo, block: B): CoreBlock;
+  convertTx(info: ChainInfo, transaction: T, block?: B): CoreTransaction;
 }
+
+export type ChainInfo = {
+  chain: string;
+  network: string;
+  parent?: {
+    chain: string;
+    height: number;
+  };
+}
+
+export type CoreBlock = ChainInfo & {
+  size: number;
+  reward: number;
+  header: {
+    hash: string;
+    prevHash: string;
+    version: string;
+    time: number;
+    merkleRoot: string;
+    bits: string;
+    nonce: string;
+  };
+  transactions: CoreTransaction[];
+}
+
+export type CoreTransaction = ChainInfo & {
+  hash: string;
+  coinbase: boolean;
+  nLockTime: number;
+  inputs: {
+    prevTxId: string;
+    outputIndex: number;
+  }[];
+  outputs: {
+    script: Buffer;
+    satoshis: number;
+  }[];
+};
