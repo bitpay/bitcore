acf7e1904d3ce9da4a763dea9706d4d2dd2f27d4
Michael Eden <themichaeleden@gmail.com>
2018-06-03 09:52:13 -0400

    Fixed and cleaned up modified block model unit tests.

--- packages/bitcore-node/test/data/test-tx.ts
+++ packages/bitcore-node/test/data/test-tx.ts
@@ -1,3 +1,5 @@
+import { CoreTransaction } from "../../src/types/namespaces/ChainAdapter";
+
 export const TEST_TX = {
   hash: '08e23107e8449f02568d37d37aa76e840e55bbb5f100ed8ad257af303db88c08',
   _hash: '08e23107e8449f02568d37d37aa76e840e55bbb5f100ed8ad257af303db88c08',
@@ -37,4 +39,16 @@ export const TEST_TX_3 = {
   outputs: [],
   nLockTime: 0,
   toBuffer: () => Buffer.from('')
+};
+
+export const TEST_CORE_TX: CoreTransaction = {
+  chain: 'BTC',
+  network: 'regtest',
+  parent: undefined,
+  hash: '08e23107e8449f02568d37d37aa76e840e55bbb5f100ed8ad257af303db88c08',
+  size: 0,
+  coinbase: true,
+  nLockTime: 0,
+  inputs: [],
+  outputs: []
 };
