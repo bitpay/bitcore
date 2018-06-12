1c2a129d9a8923fd629044ffb07729da75d6684a
Darren Nelsen <darren@bitpay.com>
2018-06-12 14:16:12 -0400

    adds template for ionic config to be used in docker

--- app/.docker/scripts/replace.sh
+++ app/.docker/scripts/replace.sh
@@ -5,6 +5,8 @@ echo "run directory is ${RUN_DIR}"
 echo "running in..."
 cd ${RUN_DIR}
 pwd
+echo "copying ionic.config.json template..."
+cp .docker/templates/ionic.config.json .
 npm install
 npm rebuild node-sass
 npm install -g replace
