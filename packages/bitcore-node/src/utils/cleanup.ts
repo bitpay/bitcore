f33eb65c24d3624f41cf63b6b8c9f4de36dfbb90
Micah Riggan <mriggan@bitpay.com>
2018-07-17 15:03:02 -0400

    Moving to transform stream

--- packages/bitcore-node/src/utils/cleanup.ts
+++ packages/bitcore-node/src/utils/cleanup.ts
@@ -1,28 +1,37 @@
 import { BlockModel } from '../models/block';
+import { Transform } from 'stream';
 import { TransactionModel } from '../models/transaction';
 import { CoinModel } from '../models/coin';
 import { Storage } from '../services/storage';
 
+class CleanupTransform extends Transform {
+  constructor() {
+    super({ objectMode: true });
+  }
+
+  async _transform(block, _, done) {
+    console.log(`Finding transactions for block ${block.hash}`);
+    const txs = await TransactionModel.collection.find({ blockHash: block.hash }).toArray();
+    for (let tx of txs) {
+      console.log(`Finding coins for tx ${tx.txid}`);
+      let mints = await CoinModel.collection.find({ mintTxid: tx.txid }).toArray();
+      for (let mint of mints) {
+        if (mint.mintHeight != block.height && block.height > mint.mintHeight) {
+          console.log(mint);
+        }
+      }
+    }
+    done();
+  }
+}
+
 Storage.start({})
   .then(() => {
-    BlockModel.collection
-      .find({})
-      .sort({ height: -1 })
-      .stream({
-        transform: async block => {
-          console.log(`Finding transactions for block ${block.hash}`);
-          const txs = await TransactionModel.collection.find({ blockHash: block.hash }).toArray();
-          for (let tx of txs) {
-            console.log(`Finding coins for tx ${tx.txid}`);
-            let mints = await CoinModel.collection.find({ mintTxid: tx.txid }).toArray();
-            for (let mint of mints) {
-              if (mint.mintHeight != block.height && block.height > mint.mintHeight) {
-                console.log(mint);
-              }
-            }
-          }
-        }
-      });
+    let cursor = BlockModel.collection.find({});
+    cursor.addCursorFlag('noCursorTimeout', true);
+    cursor.pipe(new CleanupTransform());
+    cursor.on('data', console.log);
+    cursor.on('end', () => console.log('done'));
   })
   .catch(e => {
     console.error('fatal', e);
