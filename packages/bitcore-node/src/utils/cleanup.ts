0f29db2ba94916f8433f09af05b716d87f235573
Micah Riggan <mriggan@bitpay.com>
2018-07-17 14:46:57 -0400

    Adding better messages

--- packages/bitcore-node/src/utils/cleanup.ts
+++ packages/bitcore-node/src/utils/cleanup.ts
@@ -3,21 +3,27 @@ import { TransactionModel } from '../models/transaction';
 import { CoinModel } from '../models/coin';
 import { Storage } from '../services/storage';
 
-Storage.start({}).then(() => {
-  BlockModel.collection
-    .find({})
-    .sort({ height: -1 })
-    .stream({
-      transform: async block => {
-        const txs = await TransactionModel.collection.find({ blockHash: block.hash }).toArray();
-        for(let tx of txs) {
-          let mints = await CoinModel.collection.find({ mintTxid: tx.txid }).toArray();
-          for (let mint of mints) {
-            if (mint.mintHeight != block.height && block.height > mint.mintHeight) {
-              console.log(mint);
+Storage.start({})
+  .then(() => {
+    BlockModel.collection
+      .find({})
+      .sort({ height: -1 })
+      .stream({
+        transform: async block => {
+          console.log(`Finding transactions for block ${block.hash}`);
+          const txs = await TransactionModel.collection.find({ blockHash: block.hash }).toArray();
+          for (let tx of txs) {
+            console.log(`Finding coins for tx ${tx.txid}`);
+            let mints = await CoinModel.collection.find({ mintTxid: tx.txid }).toArray();
+            for (let mint of mints) {
+              if (mint.mintHeight != block.height && block.height > mint.mintHeight) {
+                console.log(mint);
+              }
             }
           }
         }
-      }
-    })
-});
+      });
+  })
+  .catch(e => {
+    console.error('fatal', e);
+  });
