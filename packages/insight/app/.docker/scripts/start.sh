bad252e3da71c9dd895bbf06983682e9bc257a03
Micah Riggan <mriggan@bitpay.com>
2018-08-14 14:51:00 -0400

    Setting CHAIN in docker files

---
+++ packages/insight/app/.docker/scripts/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "current directory is $PWD"
+echo "run directory is ${RUN_DIR}"
+echo "running in..."
+cd ${RUN_DIR}
+pwd
+echo "copying ionic.config.json template..."
+cp .docker/templates/ionic.config.json .
+npm install
+npm rebuild node-sass
+npm install -g replace
+npm start
