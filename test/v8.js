ea605840662275fcb36a2d2095abb7e484381de9
Matias Alejo Garcia <ematiu@gmail.com>
2018-10-11 10:34:21 -0300

    fix partial JSON handling

---
+++ test/v8.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var _ = require('lodash');
+var chai = require('chai');
+var sinon = require('sinon');
+var should = chai.should();
+var V8 = require('../lib/blockchainexplorers/v8.js');
+var B = require('bitcore-lib-cash');
+const { Readable } = require('stream');
+
+
+
+class Client {
+  constructor() {
+  }
+  listTransactions () {
+  }
+};
+
+var t = (new Date).toISOString();
+var external = '11234';
+var txs = [{
+  id: 1,
+  txid: 'txid1',
+  confirmations: 1,
+  blockTime: t,
+  size: 226,
+  category: 'send',
+  toAddress: external,
+  satoshis: 0.5e8,
+},
+{
+  id: 2,
+  txid: 'txid2',
+  confirmations: 1,
+  category: 'send',
+  blockTime: t,
+  satoshis: 0.3e8,
+  toAddress: external,
+},
+{
+  id: 3,
+  txid: 'txid3',
+  confirmations: 1,
+  blockTime: t,
+  satoshis: 5460,
+  category: 'fee',
+},
+]; 
+
+
+describe('V8', () => {
+  var wallet={};
+
+  wallet.beAuthPrivateKey= new B.PrivateKey();
+
+  describe('#listTransactions', () => {
+    it('should handle partial json results', (done) => {
+
+      class PartialJson extends Client {
+        listTransactions(opts) {
+          class MyReadable extends Readable {
+            constructor(options) {
+              super(options);
+              var txStr = JSON.stringify(txs);
+              this.push(txStr.substr(0,10));
+              this.push(txStr.substr(10));
+              this.push(null);
+              }
+          };
+
+          return new MyReadable;
+        };
+      };
+      
+      var be = new V8({
+        coin: 'btc',
+        network: 'livenet',
+        url: 'http://dummy/',
+        apiPrefix: 'dummyPath',
+        userAgent: 'testAgent',
+        addressFormat: null,
+        client: PartialJson,
+      });
+
+      be.getTransactions(wallet, 0, 10, (err, txs) => {
+        should.not.exist(err);
+        should.exist(txs);
+        txs.length.should.equal(3);
+        return done();
+      });
+    });
+  });
+});
