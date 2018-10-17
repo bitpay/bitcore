6ce6f33185980bcd69376f851a233bb32aa947c9
Matias Alejo Garcia <ematiu@gmail.com>
2018-10-17 09:44:40 -0300

    scan refactor to support steps

--- test/v8.js
+++ test/v8.js
@@ -56,7 +56,6 @@ describe('V8', () => {
 
   describe('#listTransactions', () => {
     it('should handle partial json results', (done) => {
-
       class PartialJson extends Client {
         listTransactions(opts) {
           class MyReadable extends Readable {
@@ -90,5 +89,43 @@ describe('V8', () => {
         return done();
       });
     });
+
+    it('should handle partial jsonline results', (done) => {
+      class PartialJsonL extends Client {
+        listTransactions(opts) {
+          class MyReadable extends Readable {
+            constructor(options) {
+              super(options);
+              var txStr = '{ "id": 1, "txid": "txid1", "confirmations": 1, "blockTime": "'+ 
+                t + '", "size": 226, "category": "send", "toAddress": "'+
+                external +'", "satoshis": 0.5e8 } { "id": 2, "txid": "txid2", "confirmations": 1, "category": "send", "blockTime": "'+
+                t + '", "satoshis": 0.3e8, "toAddress": "'+external + '"}:"'; 
+console.log('[v8.js.99:txStr:]',txStr); //TODO
+              this.push(txStr.substr(0,10));
+              this.push(txStr.substr(10));
+              this.push(null);
+              }
+          };
+
+          return new MyReadable;
+        };
+      };
+      var be2 = new V8({
+        coin: 'btc',
+        network: 'livenet',
+        url: 'http://dummy/',
+        apiPrefix: 'dummyPath',
+        userAgent: 'testAgent',
+        addressFormat: null,
+        client: PartialJsonL,
+      });
+      be2.getTransactions(wallet, 0, 10, (err, txs) => {
+        should.not.exist(err);
+        should.exist(txs);
+        txs.length.should.equal(2);
+        return done();
+      });
+    });
+
   });
 });
