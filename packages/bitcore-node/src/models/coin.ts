3f4afb04567676dc7ed99356ea93aacdde3f0dfd
Justin Langston <justin@bitpay.com>
2018-11-13 16:16:36 -0500

    cover that query

--- packages/bitcore-node/src/models/coin.ts
+++ packages/bitcore-node/src/models/coin.ts
@@ -66,7 +66,7 @@ class Coin extends BaseModel<ICoin> {
     this.collection.createIndex({ chain: 1, network: 1, mintHeight: 1 }, { background: true });
     this.collection.createIndex({ spentTxid: 1 }, { background: true, sparse: true });
     this.collection.createIndex({ chain: 1, network: 1, spentHeight: 1 }, { background: true });
-    this.collection.createIndex({ wallets: 1, spentHeight: 1 }, { background: true, partialFilterExpression: { 'wallets.0': { $exists: true } } });
+    this.collection.createIndex({ wallets: 1, spentHeight: 1, value: 1 }, { background: true, partialFilterExpression: { 'wallets.0': { $exists: true } } });
   }
 
   getBalance(params: { query: any }) {
