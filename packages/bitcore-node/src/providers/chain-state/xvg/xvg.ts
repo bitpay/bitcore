ed7c6f75732e043546a95a72888850ffba95acd4
Swen van Zanten <info@hdsign.nl>
2018-12-20 16:40:49 +0100

    Remove chain provider fee method

--- packages/bitcore-node/src/providers/chain-state/xvg/xvg.ts
+++ packages/bitcore-node/src/providers/chain-state/xvg/xvg.ts
@@ -1,13 +1,7 @@
 import { BTCStateProvider } from '../btc/btc';
-import { CSP } from '../../../types/namespaces/ChainStateProvider';
 
 export class XVGStateProvider extends BTCStateProvider {
   constructor(chain: string = 'XVG') {
     super(chain);
-  }
-
-  async getFee(params: CSP.GetEstimateSmartFeeParams) {
-    const { chain, network, target } = params;
-    return { feerate: await this.getRPC(chain, network).getEstimateFee(Number(target)) };
   }
 }
