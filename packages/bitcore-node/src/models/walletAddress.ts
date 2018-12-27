4da3c0438918a68ba80c51889fe3d055405a4ab5
Justin Langston <justin@bitpay.com>
2018-12-27 15:07:48 -0500

    wip

--- packages/bitcore-node/src/models/walletAddress.ts
+++ packages/bitcore-node/src/models/walletAddress.ts
@@ -1,10 +1,11 @@
-import { CoinStorage, ICoin } from './coin';
+import { CoinStorage } from './coin';
 import { TransformOptions } from '../types/TransformOptions';
 import { ObjectID } from 'mongodb';
 import { BaseModel } from './base';
 import { IWallet } from './wallet';
 import { TransactionStorage } from './transaction';
 import { StorageService } from '../services/storage';
+import { Writable } from 'stream';
 
 export type IWalletAddress = {
   wallet: ObjectID;
@@ -32,95 +33,77 @@ export class WalletAddressModel extends BaseModel<IWalletAddress> {
     return JSON.stringify(transform);
   }
 
-  getUpdateCoinsObj(params: { wallet: IWallet; address: string }) {
-    const { wallet, address } = params;
-    const { chain, network } = wallet;
-
-    return {
-      updateMany: {
-        filter: { chain, network, address },
-        update: {
-          $addToSet: { wallets: wallet._id }
-        }
-      }
-    };
-  }
-
-  getUpdateWalletAddressObj(params: { wallet: IWallet; address: string }) {
-    const { wallet, address } = params;
-    const { chain, network } = wallet;
-
-    return {
-      updateOne: {
-        filter: { wallet: wallet._id, address: address },
-        update: { wallet: wallet._id, address: address, chain, network },
-        upsert: true
-      }
-    };
-  }
-
   async updateCoins(params: { wallet: IWallet; addresses: string[] }) {
     const { wallet, addresses } = params;
     const { chain, network } = wallet;
 
+    const addAddresses = addresses => {
+      return WalletAddressStorage.collection.updateMany(
+        { wallet: wallet._id, address: { $in: addresses } },
+        { $set: { wallet: wallet._id, chain, network } },
+        { upsert: true }
+      );
+    };
+    const tagCoins = addresses => {
+      return CoinStorage.collection.updateMany(
+        { chain, network, address: { $in: addresses } },
+        { $addToSet: { wallets: wallet._id } }
+      );
+    };
+
+    const processBatch = batch => {
+      return Promise.all([addAddresses(batch), tagCoins(batch)]);
+    };
+
+    class TxUpdaterStream extends Writable {
+      txids: { [key:string] : boolean};
+      constructor() {
+        super({ objectMode: true, highWaterMark: 4 });
+        this.txids = {};
+      }
+      async _write(coin, _, callback) {
+        if (!this.txids[coin.mintTxid]) {
+          await TransactionStorage.collection.updateMany(
+            { txid: coin.mintTxid, network, chain },
+            { $addToSet: { wallets: wallet._id } }
+          );
+        }
+        this.txids[coin.mintTxid] = true;
+        if (coin.spentTxid && !this.txids[coin.spentTxid]) {
+          await TransactionStorage.collection.updateMany(
+            { txid: coin.spentTxid, network, chain },
+            { $addToSet: { wallets: wallet._id } }
+          );
+        }
+        this.txids[coin.spentTxid] = true;
+        callback();
+      }
+    }
+
     return new Promise(async resolve => {
       let batch = new Array<string>();
-      const AddAddresses = addresses => {
-        return WalletAddressStorage.collection.updateMany(
-          { wallet: wallet._id, address: { $in: addresses } },
-          { $set: { wallet: wallet._id, chain, network } },
-          { upsert: true }
-        );
-      };
-      const UpdateCoins = addresses => {
-        return CoinStorage.collection.updateMany(
-          { chain, network, address: { $in: addresses } },
-          { $addToSet: { wallets: wallet._id } }
-        );
-      };
-
-      const ProcessBatch = batch => {
-        return Promise.all([AddAddresses(batch), UpdateCoins(batch)]);
-      };
-
+      
       for (const address of addresses) {
         batch.push(address);
         if (batch.length > 10000) {
-          await ProcessBatch(batch);
+          await processBatch(batch);
           batch = new Array<string>();
         }
       }
       if (batch.length > 0) {
-        await ProcessBatch(batch);
+        await processBatch(batch);
         batch = new Array<string>();
       }
+
+      const txUpdaterStream = new TxUpdaterStream();
 
       let coinStream = CoinStorage.collection
         .find({ wallets: wallet._id, 'wallets.0': { $exists: true } })
         .project({ spentTxid: 1, mintTxid: 1 })
         .addCursorFlag('noCursorTimeout', true);
-      let txids = {};
-      coinStream.on('data', (coin: ICoin) => {
-        coinStream.pause();
-        if (!txids[coin.mintTxid]) {
-          TransactionStorage.collection.updateMany(
-            { txid: coin.mintTxid, network, chain },
-            { $addToSet: { wallets: wallet._id } }
-          );
-        }
-        txids[coin.mintTxid] = true;
-        if (coin.spentTxid && !txids[coin.spentTxid]) {
-          TransactionStorage.collection.updateMany(
-            { txid: coin.spentTxid, network, chain },
-            { $addToSet: { wallets: wallet._id } }
-          );
-        }
-        txids[coin.spentTxid] = true;
-        coinStream.resume();
-      });
-      coinStream.on('end', async () => {
-        resolve();
-      });
+
+      coinStream.pipe(txUpdaterStream);
+      coinStream.on('end', resolve);
     });
   }
 }
