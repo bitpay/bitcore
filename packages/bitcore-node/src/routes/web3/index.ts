b3d6575966d51d8c0fdd4b56cf7925fe2ea90962
Micah Riggan <mriggan@bitpay.com>
2019-01-04 11:25:17 -0500

    Adding error handling

--- packages/bitcore-node/src/routes/web3/index.ts
+++ packages/bitcore-node/src/routes/web3/index.ts
@@ -5,22 +5,24 @@ import request from 'request';
 export function Web3Proxy(req: express.Request, res: express.Response) {
   const { chain, network } = req.params;
   const chainConfig = Config.chainConfig({ chain, network });
-  const { host, port } = chainConfig.rpc;
-  const url = `http://${host}:${port}`;
-  let requestStream;
-  if (req.body.jsonrpc) {
-    const options = {
-      uri: url,
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      method: req.method,
-      body: JSON.stringify(req.body),
-      json: true
-    };
-    requestStream = request(options);
+  if (chainConfig && chainConfig.rpc) {
+    const { host, port } = chainConfig.rpc;
+    const url = `http://${host}:${port}`;
+    let requestStream;
+    if (req.body.jsonrpc) {
+      const options = {
+        uri: url,
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        method: req.method,
+        body: JSON.stringify(req.body),
+        json: true
+      };
+      requestStream = request(options);
+    } else {
+      requestStream = req.pipe(request(url));
+    }
+    requestStream.on('error', e => console.log(e)).pipe(res);
   } else {
-    requestStream = req.pipe(request(url));
+    res.status(500).send(`This node is not configured with a web3 connection for ${chain} ${network} `);
   }
-  requestStream
-    .on('error', e => console.log(e))
-    .pipe(res);
 }
