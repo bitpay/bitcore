fcbaee9d441e5e69024b5ca1774a77f93a827782
Justin <justin@Satoshis-MacBook-Pro.local>
2019-01-09 16:50:19 -0500

    deleted comments

--- packages/bitcore-node/src/routes/api/wallet.ts
+++ packages/bitcore-node/src/routes/api/wallet.ts
@@ -25,25 +25,6 @@ type AuthenticatedRequest = {
   wallet?: MongoBound<IWallet>;
 } & PreAuthRequest;
 
-/**
- * A sample async function (to demo Typescript's es7 async/await downleveling).
- *
- * ### Example (es imports)
- * ```js
- * import { asyncABC } from 'typescript-starter'
- * console.log(await asyncABC())
- * // => ['a','b','c']
- * ```
- *
- * ### Example (commonjs)
- * ```js
- * var double = require('typescript-starter').asyncABC;
- * asyncABC().then(console.log);
- * // => ['a','b','c']
- * ```
- *
- * @returns       a Promise which should contain `['a','b','c']`
- */
 const verifyRequestSignature = (params: VerificationPayload): boolean => {
   const { message, pubKey, signature } = params;
   const pub = new bitcoreLib.PublicKey(pubKey).toBuffer();
