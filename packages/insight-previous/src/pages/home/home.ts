1f738878c30e7dac8d8b01ba5f39b4d4d38da992
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2019-01-16 17:26:45 -0300

    Fix path for redir

--- packages/insight-previous/src/pages/home/home.ts
+++ packages/insight-previous/src/pages/home/home.ts
@@ -7,7 +7,7 @@ import { PriceProvider } from '../../providers/price/price';
 @Injectable()
 @IonicPage({
   name: 'home',
-  segment: ':chain/:network'
+  segment: ':chain/:network/'
 })
 @Component({
   selector: 'page-home',
