cc8eb28e38200727a3bcf44f9bcd0b1daeac9ebe
Justin Kook <jkook@bitpay.com>
2019-01-28 17:21:07 -0500

    Added RPC method getnewaddress, added test for wallet address import

--- packages/bitcore-node/test/integration/models/wallet.integration.ts
+++ packages/bitcore-node/test/integration/models/wallet.integration.ts
@@ -1,8 +1,11 @@
 import { expect } from 'chai';
 import { Wallet } from 'bitcore-client';
 import { Api } from '../../../src/services/api';
+import { AsyncRPC } from '../../../src/rpc';
 import { Event } from '../../../src/services/event';
 import { WalletStorage } from '../../../src/models/wallet';
+import config from '../../../src/config';
+import { WalletAddressStorage } from '../../../src/models/walletAddress';
 
 describe('Wallet Model', () => {
   describe('Wallet Create', () => {
@@ -51,6 +54,59 @@ describe('Wallet Model', () => {
       expect(result).to.have.property('pubKey');
       expect(result).to.have.property('path');
       expect(result).to.have.property('singleAddress');
+    });
+
+    it('should generate addresses using rpc then import to new wallet', async () => {
+      const walletName = 'Test Wallet 2';
+      const password = 'iamsatoshi2';
+      const chain = 'BTC';
+      const network = 'regtest';
+      const baseUrl = 'http://localhost:3000/api';
+      let lockedWallet: Wallet;
+      const chainConfig = config.chains[chain][network];
+      const creds = chainConfig.rpc;
+      const rpc = new AsyncRPC(creds.username, creds.password, creds.host, creds.port);
+
+      const address1 = await rpc.getnewaddress('');
+
+      const importAddressJSON = {
+        keys: [{ address: address1 }]
+      };
+
+      lockedWallet = await Wallet.create({
+        name: walletName,
+        chain,
+        network,
+        baseUrl,
+        password
+      });
+
+      const unlockedWallet = await lockedWallet.unlock(password);
+
+      await unlockedWallet.importKeys(importAddressJSON);
+
+      let result = await WalletStorage.collection.findOne({
+        name: walletName,
+        chain,
+        network
+      });
+
+      if (result && result._id) {
+        const importResult = await WalletAddressStorage.collection
+          .find({
+            wallet: result._id,
+            chain,
+            network,
+            address: address1
+          })
+          .toArray();
+
+        expect(importResult[0]).to.have.deep.property('chain', chain);
+        expect(importResult[0]).to.have.deep.property('network', network);
+        expect(importResult[0]).to.have.deep.property('wallet', result._id);
+        expect(importResult[0]).to.have.deep.property('address', address1);
+        expect(importResult[0]).to.have.deep.property('processed', true);
+      }
     });
   });
 });
