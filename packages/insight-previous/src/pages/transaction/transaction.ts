c2933bc6592e6682584a3147c57c30dbc9c59ee5
Juan Pablo Abuin <juanpabloabuin@gmail.com>
2019-02-01 11:51:12 -0300

    link to prev/next tx. UI fixes

--- packages/insight-previous/src/pages/transaction/transaction.ts
+++ packages/insight-previous/src/pages/transaction/transaction.ts
@@ -22,6 +22,8 @@ export class TransactionPage {
   private txId: string;
   private chainNetwork: ChainNetwork;
   public tx: any = {};
+  public vout: number;
+  public fromVout: boolean;
 
   constructor(
     public navParams: NavParams,
@@ -33,6 +35,8 @@ export class TransactionPage {
     public redirProvider: RedirProvider
   ) {
     this.txId = navParams.get('txId');
+    this.vout = navParams.get('vout');
+    this.fromVout = navParams.get('fromVout') || undefined;
 
     const chain: string =
       navParams.get('chain') || this.apiProvider.getConfig().chain;
