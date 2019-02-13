637f58225a2c9c997705e51d3aeb0c05ae9204a4
Justin Kook <jkook@bitpay.com>
2019-02-13 14:01:39 -0500

    added getInternalEthTransactions rpc method, chainged port in bitcore.config.json to 8545 and chain provider to parity

--- packages/bitcore-node/src/rpc.ts
+++ packages/bitcore-node/src/rpc.ts
@@ -84,6 +84,51 @@ export class RPC {
     });
   }
 
+  getInternalEthTransactions(address: string, callback: CallbackType) {
+    //WIP trace filter rpc method
+    // Port number 8545
+    /**
+     * Example Response:
+     * type response.result: array[]
+  "result": [
+    {
+      "action": {
+        "callType": "call",
+        "from": "0x32be343b94f860124dc4fee278fdcbd38c102d88",
+        "gas": "0x4c40d",
+        "input": "0x",
+        "to": "0x8bbb73bcb5d553b5a556358d27625323fd781d37",
+        "value": "0x3f0650ec47fd240000"
+      },
+      "blockHash": "0x86df301bcdd8248d982dbf039f09faf792684e1aeee99d5b58b77d620008b80f",
+      "blockNumber": 3068183,
+      "result": {
+        "gasUsed": "0x0",
+        "output": "0x"
+      },
+      "subtraces": 0,
+      "traceAddress": [],
+      "transactionHash": "0x3321a7708b1083130bd78da0d62ead9f6683033231617c9d268e2c7e3fa6c104",
+      "transactionPosition": 3,
+      "type": "call"
+    },
+    ...
+  ]
+}
+     *  */
+    this.callMethod(
+      'trace_filter',
+      [
+        {
+          fromAddress: [address],
+          after: 1000,
+          count: 100
+        }
+      ],
+      callback
+    );
+  }
+
   getTransaction(txid: string, callback: CallbackType) {
     this.callMethod('getrawtransaction', [txid, true], (err, result) => {
       if (err) {
