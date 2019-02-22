04b9fc8c2648071d45537d8ff7292ea4c20792eb
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2019-02-22 16:47:27 -0300

    Ref(insight-previous): fix broadcast transaction link

--- packages/insight-previous/src/pages/blocks/blocks.ts
+++ packages/insight-previous/src/pages/blocks/blocks.ts
@@ -1,6 +1,6 @@
 import { Component, Injectable } from '@angular/core';
 import { IonicPage, NavParams } from 'ionic-angular';
-import { ApiProvider } from '../../providers/api/api';
+import { ApiProvider, ChainNetwork } from '../../providers/api/api';
 import { BlocksProvider } from '../../providers/blocks/blocks';
 import { CurrencyProvider } from '../../providers/currency/currency';
 import { Logger } from '../../providers/logger/logger';
@@ -19,6 +19,7 @@ import { PriceProvider } from '../../providers/price/price';
 export class BlocksPage {
   public loading = true;
   public blocks: any[] = [];
+  private chainNetwork: ChainNetwork;
 
   constructor(
     public navParams: NavParams,
@@ -28,9 +29,15 @@ export class BlocksPage {
     private currencyProvider: CurrencyProvider,
     private priceProvider: PriceProvider
   ) {
-    const chain: string = this.apiProvider.getConfig().chain;
-    const network: string = this.apiProvider.getConfig().network;
-    this.apiProvider.changeNetwork({ chain, network });
+    const chain: string =
+      navParams.get('chain') || this.apiProvider.getConfig().chain;
+    const network: string =
+      navParams.get('network') || this.apiProvider.getConfig().network;
+    this.chainNetwork = {
+      chain,
+      network
+    };
+    this.apiProvider.changeNetwork(this.chainNetwork);
     this.currencyProvider.setCurrency();
     this.priceProvider.setCurrency();
 
