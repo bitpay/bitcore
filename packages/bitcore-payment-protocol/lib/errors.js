3e10566454ed90714db0024c4c4573b2f246a7fc
Matias Alejo Garcia <ematiu@gmail.com>
2019-03-21 13:02:03 -0300

    rm old payment protocol package

--- packages/bitcore-payment-protocol/lib/errors.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var spec = {
-  name: 'PaymentProtocol',
-  message: 'Internal Error on bitcore-payment-protocol Module: {0}'
-};
-
-module.exports = require('bitcore-lib').errors.extend(spec);
