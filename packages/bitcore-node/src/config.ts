bf919def7184ce25ae924ce32fbf6b0525846b98
Terekhin <terehinis@gmail.com>
2019-04-05 10:34:52 +0300

    add auth for MongoDB

--- packages/bitcore-node/src/config.ts
+++ packages/bitcore-node/src/config.ts
@@ -57,6 +57,8 @@ const Config = function (): ConfigType {
     dbHost: process.env.DB_HOST || '127.0.0.1',
     dbName: process.env.DB_NAME || 'bitcore',
     dbPort: process.env.DB_PORT || '27017',
+    dbUser: process.env.DB_USER || '',
+    dbPass: process.env.DB_PASS || '',
     numWorkers: cpus().length,
     chains: {},
     services: {
