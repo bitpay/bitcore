498d6847450f9ecf1b553585db17a7bfa47449af
Micah Riggan <micahriggan@gmail.com>
2019-04-18 13:56:16 -0400

    Can now start p2p, api, and full services. Added pruning service

---
+++ packages/bitcore-node/src/services/pruning.ts
@@ -0,0 +1,70 @@
+import { TransactionStorage, TransactionModel } from '../models/transaction';
+import { CoinStorage, CoinModel } from '../models/coin';
+import logger from '../logger';
+import { Config } from './config';
+import '../utils/polyfills';
+
+export class PruningService {
+  transactionModel: TransactionModel;
+  coinModel: CoinModel;
+  stopping = false;
+
+  constructor({ transactionModel = TransactionStorage, coinModel = CoinStorage } = {}) {
+    this.transactionModel = transactionModel;
+    this.coinModel = coinModel;
+  }
+
+  async start() {
+    this.detectAndClear().then(() => process.emit('SIGINT'));
+  }
+
+  async stop() {
+    logger.info('Stopping Pruning Service');
+    this.stopping = true;
+  }
+
+  async detectAndClear() {
+    for (let chainNetwork of Config.chainNetworks()) {
+      const { chain, network } = chainNetwork;
+      const invalids = this.detectInvalidCoins(chain, network);
+      for await (const invalidCoins of invalids) {
+        if (this.stopping) {
+          return;
+        }
+        const txids = invalidCoins.map(c => c.mintTxid);
+        await this.clearInvalid(txids);
+      }
+    }
+  }
+
+  async *detectInvalidCoins(chain, network) {
+    const coins = await this.coinModel.collection.find({ chain, network, mintHeight: -3 }).toArray();
+    logger.info('Pruning worker found', coins.length, 'invalid coins for ', chain, network);
+    for (const coin of coins) {
+      yield await this.scanForInvalid(coin.spentTxid);
+    }
+  }
+
+  async scanForInvalid(spentTxid: string) {
+    const foundCoins = await this.coinModel.collection.find({ mintTxid: spentTxid, mintHeight: { $ne: -3 } }).toArray();
+    if (foundCoins.length === 0) {
+      return foundCoins;
+    } else {
+      for (const coin of foundCoins) {
+        if (coin.spentTxid) {
+          foundCoins.push(...(await this.scanForInvalid(coin.spentTxid)));
+        }
+      }
+    }
+    return foundCoins;
+  }
+
+  async clearInvalid(invalidTxids: Array<string>) {
+    logger.info('Pruning worker clearing', invalidTxids.length, 'txids');
+    return Promise.all([
+      this.transactionModel.collection.updateMany({ txid: { $in: invalidTxids } }, { $set: { blockHeight: -3 } }),
+      this.coinModel.collection.updateMany({ mintTxid: { $in: invalidTxids } }, { $set: { mintHeight: -3 } })
+    ]);
+  }
+}
+export const Pruning = new PruningService();
