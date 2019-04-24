affb59ed8de26a8d7ef8aa8ddf1bd143a2dc9896
Micah Riggan <micahriggan@gmail.com>
2019-04-24 10:43:16 -0400

    Remove double export

--- packages/bitcore-node/src/logger.ts
+++ packages/bitcore-node/src/logger.ts
@@ -2,7 +2,7 @@ import * as winston from 'winston';
 import parseArgv from './utils/parseArgv';
 let args = parseArgv([], ['DEBUG']);
 const logLevel = args.DEBUG ? 'debug' : 'info';
-export const logger = new winston.Logger({
+const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
       colorize: true,
