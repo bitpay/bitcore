b1013e93e3aebeb8b53050df185d02064db086e2
Justin Kook <jkook@bitpay.com>
2019-07-18 11:38:36 -0400

    remove gas price check on feePerKb

--- packages/bitcore-wallet-service/src/lib/server.ts
+++ packages/bitcore-wallet-service/src/lib/server.ts
@@ -1831,19 +1831,11 @@ export class WalletService {
       }
 
       if (_.isNumber(opts.feePerKb)) {
-        if (Constants.UTXO_COINS[opts.coin.toUpperCase()]) {
           if (
             opts.feePerKb < Defaults.MIN_FEE_PER_KB ||
             opts.feePerKb > Defaults.MAX_FEE_PER_KB
           )
             return cb(new ClientError('Invalid fee per KB'));
-          } else {
-            if (
-              opts.feePerKb < Defaults.MIN_GAS_PRICE ||
-              opts.feePerKb > Defaults.MAX_GAS_PRICE
-            )
-              return cb(new ClientError('Invalid gas price'));
-          }
       }
 
       this._getUtxosForCurrentWallet({}, (err, utxos) => {
@@ -2542,19 +2534,11 @@ export class WalletService {
           }
 
           if (_.isNumber(opts.feePerKb)) {
-            if (Constants.UTXO_COINS[opts.coin.toUpperCase()]) {
               if (
                 opts.feePerKb < Defaults.MIN_FEE_PER_KB ||
                 opts.feePerKb > Defaults.MAX_FEE_PER_KB
               )
                 return cb(new ClientError('Invalid fee per KB'));
-              } else {
-                if (
-                  opts.feePerKb < Defaults.MIN_GAS_PRICE ||
-                  opts.feePerKb > Defaults.MAX_GAS_PRICE
-                )
-                  return cb(new ClientError('Invalid gas price'));
-              }
           }
 
           if (_.isNumber(opts.fee) && _.isEmpty(opts.inputs))
