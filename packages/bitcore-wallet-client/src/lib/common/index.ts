a2f18248d99eef9ecd00a606d390980e2408a9b3
Micah Riggan <micahriggan@gmail.com>
2019-08-21 14:59:38 -0400

    Commented out code removed, and typo fixed

--- packages/bitcore-wallet-client/src/lib/common/index.ts
+++ packages/bitcore-wallet-client/src/lib/common/index.ts
@@ -2,10 +2,3 @@ import { Constants } from './constants';
 import { Defaults } from './defaults';
 import { Utils } from './utils';
 export const Common = { Constants, Defaults, Utils };
-/*
- *module.exports = {
- *  Constants: require('./constants'),
- *  Defaults: require('./defaults'),
- *  Utils: require('./utils')
- *};
- */
