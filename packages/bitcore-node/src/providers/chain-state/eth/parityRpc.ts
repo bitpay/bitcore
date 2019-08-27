b09632800fc45d124cd294ef353beb5803761303
Micah Riggan <micahriggan@gmail.com>
2019-08-27 16:14:06 -0400

    Resync and repair works

--- packages/bitcore-node/src/providers/chain-state/eth/parityRpc.ts
+++ packages/bitcore-node/src/providers/chain-state/eth/parityRpc.ts
@@ -1,5 +1,6 @@
 import Web3 from 'web3';
-import "../../../utils/polyfills";
+import { JsonRPCResponse, Callback } from './eth';
+import '../../../utils/polyfills';
 
 interface ParityBlockReward {
   author: string;
@@ -64,7 +65,9 @@ export class ParityRPC {
           jsonrpc: '2.0',
           id: 0
         },
-        (_, data) => resolve(data.result as Array<ParityTraceResponse>)
+        function(_, data) {
+          resolve(data.result as Array<ParityTraceResponse>);
+        } as Callback<JsonRPCResponse>
       )
     );
   }
