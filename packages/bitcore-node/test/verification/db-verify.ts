5d7a1833bfa4fb152edf9c55e865f7442856498a
Micah Riggan <micahriggan@gmail.com>
2019-08-27 16:49:46 -0400

    Fixing merge conflicts

--- packages/bitcore-node/test/verification/db-verify.ts
+++ packages/bitcore-node/test/verification/db-verify.ts
@@ -21,9 +21,9 @@ type ErrorType = {
 export async function validateDataForBlock(blockNum: number, log = false) {
   let success = true;
   const [block, blockTxs, blocksForHeight] = await Promise.all([
-    BlockStorage.collection.findOne({ chain, network, height: blockNum, processed: true }),
+    BitcoinBlockStorage.collection.findOne({ chain, network, height: blockNum, processed: true }),
     TransactionStorage.collection.find({ chain, network, blockHeight: blockNum }).toArray(),
-    BlockStorage.collection.countDocuments({
+    BitcoinBlockStorage.collection.countDocuments({
       chain,
       network,
       height: blockNum,
@@ -35,7 +35,7 @@ export async function validateDataForBlock(blockNum: number, log = false) {
   const [coinsForTx, mempoolTxs, blocksForHash] = await Promise.all([
     CoinStorage.collection.find({ chain, network, mintTxid: { $in: blockTxids } }).toArray(),
     TransactionStorage.collection.find({ chain, network, blockHeight: -1, txid: { $in: blockTxids } }).toArray(),
-    BlockStorage.collection.countDocuments({ chain, network, hash: firstHash })
+    BitcoinBlockStorage.collection.countDocuments({ chain, network, hash: firstHash })
   ]);
 
   const seenTxs = {} as { [txid: string]: ITransaction };
