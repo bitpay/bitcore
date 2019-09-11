2f7babf39650dd357e7600e97d81f93e8a9d16c3
Bernard Snowden <bsnowden@bitpay.com>
2019-09-11 12:02:21 -0400

    price chart squashed

--- packages/insight/src/pages/home/home.ts
+++ packages/insight/src/pages/home/home.ts
@@ -1,9 +1,13 @@
 import { Component, Injectable, ViewChild } from '@angular/core';
 import { Events, IonicPage, Nav, NavParams } from 'ionic-angular';
+import _ from 'lodash';
+import { DailyTransactionChartComponent } from '../../components/daily-transaction-chart/daily-transaction-chart';
 import { LatestBlocksComponent } from '../../components/latest-blocks/latest-blocks';
+import { PriceChartComponent } from '../../components/price-card/price-chart/price-chart';
 import { ApiProvider, ChainNetwork } from '../../providers/api/api';
 import { CurrencyProvider } from '../../providers/currency/currency';
 import { PriceProvider } from '../../providers/price/price';
+import { TxsProvider } from '../../providers/transactions/transactions';
 
 @Injectable()
 @IonicPage({
@@ -16,30 +20,170 @@ import { PriceProvider } from '../../providers/price/price';
 })
 export class HomePage {
   @ViewChild('latestBlocks')
+  @ViewChild('priceChart') priceChart
+  @ViewChild('dailyTxChart') dailyTxChart
+
   public latestBlocks: LatestBlocksComponent;
+  public priceChart: PriceChartComponent;
+  public dailyTxChart: DailyTransactionChartComponent;
+  public coin: string;
   public chain: string;
+  public showPriceChart: boolean;
+  public showBlocks: boolean;
+  public showDailyTxChart: boolean;
+  public currentView: string;
   public chainNetwork: ChainNetwork;
   public network: string;
+  public coins: any;
+  
   constructor(
     public nav: Nav,
     public navParams: NavParams,
     private apiProvider: ApiProvider,
     private priceProvider: PriceProvider,
     public events: Events,
-    public currencyProvider: CurrencyProvider
+    public currencyProvider: CurrencyProvider,
+    public transactionProvider: TxsProvider,
   ) {
-    const chain: string =
+    const chain =
       navParams.get('chain') || this.apiProvider.getConfig().chain;
     const network: string =
       navParams.get('network') || this.apiProvider.getConfig().network;
+
+    const views = ["blocks", "price", "txsperday"];
+    
+    this.coin = chain;
+    this.showPriceChart = false;
+    this.showDailyTxChart = false;
+
+    this.currentView = "blocks";
 
     this.chainNetwork = {
       chain,
       network
     };
+
     this.apiProvider.changeNetwork(this.chainNetwork);
     this.currencyProvider.setCurrency(this.chainNetwork);
     this.priceProvider.setCurrency();
+    this.coins = {
+      'btc': {
+        name: 'Bitcoin',
+        historicalRates: [],
+        dailyTransactionCounts: [],
+        currentPrice: 0,
+        averagePrice: 0,
+        lastNumberOfTransactionsConfirmed: 0,
+        backgroundColor: 'rgba(69,99,246,1)',
+        gradientBackgroundColor: 'rgba(69,99,246, 0.2)',
+        ticks: {
+          thirtyDayTicks: {
+            // tslint:disable-next-line: object-literal-shorthand
+            callback: function(value) {
+              return this.numberWithCommas(value);
+            },
+            yAxesTicks: {
+              maxTicksLimit: 10,
+              stepSize: 500,
+              // tslint:disable-next-line: object-literal-shorthand
+              callback: function(value) {
+                return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+              },
+            },
+            xAxesTicks: {
+              maxTicksLimit: 5,
+              stepSize: 5,
+            }
+          },
+          sevenDayTicks: {
+            // tslint:disable-next-line: object-literal-shorthand
+            yAxesTicks: {
+              maxTicksLimit: 10,
+              stepSize: 500,
+              // tslint:disable-next-line: object-literal-shorthand
+              callback: function(value) {
+                return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+              },
+            },
+            xAxesTicks: {
+              maxTicksLimit: 7,
+              stepSize: 1,
+            }
+          }
+        },
+      },
+      'bch': {
+        name: 'Bitcoin Cash',
+        historicalRates: [],
+        dailyTransactionCounts: [],
+        currentPrice: 0,
+        averagePrice: 0,
+        backgroundColor: 'rgba(69,99,246,1)',
+        gradientBackgroundColor: 'rgba(69,99,246, 0.2)',
+        ticks: {
+          thirtyDayTicks: {
+            yAxesTicks: {
+              maxTicksLimit: 10,
+              stepSize: 25
+            },
+            xAxesTicks: {
+              maxTicksLimit: 5,
+              stepSize: 5,
+            }
+          },
+          sevenDayTicks: {
+            yAxesTicks: {
+              maxTicksLimit: 10,
+              stepSize: 25,
+            },
+            xAxesTicks: {
+              maxTicksLimit: 7,
+              stepSize: 1,
+            }
+          }
+      },
+    }
+  }
+}
+  public numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  private switchView(view) {
+    this.currentView = view;
+  }
+
+
+  public getDailyTransactionCount(currency?: string, numOfDays?: number) {
+    this.transactionProvider.getDailyTransactionHistory(this.chainNetwork).subscribe((response: any) => {
+      this.coins[currency].dailyTransactionCounts = response.results.slice((response.results.length - 1) - numOfDays, response.results.length-1);
+      this.coins[currency].lastNumberOfTransactionsConfirmed = response.results[response.results.length -1].transactionCount;
+      if(this.coins[currency].name === 'Bitcoin' && this.chainNetwork.network == "mainnet") {
+        this.dailyTxChart.drawChart(this.coins[currency], 7, this.coins[currency].lastNumberOfTransactionsConfirmed);
+      }
+    });
+ } 
+
+  public getHistoricalPriceForCurrencies(currency?: string, isoCode?: string, days?: number) {
+    this.priceProvider.getHistoricalRate(currency, isoCode ,days).subscribe((response: any) => {
+      this.coins[currency].currentPrice = response[(days-1)].rate; 
+      this.coins[currency].historicalRates = response;
+      this.priceChart.drawPriceChart(this.coins[currency], days);
+    });
+  }
+
+  public goToPriceChartHandler() {
+    this.switchView('price-chart');
+    this.getHistoricalPriceForCurrencies(this.coin.toLowerCase(), 'USD', 7);
+  }
+
+  public drawDailyTxChartHandler() {
+    this.switchView('txsperday');
+    this.getDailyTransactionCount(this.coin.toLowerCase(), 7);
+  }
+
+  public goToBlocks() {
+    this.switchView('blocks');
   }
 
   public openPage(page: string): void {
