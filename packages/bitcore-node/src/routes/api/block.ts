9f55afba73aa4fbe05cf59110c7aff02eedb8738
Bernard Snowden <bsnowden@bitpay.com>
2019-09-26 16:55:15 -0400

    add paginated endpoint for retrieving block tx (input and output) data

--- packages/bitcore-node/src/routes/api/block.ts
+++ packages/bitcore-node/src/routes/api/block.ts
@@ -3,6 +3,7 @@ import { ChainStateProvider } from '../../providers/chain-state';
 import { SetCache, CacheTimes, Confirmations } from '../middleware';
 import { BitcoinBlockStorage } from '../../models/block';
 import { TransactionStorage } from '../../models/transaction';
+import { CoinStorage } from '../../models/coin';
 
 const router = require('express').Router({ mergeParams: true });
 
@@ -55,33 +56,108 @@ router.get('/:blockId', async function(req: Request, res: Response) {
   }
 });
 
-//return mapping of { txid, coin } for a block
-router.get('/:blockHash/coins', async function (req: Request, res: Response) {
-  let { chain, network, blockHash } = req.params;
+//return mapping of {txid: {inputs, ouputs}} paginated at 20 per page, to limit reqs and overload
+router.get('/:blockHash/coins/:limit/:pgnum', async function (req: Request, res: Response) {
+  let { chain, network, blockHash, limit, pgnum } = req.params;
+   
   try {
-    const txs = await TransactionStorage.collection.find({ chain, network, blockHash }).toArray();
-    
+    pgnum = parseInt(pgnum, 10);
+    limit = parseInt(limit, 10);
+    if(!(typeof pgnum === "number" && typeof limit === "number")) {
+      res.status(400).send("Please enter limit and number as valid decimal numbers")
+    } 
+    if (limit) {
+      if (limit > 500) limit = 500;
+    }
+  } catch(err) {
+    console.log(err);
+  }
+
+
+  let skips = limit * (pgnum - 1);
+  let numOfTxs : number = await TransactionStorage.collection.find({ chain, network, blockHash }).count();
+  try {
+    let txs;
+    if(numOfTxs < limit) {
+      txs = await TransactionStorage.collection.find({ chain, network, blockHash }).toArray();
+    } else {
+      console.log(limit);
+      txs = await TransactionStorage.collection.find({ chain, network, blockHash }).skip(skips).limit(limit).toArray();
+    }
+
+    if (!txs) {
+      return res.status(422).send("No txs for page");
+    }
+
     const txidIndexes : any= {};
-    
-    const txidCoinPromises = txs.map(function(tx, index) {
-      let txid = tx.txid;
-      txidIndexes[index] = txid;
-      return ChainStateProvider.getCoinsForTx({ chain, network, txid });
-    });
-  
-    let txidCoins = await Promise.all(txidCoinPromises).then(
-        (data) => 
-        { 
-          let response : any = {};
-          data.forEach((coin, index) => {
-            let txid : string = txidIndexes[index];
-            response[txid] = coin;
-        }); 
-        return response;
+    let txids = txs.map((tx, index) => { txidIndexes[index] = tx.txid; return tx.txid });
+    let inputTxidIndexes : any = {};
+    let outputTxidIndexes: any = {};
+
+    let inputsPromises = txids.map((txid,index) => {
+      try {
+        inputTxidIndexes[txid] = index;
+        return CoinStorage.collection
+          .find({
+            chain,
+            network,
+            spentTxid: txid
+          })
+          .addCursorFlag('noCursorTimeout', true)
+          .toArray();
+      }  catch(err) {
+        console.log("Error reading inputs", err)
+         return err;
       }
-    );
-    return res.json(txidCoins);
+    });
+
+    let inputsResults = await Promise.all(inputsPromises).then(inputs => { return inputs; } );
+
+    let outputsPromises = txids.map((txid, index) => {
+      try {
+        outputTxidIndexes[txid] = index;
+        return CoinStorage.collection
+          .find({
+            chain,
+            network,
+            mintTxid: txid
+          })
+          .addCursorFlag('noCursorTimeout', true)
+          .toArray();
+
+      } catch (err) {
+        console.log("Error reading outputs", err)
+        return err;
+      }
+    });
+
+    let outputsResults = await Promise.all(outputsPromises).then(outputs => { return outputs; });
+
+    let txsResults : any = {};
+  
+    txids.forEach((txid) => {
+      let inputs = inputsResults[inputTxidIndexes[txid]];
+      let outputs = outputsResults[outputTxidIndexes[txid]];
+
+      txsResults[txid] = { inputs, outputs }
+    });
+
+    let prevPageNum;
+    let nxtPageNum;
+    let previous: string = ''; 
+    let next : string = '';
+    if((pgnum !== 1)) {
+      prevPageNum = parseInt(pgnum) - 1;
+      previous = `/block/${blockHash}/coins/${limit}/${prevPageNum}`;
+    } 
+    if(numOfTxs - (limit * pgnum) > 0) {
+      nxtPageNum = pgnum + 1;
+      next = `/block/${blockHash}/coins/${limit}/${nxtPageNum}`;
+    }
+
+    return res.json({ txsResults, previous, next });
   } catch (err) {
+    console.log(err);
     return res.status(500).send(err);
   }
 });
