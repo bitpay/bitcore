844b543bd187901081ca40b0cbc89a3f3bbe67d8
Jack Bonaguro <jbonaguro@bitpay.com>
2019-10-03 15:44:57 -0400

    Cherry picked: encode _unsigned_ rawTxs with binary codec

--- packages/crypto-wallet-core/src/transactions/xrp/index.ts
+++ packages/crypto-wallet-core/src/transactions/xrp/index.ts
@@ -1,9 +1,11 @@
+// tslint:disable-next-line:no-submodule-imports
+import { Key } from 'derivation';
+import rippleBinaryCodec from 'ripple-binary-codec';
 import { RippleAPI } from 'ripple-lib';
 // tslint:disable-next-line:no-submodule-imports
 import { Payment } from 'ripple-lib/dist/npm/transaction/payment';
 // tslint:disable-next-line:no-submodule-imports
 import { Instructions } from 'ripple-lib/dist/npm/transaction/types';
-import { Key } from '../../derivation';
 
 export class XRPTxProvider {
   create(params: {
@@ -45,7 +47,7 @@ export class XRPTxProvider {
 
     let rippleAPI = new RippleAPI();
     return rippleAPI.preparePayment(from, payment, instructions).then((preparedTx) => {
-      return preparedTx.txJSON;
+      return rippleBinaryCodec.encode(preparedTx.txJSON);
     }).catch((err) => {
       console.error(err);
       return;
@@ -54,7 +56,7 @@ export class XRPTxProvider {
 
   sign(params: { tx: string; key: Key; }) {
     const { tx, key } = params;
-    const txJSON = tx;
+    const txJSON = rippleBinaryCodec.decode(tx);
     let rippleAPI = new RippleAPI();
     const signedTx = rippleAPI.sign(txJSON, {
       privateKey: key.privKey,
