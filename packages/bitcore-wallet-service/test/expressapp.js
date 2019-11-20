02a7047dfb538e356f195526a172604d510d1f7b
Bernard Snowden <bsnowden@bitpay.com>
2019-11-20 15:15:50 -0500

    test progress

--- packages/bitcore-wallet-service/test/expressapp.js
+++ packages/bitcore-wallet-service/test/expressapp.js
@@ -135,42 +135,70 @@ describe('ExpressApp', function() {
         });
       });
 
+  
       it('latest-copay-version', function(done) {
-        var callback = sinon.spy();
-        var server = {
-          getStatus: sinon.stub().callsArgWith(1, null, {}),
-          storage: {
-            checkAndUseGlobalCache: sinon.stub().callsArgWith(3, null, 'latest-copay-version', 6 * 60 * 1000, callback),
-          },
-        };
-
-        var {ExpressApp: TestExpressApp} = proxyquire('../ts_build/lib/expressapp', {
-          './server': {
-            WalletService: {
-              initialize: sinon.stub().callsArg(1, null),
-              getServiceVersion: WalletService.getServiceVersion,
-              getInstance: sinon.stub().callsArgWith(1, null, null, server),
-            }
-          }
-        });
-
-        start(TestExpressApp, function() {
-          var requestOptions = {
-            url: testHost + ':' + testPort + config.basePath + '/latest-version',
-            headers: {
-              'x-identity': 'identity',
-              'x-signature': 'signature'
-            }
-          };
-
-          request(requestOptions, function(err, res, body) {
-            //sinon.assert.calledOnce(callback);
-            should.not.exist(err);
-            res.statusCode.should.equal(500);
-            body.should.equal({"version":"v7.0.4"});
-            done();
+          var clock;
+          before(function() {
+            clock = sinon.useFakeTimers();
           });
-        });
+          after(function() {
+            clock.restore();
+          });
+          var callback = sinon.spy();
+          var COPAY_CACHE_DURATION_old = Defaults.COPAY_CACHE_DURATION;
+
+          // var {Storage: s} = proxyquire('../ts_build/lib/storage', {
+          //   './storage': {
+          //       Storage: {
+          //         storeGlobalCache: sinon.stub().callsArgWith(3, null, 'latest-copay-version', '7.1.1', () => {}),
+          //         checkAndUseGlobalCache: sinon.stub('latest-copay-version', COPAY_CACHE_DURATION_old, callback),
+          //       }
+          //   }
+          // });
+          var githubResponse = sinon.stub().returns({
+            "url": "https://api.github.com/repos/bitpay/copay/releases/21158137",
+            "assets_url": "https://api.github.com/repos/bitpay/copay/releases/21158137/assets",
+            "upload_url": "https://uploads.github.com/repos/bitpay/copay/releases/21158137/assets{?name,label}",
+            "html_url": "https://github.com/bitpay/copay/releases/tag/v7.1.1",
+            "id": 21158137,
+            "node_id": "MDc6UmVsZWFzZTIxMTU4MTM3",
+            "tag_name": "v7.1.1",
+            "target_commitish": "master",
+            "name": "v7.1.1",
+            "draft": false,
+            "author": {
+              "login": "cmgustavo",
+              "id": 237435,
+              "node_id": "MDQ6VXNlcjIzNzQzNQ==",
+              "avatar_url": "https://avatars3.githubusercontent.com/u/237435?v=4",
+              "gravatar_id": "",
+              "url": "https://api.github.com/users/cmgustavo",
+              "html_url": "https://github.com/cmgustavo",
+              "followers_url": "https://api.github.com/users/cmgustavo/followers",
+              "following_url": "https://api.github.com/users/cmgustavo/following{/other_user}",
+              "gists_url": "https://api.github.com/users/cmgustavo/gists{/gist_id}",
+              "starred_url": "https://api.github.com/users/cmgustavo/starred{/owner}{/repo}",
+              "subscriptions_url": "https://api.github.com/users/cmgustavo/subscriptions",
+              "organizations_url": "https://api.github.com/users/cmgustavo/orgs",
+              "repos_url": "https://api.github.com/users/cmgustavo/repos",
+              "events_url": "https://api.github.com/users/cmgustavo/events{/privacy}",
+              "received_events_url": "https://api.github.com/users/cmgustavo/received_events",
+              "type": "User",
+              "site_admin": false
+            },
+            "prerelease": false,
+            "created_at": "2019-10-29T14:09:01Z",
+            "published_at": "2019-11-01T19:50:37Z",
+            "assets": [
+          
+            ],
+            "tarball_url": "https://api.github.com/repos/bitpay/copay/tarball/v7.1.1",
+            "zipball_url": "https://api.github.com/repos/bitpay/copay/zipball/v7.1.1",
+            "body": "### Changelog\r\n\r\nNEW\r\n\r\n* ETH Testnet support\r\n* EUR Gift Cards\r\n* Export Key to another wallet as QR code\r\n\r\nBUG FIXES\r\n\r\n* Open app from ETH link (only Desktop)\r\n* Clear badge of pending notification (iOS)\r\n* UI issues on Settings Page\r\n* Send-max for top up cards\r\n\r\n### Download\r\n\r\n<table>\r\n<tbody>\r\n<tr>\r\n<td>App</td>\r\n<td>for Mac OS</td>\r\n<td>for Windows</td>\r\n<td>for Linux</td>\r\n</tr>\r\n<tr>\r\n<td>\r\n<a href=\"https://bitpay.com/wallet\" target=\"_blank\"><img src=\"https://user-images.githubusercontent.com/237435/48089088-68afa480-e1e2-11e8-83a8-361d0440528c.png\" alt=\"BitPay\"></a>\r\n</td>\r\n<td>\r\n<a href=\"https://itunes.apple.com/us/app/bitpay/id1440200291?ls=1&mt=12\" target=\"_blank\"><img src=\"https://user-images.githubusercontent.com/237435/48092454-7ddd0100-e1eb-11e8-9e13-3fe80bba7f00.png\" alt=\"mac\" width=\"120\"></a>\r\n</td>\r\n<td>\r\n<a href=\"https://www.microsoft.com/store/apps/9NBR15SK4ZJV\" target=\"_blank\"><img src=\"https://user-images.githubusercontent.com/237435/48092465-82091e80-e1eb-11e8-9e06-36b36cd44021.png\" alt=\"windows\" width=\"120\"></a>\r\n</td>\r\n<td>\r\n<a href=\"https://snapcraft.io/bitpay\" target=\"_blank\"><img src=\"https://user-images.githubusercontent.com/237435/48092664-09ef2880-e1ec-11e8-94a2-184446cb7183.png\" alt=\"linux\" width=\"120\"></a>\r\n</td>\r\n</tr>\r\n<tr>\r\n<td>\r\n<a href=\"https://copay.io\" target=\"_blank\"><img src=\"https://user-images.githubusercontent.com/237435/48089097-6cdbc200-e1e2-11e8-9e54-363d54ae8fc6.png\" alt=\"Copay\"></a>\r\n</td>\r\n<td>\r\n<a href=\"https://itunes.apple.com/us/app/copay/id1440201813\" target=\"_blank\"><img src=\"https://user-images.githubusercontent.com/237435/48092454-7ddd0100-e1eb-11e8-9e13-3fe80bba7f00.png\" alt=\"mac\" width=\"120\"></a>\r\n</td>\r\n<td>\r\n<a href=\"https://www.microsoft.com/store/apps/9MZGT30HL9DF\" target=\"_blank\"><img src=\"https://user-images.githubusercontent.com/237435/48092465-82091e80-e1eb-11e8-9e06-36b36cd44021.png\" alt=\"windows\" width=\"120\"></a>\r\n</td>\r\n<td>\r\n<a href=\"https://snapcraft.io/copay\" target=\"_blank\"><img src=\"https://user-images.githubusercontent.com/237435/48092664-09ef2880-e1ec-11e8-94a2-184446cb7183.png\" alt=\"linux\" width=\"120\"></a>\r\n</td>\r\n</tr>\r\n</tbody>\r\n</table>"
+          });
+          storage.checkAndUseGlobalCache('latest-copay-version', COPAY_CACHE_DURATION_old, callback);
+          clock.tick(COPAY_CACHE_DURATION_old);
+          assert(callback.calledOnce);
       });
 
       it('/v1/sendmaxinfo', function(done) {
@@ -188,6 +216,7 @@ describe('ExpressApp', function() {
             }
           }
         });
+        
         start(TestExpressApp, function() {
           var requestOptions = {
             url: testHost + ':' + testPort + config.basePath + '/v1/sendmaxinfo?feePerKb=10000&returnInputs=1',
