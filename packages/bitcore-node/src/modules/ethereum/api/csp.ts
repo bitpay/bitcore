170f44b2310f4e9e8a4da80d4dbc8484fb940e99
Micah Riggan <micahriggan@gmail.com>
2019-12-10 13:42:59 -0500

    Invoice payment transactions should also be in ERC20 wallet history, if the payment is denominated in tokens

--- packages/bitcore-node/src/modules/ethereum/api/csp.ts
+++ packages/bitcore-node/src/modules/ethereum/api/csp.ts
@@ -293,20 +293,28 @@ export class ETHStateProvider extends InternalStateProvider implements CSP.IChai
       const query = {
         chain,
         network,
-        to: web3.utils.toChecksumAddress(args.tokenAddress),
         $or: [
           {
             wallets: wallet._id,
+            abiType: { $exists: true },
+            to: web3.utils.toChecksumAddress(args.tokenAddress),
             'abiType.type': 'ERC20',
             'abiType.name': 'transfer',
-            'wallets.0': { $exists: true },
-            abiType: { $exists: true }
+            'wallets.0': { $exists: true }
           },
           {
             abiType: { $exists: true },
+            to: web3.utils.toChecksumAddress(args.tokenAddress),
             'abiType.type': 'ERC20',
             'abiType.name': 'transfer',
             'abiType.params.0.value': { $in: walletAddresses.map(w => w.address.toLowerCase()) }
+          },
+          {
+            wallets: wallet._id,
+            abiType: { $exists: true },
+            'abiType.type': 'INVOICE',
+            'abiType.params.8.value': args.tokenAddress.toLowerCase(),
+            'wallets.0': { $exists: true }
           }
         ]
       };
@@ -318,11 +326,21 @@ export class ETHStateProvider extends InternalStateProvider implements CSP.IChai
           new Transform({
             objectMode: true,
             transform: (tx: any, _, cb) => {
-              cb(null, {
-                ...tx,
-                value: tx.abiType!.params[1].value,
-                to: web3.utils.toChecksumAddress(tx.abiType!.params[0].value)
-              });
+              if (tx.abiType && tx.abiType.type === 'ERC20') {
+                return cb(null, {
+                  ...tx,
+                  value: tx.abiType!.params[1].value,
+                  to: web3.utils.toChecksumAddress(tx.abiType!.params[0].value)
+                });
+              }
+              if (tx.abiType && tx.abiType.type === 'INVOICE') {
+                return cb(null, {
+                  ...tx,
+                  value: tx.abiType!.params[0].value,
+                  to: tx.to
+                });
+              }
+              return cb(null, tx);
             }
           })
         );
