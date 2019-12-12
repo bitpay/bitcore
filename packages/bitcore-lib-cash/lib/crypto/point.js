f608a6895c90baef85c794ebd74e253fbac381cb
Bernard Snowden <bsnowden@bitpay.com>
2019-12-12 17:51:39 -0500

    correct hasSquare() function

--- packages/bitcore-lib-cash/lib/crypto/point.js
+++ packages/bitcore-lib-cash/lib/crypto/point.js
@@ -27,7 +27,7 @@ var Point = function Point(x, y, isRed) {
     throw new Error('Invalid Point');
   }
   point.validate();
-  return point;s
+  return point;
 };
 
 Point.prototype = Object.getPrototypeOf(ec.curve.point());
@@ -151,15 +151,23 @@ Point.pointToCompressed = function pointToCompressed(point) {
 
 // todo: needs test case
 Point.prototype.hasSquare = function() {
-  return (!this.isInfinity() && this.isSquare(this.getY()));
+  console.log("Point", this.isInfinity());
+  console.log("isSquare:", this.isSquare(this.getY()));
+  return !this.isInfinity() && this.isSquare(this.getY());
 }
 
 // todo: needs test cases
 Point.prototype.isSquare = function(x) {
   let p = new BN('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F', 'hex');
   let x0 = new BN(x);
-  let base = x0.toRed(BN.red((p.sub(new BN(1))).div(new BN(2))));
-  return (base.redPow(p).fromRed()).eqn(1); //refactor to BN arithmetic operations
+
+  let base = x0.toRed(BN.red(p));
+  
+  
+  let res = (base.redPow((p.sub(new BN(1))).div(new BN(2)))).fromRed(); //refactor to BN arithmetic operations
+  // console.log(res.eq(new BN(1)));
+  // console.log(res.toString());
+  return res.eq(new BN(1));
 }
 
 module.exports = Point;
--- packages/bitcore-lib-cash/lib/crypto/schnorr.js
+++ packages/bitcore-lib-cash/lib/crypto/schnorr.js
@@ -68,10 +68,12 @@ Schnorr.prototype._findSignature = function(d, e) {
     P = G.mul(dPrime);
 
 
-    if(!(P.hasSquare())) {
-        D = N.sub(dPrime);
+    if((P.hasSquare())) {
+      console.log('has square')
+      D = d; // D is secret key
     } else {
-      D = d // D is secret key
+      D = N.sub(dPrime);
+      //D = d;
     }
     
     let secretKeyMessageConcat =  Buffer.concat([D.toBuffer(), e.toBuffer()]);
@@ -84,34 +86,76 @@ Schnorr.prototype._findSignature = function(d, e) {
     
     R = G.mul(k0);
 
-    if(!(R.hasSquare())) {
-      k = N.sub(k0);
+    // Find deterministic k
+    if(R.hasSquare()) {
+      console.log('has square');
+      k = k0;
     } else {
-      k = new BN(k0);
+      k = N.sub(k0);
+      //k = k0;
     }
     
     // e = int_from_bytes(tagged_hash("BIPSchnorr", bytes_from_point(R) + bytes_from_point(P) + msg)) % n
-    let pointConcat= taggedHash("BIPSchnorr", Buffer.concat([R.getX().toBuffer(), P.getX().toBuffer(), e.toBuffer()])); //Buffer
-    let  e_sig = (BN.fromBuffer(pointConcat)).mod(N);
+    let concatBytes = Buffer.concat([R.getX().toBuffer(), P.getX().toBuffer(), e.toBuffer()]);
+    let pointConcat= taggedHash("BIPSchnorr", concatBytes); //Buffer
+    let eSig = BN.fromBuffer(pointConcat).mod(N);
 
-    let sig_operation = k.add(e_sig.mul(D)); // k + ed
-    //let half_sig_buffer = new BN(new Number(sig_operation.toNumber().valueOf() % N.toNumber().valueOf())); // Bytes((k+ ed) mod n)
-    let half_sig_buffer = sig_operation.mod(N);
+    console.log(eSig.toString());
     
-    let sig = Buffer.concat([R.getX().toBuffer(), half_sig_buffer.toBuffer()]);
+    let a = (eSig.mul(D)).add(k);
+    // let sig_operation = k.add(a); // k + ed
+    let half_sig = a.mod(N);
+    
+    let sig = Buffer.concat([R.getX().toBuffer(), half_sig.toBuffer()]);
 
     let r = sig.slice(0, 32);
     let s = sig.slice(32, 64);
 
     return {
-      r: r,
-      s: s
+      r: BN.fromBuffer(r),
+      s: BN.fromBuffer(s)
     };
-  
   };
 
   Schnorr.prototype.sigError = function() {
+    if (!BufferUtil.isBuffer(this.hashbuf) || this.hashbuf.length !== 32) {
+      return 'hashbuf must be a 32 byte buffer';
+    }
+
+    var sigBuf = this.sig.toBuffer();
+
+    if(sigBuf.length !== 64) {
+      return 'signature must be a 64 byte array';
+    }
+
+    if(this.pubkey.toBuffer().length !== 32) {
+      return 'public key must be a 32 bytes';
+    } 
+
+    //Check if point P is none.
+    var pointObj = this.pubkey.pointObject();
+    var pubkeyPoint = new Point(pointObj.x, pointObj.y);
+    if(pubkeyPoint.isInfinity()) return true;
     
+    var r = this.sig.r;
+    var s = this.sig.s;
+
+    let p = new BN('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F', 'hex');
+    let n = Point.getN();
+
+    if(r.gte(p) || s.gte(n)) {
+      return true;
+    }
+
+    let e = BN.fromBuffer(taggedHash("BIPSchnorr", Buffer.concat([r.toBuffer(),this.pubkey.toBuffer(), this.hashbuf]))).mod(n);
+
+    // G.mul(s);
+    // P.mul(n.sub(e));
+    let R = (G.mul(s)).add(P.mul(n.sub(e)));
+    if(R.isInfinity() || !R.hasSquare() || !R.getX().eq(r)) {
+      return true;
+    }
+    return false;
   };
 
   Schnorr.prototype.verify = function() {
@@ -123,11 +167,31 @@ Schnorr.prototype._findSignature = function(d, e) {
     return this;
   };
 
-
-
+  /*
+  ** @param {tag | string}
+  ** @param {bytesSecretKeyMessage | Buffer} buffer to be hashed
+  **/
   function taggedHash(tag,bytesSecretKeyMessage) {
-    return Hash.sha256(Buffer.concat([Hash.sha256(Buffer.from(tag, 'utf-8')), Hash.sha256(Buffer.from(tag, 'utf-8')), bytesSecretKeyMessage]))
+    let tagHash = Hash.sha256(Buffer.from(tag, 'utf-8'));
+    return Hash.sha256(Buffer.concat([tagHash, tagHash, bytesSecretKeyMessage]));
   }
+
+  Schnorr.sign = function(hashbuf, privkey, endian) {
+    return Schnorr().set({
+      hashbuf: hashbuf,
+      endian: endian,
+      privkey: privkey
+    }).sign().sig;
+  };
+  
+  Schnorr.verify = function(hashbuf, sig, pubkey, endian) {
+    return Schnorr().set({
+      hashbuf: hashbuf,
+      endian: endian,
+      sig: sig,
+      pubkey: pubkey
+    }).verify().verified;
+  };
 
   module.exports = Schnorr;
 
