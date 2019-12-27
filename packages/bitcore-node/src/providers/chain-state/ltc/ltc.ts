5abcec5272cd9c4f6014bde170873953416ed03f
Justin Langston <justin@bitpay.com>
2019-12-27 15:42:33 -0500

    fee structure same as btc

--- packages/bitcore-node/src/providers/chain-state/ltc/ltc.ts
+++ packages/bitcore-node/src/providers/chain-state/ltc/ltc.ts
@@ -1,13 +1,7 @@
 import { BTCStateProvider } from '../btc/btc';
-import { CSP } from '../../../types/namespaces/ChainStateProvider';
 
 export class LTCStateProvider extends BTCStateProvider {
   constructor(chain: string = 'LTC') {
     super(chain);
-  }
-
-  async getFee(params: CSP.GetEstimateSmartFeeParams) {
-    const { chain, network } = params;
-    return { feerate: await this.getRPC(chain, network).getEstimateFee() };
   }
 }
