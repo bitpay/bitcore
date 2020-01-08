c6ebec7ca2de1b2d499e7596d6a48aed12288f98
Bernard Snowden <bsnowden@bitpay.com>
2020-01-08 13:36:21 -0500

    add schnorr sig to sighash sign/verify

--- packages/bitcore-lib-cash/lib/crypto/signature.js
+++ packages/bitcore-lib-cash/lib/crypto/signature.js
@@ -64,7 +64,7 @@ Signature.fromCompact = function(buf) {
 
 Signature.fromDER = Signature.fromBuffer = function(buf, strict) {
   // Schnorr Signatures use 64/65 byte for in tx r [len] 32 , s [len] 32, nhashtype 
-  if((buf.length === 64 || buf.length === 65) && !Signature.isDER(buf)) {
+  if((buf.length === 64 || buf.length === 65)) {
     let obj = Signature.parseSchnorrEncodedSig(buf);
     let sig = new Signature();
     sig.r = obj.r;
@@ -208,12 +208,10 @@ Signature.prototype.toBuffer = Signature.prototype.toDER = function() {
   var rnbuf = this.r.toBuffer();
   var snbuf = this.s.toBuffer();
 
-  if(this.isSchnorr && !this.nhashtype) {
+  if(this.isSchnorr) {
     return Buffer.concat([rnbuf, snbuf]);
-  } else if (this.isSchnorr && this.nhashtype) {
-    return Buffer.concat([rnbuf, snbuf, this.nhashtype]);
   }
-
+  
   var rneg = rnbuf[0] & 0x80 ? true : false;
   var sneg = snbuf[0] & 0x80 ? true : false;
 
