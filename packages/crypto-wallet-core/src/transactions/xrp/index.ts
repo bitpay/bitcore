6afb6c031db72fea2c827abfb908ddf79131d0f7
Micah Riggan <micahriggan@gmail.com>
2020-01-13 11:10:08 -0500

    Adding max ledger offset and max ledger version to cwc

--- packages/crypto-wallet-core/src/transactions/xrp/index.ts
+++ packages/crypto-wallet-core/src/transactions/xrp/index.ts
@@ -4,13 +4,16 @@ import { RippleAPI } from 'ripple-lib';
 // tslint:disable-next-line:no-submodule-imports
 import { Payment } from 'ripple-lib/dist/npm/transaction/payment';
 // tslint:disable-next-line:no-submodule-imports
-import { Instructions, TransactionJSON } from 'ripple-lib/dist/npm/transaction/types';
+import {
+  Instructions,
+  TransactionJSON
+} from 'ripple-lib/dist/npm/transaction/types';
 import { Key } from '../../derivation';
 
 enum HashPrefix {
   // transaction plus signature to give transaction ID
-  livenet = 0x54584E00,
-  mainnet = 0x54584E00,
+  livenet = 0x54584e00,
+  mainnet = 0x54584e00,
   testnet = 0x73747800
 }
 export class XRPTxProvider {
@@ -21,8 +24,19 @@ export class XRPTxProvider {
     invoiceID?: string;
     fee: number;
     nonce: number;
+    maxLedgerVersionOffset: number;
+    maxLedgerVersion: number;
   }) {
-    const { recipients, tag, from, invoiceID, fee, nonce } = params;
+    const {
+      recipients,
+      tag,
+      from,
+      invoiceID,
+      fee,
+      nonce,
+      maxLedgerVersionOffset,
+      maxLedgerVersion
+    } = params;
     const { address, amount } = recipients[0];
     const rippleAPI = new RippleAPI();
     const { schemaValidate } = RippleAPI._PRIVATE.schemaValidator;
@@ -48,7 +62,8 @@ export class XRPTxProvider {
     const instructions: Instructions = {
       fee: feeStr,
       sequence: nonce,
-      maxLedgerVersion: null,
+      maxLedgerVersion,
+      maxLedgerVersionOffset
     };
     const txJSON: TransactionJSON = {
       TransactionType: 'Payment',
@@ -67,12 +82,16 @@ export class XRPTxProvider {
       payment.destination.tag = tag;
       txJSON.DestinationTag = tag;
     }
-    schemaValidate('preparePaymentParameters', { address: from, payment, instructions});
+    schemaValidate('preparePaymentParameters', {
+      address: from,
+      payment,
+      instructions
+    });
     schemaValidate('tx-json', txJSON);
     return rippleBinaryCodec.encode(txJSON);
   }
 
-  getSignatureObject (params: { tx: string; key: Key; }) {
+  getSignatureObject(params: { tx: string; key: Key }) {
     const { tx, key } = params;
     const txJSON = rippleBinaryCodec.decode(tx);
     let rippleAPI = new RippleAPI();
@@ -83,29 +102,33 @@ export class XRPTxProvider {
     return signedTx;
   }
 
-  getSignature (params: { tx: string; key: Key; }): string {
+  getSignature(params: { tx: string; key: Key }): string {
     const { signedTransaction } = this.getSignatureObject(params);
     return signedTransaction;
   }
 
-  getHash(params: { tx: string; network?: string; }): string {
+  getHash(params: { tx: string; network?: string }): string {
     const { tx, network = 'mainnet' } = params;
     const prefix = HashPrefix[network].toString(16).toUpperCase();
     return this.sha512Half(prefix + tx);
   }
 
-  applySignature(params: { tx: string; signature: string; }): string {
+  applySignature(params: { tx: string; signature: string }): string {
     const { signature } = params;
     return signature;
   }
 
-  sign(params: { tx: string; key: Key; }): string {
+  sign(params: { tx: string; key: Key }): string {
     const { tx, key } = params;
-    const signature = this.getSignature({tx, key});
-    return this.applySignature({tx, signature});
+    const signature = this.getSignature({ tx, key });
+    return this.applySignature({ tx, signature });
   }
 
   sha512Half(hex: string): string {
-    return createHash('sha512').update(Buffer.from(hex, 'hex')).digest('hex').toUpperCase().slice(0, 64);
+    return createHash('sha512')
+      .update(Buffer.from(hex, 'hex'))
+      .digest('hex')
+      .toUpperCase()
+      .slice(0, 64);
   }
 }
