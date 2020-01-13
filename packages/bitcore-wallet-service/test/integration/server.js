03de34f5bcde474d4ebdbe7ee969dc7ada917866
Matias Alejo Garcia <ematiu@gmail.com>
2020-01-13 15:06:37 -0300

    rm console.log

--- packages/bitcore-wallet-service/test/integration/server.js
+++ packages/bitcore-wallet-service/test/integration/server.js
@@ -3505,7 +3505,6 @@ describe('Wallet service', function() {
 
 
             server.createTx(txOpts, function(err, tx) {
-console.log('[server.js.3502:err:]',err); // TODO
               should.not.exist(err);
               should.exist(tx);
               tx.walletM.should.equal(1);
