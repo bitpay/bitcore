b79c80efd7134955224e53b0b53c9ef071da16c6
Micah Riggan <micahriggan@gmail.com>
2020-01-20 17:15:52 -0500

    Fixing test since it doesn't hit rpc anymore

--- packages/bitcore-node/test/integration/ripple/csp.integration.ts
+++ packages/bitcore-node/test/integration/ripple/csp.integration.ts
@@ -1,5 +1,6 @@
 import { XRP } from '../../../src/modules/ripple/api/csp';
 import { expect } from 'chai';
+import { XrpBlockStorage } from '../../../src/modules/ripple/models/block';
 const chain = 'XRP';
 const network = 'mainnet';
 
@@ -12,8 +13,24 @@ describe('Ripple Api', () => {
   });
 
   it('should be able to get local tip', async () => {
+    await XrpBlockStorage.collection.insertOne({
+      chain,
+      network,
+      height: 5,
+      hash: '528f01c17829622ed6a4af51b3b3f6c062f304fa60e66499c9cbb8622c8407f7',
+      time: new Date(1526326784),
+      timeNormalized: new Date(1526326784),
+      transactionCount: 1,
+      reward: 50,
+      previousBlockHash: '64bfb3eda276ae4ae5b64d9e36c9c0b629bc767fb7ae66f9d55d2c5c8103a929',
+      nextBlockHash: '',
+      size: 264,
+      processed: true
+    });
+
     const tip = await XRP.getLocalTip({ chain, network });
     expect(tip).to.exist;
     expect(tip.hash).to.exist;
+    expect(tip.hash).to.eq('528f01c17829622ed6a4af51b3b3f6c062f304fa60e66499c9cbb8622c8407f7');
   });
 });
