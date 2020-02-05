d7f072889bf3cafbf00215ea84ad6e3eb62245f6
sbhatnagar <sbhatnagar@bitpay.com>
2020-02-05 17:01:15 -0500

    bch properly extends btch

--- packages/crypto-wallet-core/src/transactions/bch/index.ts
+++ packages/crypto-wallet-core/src/transactions/bch/index.ts
@@ -1,6 +1,8 @@
-export class BCHTxProvider {
+import { BTCTxProvider } from '../btc';
+
+export abstract class BCHTxProvider extends BTCTxProvider {
   lib = require('bitcore-lib-cash');
-  create({ recipients, utxos, change, fee }) {
+  create({ recipients, utxos = [], change, fee = 20000 }) {
     let tx = new this.lib.Transaction().from(utxos).feePerByte(Number(fee));
     for (const recipient of recipients) {
       tx.to(recipient.address, recipient.amount);
