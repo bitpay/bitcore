50cc1f3d37f9b492e61fe4badd23827887166d31
Matias Alejo Garcia <ematiu@gmail.com>
2020-03-12 07:29:21 -0300

    implement incremental runs

--- packages/bitcore-wallet-service/src/lib/updatestats.ts
+++ packages/bitcore-wallet-service/src/lib/updatestats.ts
@@ -1,9 +1,16 @@
 import * as async from 'async';
 import moment from 'moment';
 import * as mongodb from 'mongodb';
+const ObjectID = mongodb.ObjectID;
+
 
 const storage = require('./storage');
-const INITIAL_DATE = '2015-01-01';
+const LAST_DAY = '2019-12-01';
+
+
+var objectIdFromDate = function (date) {
+  return Math.floor(date.getTime() / 1000).toString(16) + "0000000000000000";
+};
 
 export class UpdateStats {
   from: moment.MomentFormatSpecification;
@@ -11,8 +18,6 @@ export class UpdateStats {
   db: mongodb.Db;
 
   constructor() {
-    this.from = moment(INITIAL_DATE).format('YYYY-MM-DD');
-    this.to = moment().format('YYYY-MM-DD');
   }
 
   run(config, cb) {
@@ -24,6 +29,7 @@ export class UpdateStats {
       uri = uri + '?';
     }
     uri = uri + 'readPreference=secondaryPreferred';
+    console.log('Connected to ',uri); 
     mongodb.MongoClient.connect(uri, (err, db) => {
       if (err) {
         console.log('Unable to connect to the mongoDB', err);
@@ -41,15 +47,15 @@ export class UpdateStats {
     async.series(
       [
         next => {
-          console.log("Updating new wallets stats...");
+          console.log("## Updating new wallets stats...");
           this._updateNewWallets(next);
         },
         next => {
-          console.log("Updating tx proposals stats...");
+          console.log("## Updating tx proposals stats...");
           this._updateTxProposals(next);
         },
         next => {
-          console.log("Updating fiat rates stats...");
+          console.log("## Updating fiat rates stats...");
           this._updateFiatRates(next);
         }
       ],
@@ -90,27 +96,27 @@ export class UpdateStats {
         }
         if (res.length !== 0) {
           try {
-            console.log(`Checking if stats_wallets table exist.`);
+            console.log(`\tChecking if stats_wallets table exist.`);
             if (!this.db.collection('stats_wallets').find()) {
-              console.log(`stats_wallets table does not exist.`);
-              console.log(`Creating stats_wallets table.`);
+              console.log(`\tstats_wallets table does not exist.`);
+              console.log(`\tCreating stats_wallets table.`);
               await this.db.createCollection('stats_wallets');
             }
-            console.log(`Cleaning stats_wallets table.`);
+            console.log(`\tCleaning stats_wallets table.`);
             await this.db
               .collection('stats_wallets')
               .remove({})
               .then(async () => {
-                console.log(`Trying to insert ${res.length} entries`);
+                console.log(`\tTrying to insert ${res.length} entries`);
                 const opts: any = { ordered: false };
                 await this.db.collection('stats_wallets').insert(res, opts);
                 console.log(`${res.length} entries inserted in stats_wallets`);
               });
           } catch (err) {
-            console.log('Cannot insert into stats_wallets:', err);
+            console.log('!! Cannot insert into stats_wallets:', err);
           }
         } else {
-          console.log("No data to update in stats_wallets");
+          console.log("\tNo data to update in stats_wallets");
         }
         return cb();
       });
@@ -144,18 +150,18 @@ export class UpdateStats {
       ])
       .toArray(async (err, res) => {
         if (err) {
-          console.log('Update fiat rates stats throws error:', err);
+          console.log('!! Update fiat rates stats throws error:', err);
           return cb(err);
         }
         if (res.length !== 0) {
           try {
-            console.log(`Checking if stats_fiat_rates table exist.`);
+            console.log(`\tChecking if stats_fiat_rates table exist.`);
             if (!this.db.collection('stats_fiat_rates').find()) {
-              console.log(`stats_fiat_rates table does not exist.`);
-              console.log(`Creating stats_fiat_rates table.`);
+              console.log(`\tstats_fiat_rates table does not exist.`);
+              console.log(`\tCreating stats_fiat_rates table.`);
               await this.db.createCollection('stats_fiat_rates');
             }
-            console.log(`Cleaning stats_fiat_rates table.`);
+            console.log(`\tCleaning stats_fiat_rates table.`);
             await this.db
               .collection('stats_fiat_rates')
               .remove({})
@@ -166,25 +172,51 @@ export class UpdateStats {
                 console.log(`${res.length} entries inserted in stats_fiat_rates`);
               });
           } catch (err) {
-            console.log('Cannot insert into stats_fiat_rates:', err);
+            console.log('!! Cannot insert into stats_fiat_rates:', err);
           }
         } else {
-          console.log("No data to update in stats_fiat_rates");
+          console.log("\tNo data to update in stats_fiat_rates");
         }
         return cb();
       });
   }
 
-  _updateTxProposals(cb) {
+  async lastRun(coll){ 
+    // Grab last run
+    let cursor = await  this.db
+      .collection(coll)
+      .find({},{"_id":true})
+      .sort({"_id":-1})
+      .limit(1);
+
+    let last = await cursor.next();
+    let lastDay = LAST_DAY;
+    if (last && last._id) {
+      lastDay = last._id.day;
+      console.log(`Last day is ${lastDay}`);
+    }
+   console.log(`Last run of ${coll}:  ${lastDay}`);
+    return lastDay;
+  };
+
+  async _updateTxProposals(cb) {
+    let lastDay = await this.lastRun('stats_txps');
+    let od = objectIdFromDate(new Date(lastDay));
     this.db
       .collection(storage.Storage.collections.TXS)
       .aggregate([
+        {
+          $match: {
+            _id: { $gt: new ObjectID(od) },
+          }
+        },
         {
           $project: {
             date: { $add: [new Date(0), { $multiply: ['$createdOn', 1000] }] },
             network: '$network',
             coin: '$coin',
-            amount: '$amount'
+            amount: '$amount',
+            id: "$_id",
           }
         },
         {
@@ -195,38 +227,40 @@ export class UpdateStats {
               coin: '$coin'
             },
             amount: { $sum: '$amount' },
-            count: { $sum: 1 }
+            count: { $sum: 1 },
           }
-        }
+        },
       ])
       .toArray(async (err, res) => {
         if (err) {
-          console.log('Update txps stats throws error:', err);
+          console.log('!! Update txps stats throws error:', err);
           return cb(err);
         }
         if (res.length !== 0) {
           try {
-            console.log(`Checking if stats_txps table exist.`);
+            console.log(`\tChecking if stats_txps table exist.`);
             if (!this.db.collection('stats_txps').find()) {
-              console.log(`stats_txps table does not exist.`);
-              console.log(`Creating stats_txps table.`);
+              console.log(`\tstats_txps table does not exist.`);
+              console.log(`\tCreating stats_txps table.`);
               await this.db.createCollection('stats_txps');
             }
-            console.log(`Cleaning stats_txps table.`);
+            console.log(`\tRemoving entries from/after ${lastDay}`);
             await this.db
               .collection('stats_txps')
-              .remove({})
-              .then(async () => {
-                console.log(`Trying to insert ${res.length} entries`);
+              .remove({  "_id.day": { $gte: lastDay }  }) 
+              .then(async (err) => {
+                // rm day = null
+                res = res.filter( x => x._id.day );
+                console.log(`\tTrying to insert ${res.length} entries`);
                 const opts: any = { ordered: false };
                 await this.db.collection('stats_txps').insert(res, opts);
-                console.log(`${res.length} entries inserted in stats_txps`);
+                console.log(`\t${res.length} entries inserted in stats_txps`);
               });
           } catch (err) {
-            console.log('Cannot insert into stats_txps:', err);
+            console.log('!! Cannot insert into stats_txps:', err);
           }
         } else {
-          console.log("No data to update in stats_txps");
+          console.log("\tNo data to update in stats_txps");
         }
         return cb();
       });
