77a3aba69adfb08e35bc121c2472c313600862a4
Micah Riggan <micahriggan@gmail.com>
2020-04-09 14:00:25 -0400

    Working on tests for transform

--- packages/bitcore-node/test/integration/ripple/csp.integration.ts
+++ packages/bitcore-node/test/integration/ripple/csp.integration.ts
@@ -1,10 +1,11 @@
-import { XRP } from '../../../src/modules/ripple/api/csp';
 import { expect } from 'chai';
+import { XRP } from '../../../src/modules/ripple/api/csp';
 import { XrpBlockStorage } from '../../../src/modules/ripple/models/block';
+import { IXrpTransaction } from '../../../src/modules/ripple/types';
 const chain = 'XRP';
-const network = 'mainnet';
+const network = 'testnet';
 
-describe('Ripple Api', () => {
+describe.only('Ripple Api', () => {
   it('should be able to get the ledger', async () => {
     const client = await XRP.getClient(network);
     const ledger = await client.getLedger();
@@ -32,5 +33,28 @@ describe('Ripple Api', () => {
     expect(tip).to.exist;
     expect(tip.hash).to.exist;
     expect(tip.hash).to.eq('528f01c17829622ed6a4af51b3b3f6c062f304fa60e66499c9cbb8622c8407f7');
+  });
+
+  it('should transform a ripple rpc response into a bitcore transaction', async () => {
+    const txs = await XRP.getAddressTransactions({
+      chain,
+      network: 'mainnet',
+      address: 'rN33DVnneYUUgTmcxXnXvgAL1BECuLZ8pm',
+      args: {}
+    });
+    for (const tx of txs) {
+      const bitcoreTx = (await XRP.transform(tx, 'mainnet')) as IXrpTransaction;
+      expect(bitcoreTx).to.have.property('chain');
+      console.log(JSON.stringify({ tx, bitcoreTx }, null, 2));
+      expect(tx.address).to.eq(bitcoreTx.from);
+      expect(tx.outcome.ledgerVersion).to.eq(bitcoreTx.blockHeight);
+      expect(tx.outcome.fee).to.eq((bitcoreTx.fee / 1e6).toString());
+      expect(Object.keys(tx.outcome.balanceChanges)).to.contain(bitcoreTx.to!);
+      expect(Number(tx.outcome.balanceChanges[bitcoreTx.from][0].value)).to.be.lt(0);
+      if (tx.outcome.deliveredAmount) {
+        expect(tx.outcome.deliveredAmount!.value).to.eq((bitcoreTx.value / 1e6).toString());
+        expect(Number(tx.outcome.balanceChanges[bitcoreTx.to!][0].value)).to.be.gt(0);
+      }
+    }
   });
 });
