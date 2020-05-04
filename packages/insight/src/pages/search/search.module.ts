bdf71500bb2fc6df4290f7a316c232a115c4ed14
Juan Pablo Abuin <juanpabloabuin@gmail.com>
2020-05-04 17:31:29 -0300

    search page

---
+++ packages/insight/src/pages/search/search.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DailyTransactionChartComponentModule } from '../../components/daily-transaction-chart/daily-transaction-chart.module';
+import { ErrorComponentModule } from '../../components/error/error.module';
+import { FooterComponentModule } from '../../components/footer/footer.module';
+import { HeadNavComponentModule } from '../../components/head-nav/head-nav.module';
+import { LoaderComponentModule } from '../../components/loader/loader.module';
+import { SearchPage } from './search';
+
+@NgModule({
+  declarations: [SearchPage],
+  imports: [
+    IonicPageModule.forChild(SearchPage),
+    DailyTransactionChartComponentModule,
+    FooterComponentModule,
+    HeadNavComponentModule,
+    LoaderComponentModule,
+    ErrorComponentModule
+  ],
+  exports: [SearchPage]
+})
+export class SearchPageModule {}
