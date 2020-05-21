f87a47e9869d9e319668eb6d5aec9362127e2281
Micah Riggan <micahriggan@gmail.com>
2020-05-21 17:08:06 -0400

    Network in test should be regtest

--- packages/bitcore-node/test/integration/bitcoin/csp.integration.ts
+++ packages/bitcore-node/test/integration/bitcoin/csp.integration.ts
@@ -4,7 +4,7 @@ import { ChainStateProvider } from '../../../src/providers/chain-state';
 describe('Bitcoin API', function() {
   it('should be able to get the fees', async () => {
     const chain = 'BTC';
-    const network = 'testnet';
+    const network = 'regtest';
     const target = 1;
     const cacheKey = `getFee-${chain}-${network}-${target}`;
     const fee = await ChainStateProvider.getFee({ chain, network, target });
