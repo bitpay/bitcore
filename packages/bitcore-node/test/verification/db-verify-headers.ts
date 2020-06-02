da8445c66e155ff55fce2e5cae3615ea12fc453b
Micah Riggan <micahriggan@gmail.com>
2020-06-02 17:21:35 -0400

    Swapping to forEach

--- packages/bitcore-node/test/verification/db-verify-headers.ts
+++ packages/bitcore-node/test/verification/db-verify-headers.ts
@@ -15,49 +15,46 @@ if (require.main === module) {
 
     Modules.loadConfigured();
     await Storage.start();
-
-    const cursor = BitcoinBlockStorage.collection
-      .find({ chain, network, height: { $gte: resumeHeight } })
-      .sort({ height: -1 })
-      .addCursorFlag('noCursorTimeout', true);
     let prevMatch = true;
     let nextMatch = true;
     let previousBlock: IBlock | undefined;
-    let locatorBlock: IBlock | undefined;
     let checkHeight = resumeHeight;
 
     console.log('Verifying headers for', chain, network, 'from height', resumeHeight);
-    while (await cursor.hasNext()) {
-      let success = true;
-      locatorBlock = (await cursor.next()) as IBlock;
-      if (checkHeight !== locatorBlock.height) {
-        const error = {
-          model: 'block',
-          err: true,
-          type: 'MISSING_BLOCK',
-          payload: { blockNum: checkHeight }
-        };
-        console.log(JSON.stringify(error));
-        success = false;
-      } else if (previousBlock) {
-        prevMatch = prevMatch && locatorBlock.nextBlockHash === previousBlock.hash;
-        nextMatch = nextMatch && locatorBlock.hash === previousBlock.previousBlockHash;
-        if (!prevMatch || !nextMatch) {
+
+    await BitcoinBlockStorage.collection
+      .find({ chain, network, height: { $gte: resumeHeight } })
+      .sort({ height: -1 })
+      .forEach(locatorBlock => {
+        let success = true;
+        if (checkHeight !== locatorBlock.height) {
           const error = {
             model: 'block',
             err: true,
-            type: 'CORRUPTED_BLOCK',
-            payload: { blockNum: locatorBlock.height, txCount: locatorBlock.transactionCount }
+            type: 'MISSING_BLOCK',
+            payload: { blockNum: checkHeight }
           };
           console.log(JSON.stringify(error));
           success = false;
+        } else if (previousBlock) {
+          prevMatch = prevMatch && locatorBlock.nextBlockHash === previousBlock.hash;
+          nextMatch = nextMatch && locatorBlock.hash === previousBlock.previousBlockHash;
+          if (!prevMatch || !nextMatch) {
+            const error = {
+              model: 'block',
+              err: true,
+              type: 'CORRUPTED_BLOCK',
+              payload: { blockNum: locatorBlock.height, txCount: locatorBlock.transactionCount }
+            };
+            console.log(JSON.stringify(error));
+            success = false;
+          }
         }
-      }
 
-      previousBlock = locatorBlock;
-      checkHeight++;
-      console.log({ block: checkHeight, success });
-    }
+        previousBlock = locatorBlock;
+        checkHeight++;
+        console.log({ block: checkHeight, success });
+      });
     process.exit(0);
   })();
 }
