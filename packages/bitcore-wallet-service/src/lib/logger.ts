7fad6280af7cc7355428506ab9aecb42c043c308
Matias Alejo Garcia <ematiu@gmail.com>
2020-07-20 13:33:54 -0300

    fix tests

--- packages/bitcore-wallet-service/src/lib/logger.ts
+++ packages/bitcore-wallet-service/src/lib/logger.ts
@@ -1,12 +1,12 @@
 import * as winston from 'winston';
 // const logLevel = args.DEBUG ? 'debug' : 'info';
 //
-export const transport =     new winston.transports.Console({
+export const transport = new winston.transports.Console({
   level: 'debug' // TODO logLevel
 });
 
-export const  logger = winston.createLogger({
-  transports: [ transport ],
+export const logger = winston.createLogger({
+  transports: [transport]
 });
 
 const timezone = new Date()
