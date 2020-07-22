c65696c3e2dd40e65a235823f3f12ffe33667aed
Matias Alejo Garcia <ematiu@gmail.com>
2020-07-22 11:19:38 -0300

    activate schnoor for BCH livenet

--- packages/bitcore-wallet-service/src/lib/expressapp.ts
+++ packages/bitcore-wallet-service/src/lib/expressapp.ts
@@ -861,6 +861,7 @@ export class ExpressApp {
       });
     });
 
+    // DEPRECATEED
     router.post('/v1/txproposals/:id/signatures/', (req, res) => {
       getServerWithAuth(req, res, server => {
         req.body.maxTxpVersion = 3;
@@ -873,11 +874,15 @@ export class ExpressApp {
       });
     });
 
+    // We created a new endpoint that support BCH schnorr signatues
+    // so we can safely throw the error "UPGRADE NEEDED" if an old
+    // client tries to post ECDSA signatures to a Schnorr TXP.
+    // (using the old /v1/txproposal method): if (txp.signingMethod === 'schnorr' && !opts.supportBchSchnorr) return cb(Errors.UPGRADE_NEEDED);
     router.post('/v2/txproposals/:id/signatures/', (req, res) => {
       getServerWithAuth(req, res, server => {
         req.body.txProposalId = req.params['id'];
         req.body.maxTxpVersion = 3;
-        req.body.useBchSchnorr = true;
+        req.body.supportBchSchnorr = true;
         server.signTx(req.body, (err, txp) => {
           if (err) return returnError(err, res, req);
           res.json(txp);
