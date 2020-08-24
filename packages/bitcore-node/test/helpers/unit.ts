b1f120f46da84f77dd3534581da4142968542fd2
Micah Riggan <micahriggan@gmail.com>
2020-08-24 14:58:32 -0400

    Global timeout on bitcore-node

--- packages/bitcore-node/test/helpers/unit.ts
+++ packages/bitcore-node/test/helpers/unit.ts
@@ -3,8 +3,10 @@ import { Modules } from '../../src/modules';
 let loaded = false;
 export async function unitBeforeHelper() {
   if (!loaded) {
+    console.time('Loading Modules');
     Modules.loadConfigured();
     loaded = true;
+    console.timeEnd('Loading Modules');
   }
 }
 
--- packages/bitcore-node/test/unit/logger.spec.ts
+++ packages/bitcore-node/test/unit/logger.spec.ts
@@ -1,6 +1,10 @@
 import { expect } from 'chai';
+import { unitAfterHelper, unitBeforeHelper } from '../helpers/unit';
 
 describe('logger', function() {
+  before(unitBeforeHelper);
+  after(unitAfterHelper);
+
   it('should have a test which runs', function() {
     expect(true).to.equal(true);
   });
