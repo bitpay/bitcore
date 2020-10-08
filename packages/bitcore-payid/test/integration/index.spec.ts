982f49311c28c32603f7c920e8dc2b5f8b64b55a
Kenny Joseph <kjoseph@bitpay.com>
2020-10-08 17:14:49 -0400

    Implemented sign; Removed jose dependency;

--- packages/bitcore-payid/test/integration/index.spec.ts
+++ packages/bitcore-payid/test/integration/index.spec.ts
@@ -1,12 +1,14 @@
 import {
   AddressDetailsType,
   IdentityKeySigningParams,
-  sign
+  sign,
+  toKey
 } from '@payid-org/utils';
+import Bitcore from 'bitcore-lib';
 import { expect } from 'chai';
 import crypto from 'crypto';
-import { JWK } from 'jose';
 import sinon from 'sinon';
+import * as errors from '../../src/errors';
 import PayId from '../../src/index';
 
 describe('PayId', () => {
@@ -17,7 +19,13 @@ describe('PayId', () => {
   const payId = 'test$example.com';
 
   before(() => {
-    keys = crypto.generateKeyPairSync('ec', { namedCurve: 'secp256k1' });
+    keys = {
+      bitcoreHD: new Bitcore.HDPrivateKey(),
+      bitcore: new Bitcore.PrivateKey(),
+      ec: crypto.generateKeyPairSync('ec', { namedCurve: 'secp256k1', privateKeyEncoding: { format: 'pem', type: 'pkcs8' }, publicKeyEncoding: { format: 'pem', type: 'spki' } }),
+      rsa: crypto.generateKeyPairSync('rsa', { modulusLength: 2048, privateKeyEncoding: { format: 'pem', type: 'pkcs8' }, publicKeyEncoding: { format: 'pem', type: 'spki' } }),
+      sym: crypto.createSecretKey(crypto.randomBytes(32)).export()
+    };
 
     addressBTC = {
       paymentNetwork: 'BTC',
@@ -44,11 +52,100 @@ describe('PayId', () => {
     };
   });
 
+  describe('sign', () => {
+    it('should sign with Bitcore HD key', () => {
+      const signed = PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.bitcoreHD.toString());
+
+      expect(signed).to.exist;
+      expect(signed).to.have.property('payload');
+      expect(signed.payload).to.be.a.string;
+      expect(signed).to.have.property('signatures');
+      expect(signed.signatures.length).to.equal(1);
+    });
+
+    it('should sign with Bitcore non-HD key', () => {
+      const signed = PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.bitcore.toString());
+
+      expect(signed).to.exist;
+      expect(signed).to.have.property('payload');
+      expect(signed.payload).to.be.a.string;
+      expect(signed).to.have.property('signatures');
+      expect(signed.signatures.length).to.equal(1);
+    });
+
+    it('should sign with crypto-created EC key', () => {
+      const signed = PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.ec.privateKey);
+
+      expect(signed).to.exist;
+      expect(signed).to.have.property('payload');
+      expect(signed.payload).to.be.a.string;
+      expect(signed).to.have.property('signatures');
+      expect(signed.signatures.length).to.equal(1);
+    });
+
+    it('should sign with crypto-created RSA key', () => {
+      const signed = PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.rsa.privateKey);
+
+      expect(signed).to.exist;
+      expect(signed).to.have.property('payload');
+      expect(signed.payload).to.be.a.string;
+      expect(signed).to.have.property('signatures');
+      expect(signed.signatures.length).to.equal(1);
+    });
+
+    it('should fail signing with Bitcore HD public key', () => {
+      try {
+        const pk = new Bitcore.HDPublicKey(keys.bitcoreHD);
+        PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', pk.toString());
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.REQUIRE_PRIVATE_KEY);
+      }
+    });
+
+    it('should fail signing with Bitcore non-HD public key', () => {
+      try {
+        const pk = new Bitcore.PublicKey(keys.bitcore);
+        PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', pk.toString());
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.REQUIRE_PRIVATE_KEY);
+      }
+    });
+
+    it('should fail signing with EC public key', () => {
+      try {
+        PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.ec.publicKey);
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.REQUIRE_PRIVATE_KEY);
+      }
+    });
+
+    it('should fail signing with RSA public key', () => {
+      try {
+        PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.rsa.publicKey);
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.REQUIRE_PRIVATE_KEY);
+      }
+    });
+
+    it('should fail signing with symmetric key', () => {
+      try {
+        PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.sym);
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.NO_SYNC_KEY__PRIVATE);
+      }
+    });
+  });
+
   describe('verify', () => {
     let signingParams;
 
     beforeEach(() => {
-      const pk = JWK.asKey(keys.privateKey);
+      const pk = toKey(keys.ec.privateKey);
       signingParams = new IdentityKeySigningParams(pk, pk.alg || 'ES256K');
     });
 
@@ -117,6 +214,133 @@ describe('PayId', () => {
         const verified = PayId.verify(payId, address);
         expect(verified).be.true;
       });
+    });
+  });
+
+  describe('sign & verify', () => {
+    it('should work with Bitcore HD key', () => {
+      const signed = PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.bitcoreHD.toString());
+      expect(signed).to.exist;
+
+      const verified = PayId.verify(payId, signed);
+      expect(verified).be.true;
+    });
+
+    it('should work with Bitcore non-HD key', () => {
+      const signed = PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.bitcore.toString());
+      expect(signed).to.exist;
+
+      const verified = PayId.verify(payId, signed);
+      expect(verified).be.true;
+    });
+
+    it('should work with crypto-created EC key', () => {
+      const signed = PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.ec.privateKey);
+      expect(signed).to.exist;
+
+      const verified = PayId.verify(payId, signed);
+      expect(verified).be.true;
+    });
+
+    it('should work with crypto-created RSA key', () => {
+      const signed = PayId.sign(payId, addressBTC.addressDetails.address, 'BTC', keys.rsa.privateKey);
+      expect(signed).to.exist;
+
+      const verified = PayId.verify(payId, signed);
+      expect(verified).be.true;
+    });
+  });
+
+  describe('_convertIdentityKeyToJWK', () => {
+    let _buildJWKFromBitcore;
+    beforeEach(() => {
+      _buildJWKFromBitcore = sinon.spy(PayId, '_buildJWKFromBitcore');
+    });
+    afterEach(() => {
+      sinon.restore();
+    });
+    it('should convert Bitcore HD private key', () => {
+      const jwk = PayId['_convertIdentityKeyToJWK'](keys.bitcoreHD.toString());
+      expect(jwk).to.exist;
+      expect(jwk.kty).to.equal('EC');
+      expect(jwk.private).to.be.true;
+      expect(_buildJWKFromBitcore.callCount).to.equal(1);
+    });
+
+    it('should convert Bitcore non-HD private key', () => {
+      const jwk = PayId['_convertIdentityKeyToJWK'](keys.bitcore.toString());
+      expect(jwk).to.exist;
+      expect(jwk.kty).to.equal('EC');
+      expect(jwk.private).to.be.true;
+      expect(_buildJWKFromBitcore.callCount).to.equal(1);
+    });
+
+    it('should convert EC private key', () => {
+      const jwk = PayId['_convertIdentityKeyToJWK'](keys.ec.privateKey);
+      expect(jwk).to.exist;
+      expect(jwk.kty).to.equal('EC');
+      expect(jwk.private).to.be.true;
+      expect(_buildJWKFromBitcore.callCount).to.equal(0);
+    });
+
+    it('should convert RSA private key', () => {
+      const jwk = PayId['_convertIdentityKeyToJWK'](keys.rsa.privateKey);
+      expect(jwk).to.exist;
+      expect(jwk.kty).to.equal('RSA');
+      expect(jwk.private).to.be.true;
+      expect(_buildJWKFromBitcore.callCount).to.equal(0);
+    });
+
+    it('should fail to convert Bitcore HD public key', () => {
+      try {
+        const pk = new Bitcore.HDPublicKey(keys.bitcoreHD);
+        PayId['_convertIdentityKeyToJWK'](pk.toString());
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.REQUIRE_PRIVATE_KEY);
+        expect(_buildJWKFromBitcore.callCount).to.equal(0);
+      }
+    });
+
+    it('should fail to convert Bitcore non-HD public key', () => {
+      try {
+        const pk = new Bitcore.PublicKey(keys.bitcore);
+        PayId['_convertIdentityKeyToJWK'](pk.toString());
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.REQUIRE_PRIVATE_KEY);
+        expect(_buildJWKFromBitcore.callCount).to.equal(0);
+      }
+    });
+
+    it('should fail to convert EC public key', () => {
+      try {
+        PayId['_convertIdentityKeyToJWK'](keys.ec.publicKey);
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.REQUIRE_PRIVATE_KEY);
+        expect(_buildJWKFromBitcore.callCount).to.equal(0);
+      }
+    });
+
+    it('should fail to convert RSA public key', () => {
+      try {
+        PayId['_convertIdentityKeyToJWK'](keys.rsa.publicKey);
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.REQUIRE_PRIVATE_KEY);
+        expect(_buildJWKFromBitcore.callCount).to.equal(0);
+      }
+    });
+
+    it('should fail to convert symmetric key', () => {
+      try {
+        PayId['_convertIdentityKeyToJWK'](keys.sym);
+        throw new Error('Should have thrown');
+      } catch (err) {
+        expect(err.message).to.equal(errors.NO_SYNC_KEY__PRIVATE);
+        expect(_buildJWKFromBitcore.callCount).to.equal(0);
+      }
     });
 
   });
--- packages/bitcore-payid/test/unit/index.spec.ts
+++ packages/bitcore-payid/test/unit/index.spec.ts
@@ -1,9 +1,9 @@
+import Bitcore from 'bitcore-lib';
 import { expect } from 'chai';
+import crypto from 'crypto';
 import sinon from 'sinon';
+import PayId from '../../src/index';
 
 describe('PayId', () => {
-  it('should hit this placeholder unit test', () => {
-    expect(true).to.be.true;
-    return;
-  });
+  // Placeholder
 });
