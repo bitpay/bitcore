0df0a0770f4510245959c5325905605f761ad289
Gonzalo Leon <gonzalomleon@gmail.com>
2020-12-07 12:08:52 -0300

    Fix test to check missunderstoodOutputs prop

--- packages/bitcore-wallet-service/test/integration/history.js
+++ packages/bitcore-wallet-service/test/integration/history.js
@@ -995,6 +995,7 @@ describe('History', function() {
           amount: 0,
           action: 'sent',
           addressTo: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+          missunderstoodOutputs: false,
           outputs:
           [ { address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
             amount: 0 } ],
