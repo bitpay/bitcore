c74831ff5eb74e882dec90c81caa8ec2cf81f68d
Gonzalo Leon <gonzalomleon@gmail.com>
2020-12-31 11:38:37 -0300

    Fix serverMessages filter

--- packages/bitcore-wallet-service/src/serverMessages.ts
+++ packages/bitcore-wallet-service/src/serverMessages.ts
@@ -1,19 +1,23 @@
 module.exports = (serverMessages, wallet, appName, appVersion, userAgent) => {
   if (!serverMessages || !appVersion || !appName) return;
-  const _serverMessages = serverMessages.filter(msg => {
+  const _serverMessages = serverMessages.filter((msg) => {
     return (
       msg.walletNetwork == wallet.network &&
       (!msg.appName ||
         !msg.appName.length ||
-        (msg.appName && msg.appName.find(app => appName.toLowerCase().includes(app.toLowerCase())))) &&
+        (msg.appName && msg.appName.find((app) => appName.toLowerCase().includes(app.toLowerCase())))) &&
       ((msg.platforms && msg.platforms[0] === '*') ||
-        !msg.platforms ||
-        !msg.platforms.length ||
+        (!msg.platforms && !msg.exceptPlatforms) ||
+        (!msg.platforms.length && !msg.exceptPlatforms.length) ||
         (msg.platforms &&
-          msg.platforms.find(plat => userAgent.toLowerCase().includes(plat.toLowerCase())) &&
+          msg.platforms.length &&
+          msg.platforms.find((plat) => userAgent.toLowerCase().includes(plat.toLowerCase())) &&
           (!msg.exceptPlatforms ||
             !msg.exceptPlatforms.length ||
-            msg.platforms.find(plat => !userAgent.toLowerCase().includes(plat.toLowerCase())))))
+            msg.exceptPlatforms.find((plat) => !userAgent.toLowerCase().includes(plat.toLowerCase())))) ||
+        (msg.exceptPlatforms &&
+          msg.exceptPlatforms.length &&
+          msg.exceptPlatforms.find((plat) => !userAgent.toLowerCase().includes(plat.toLowerCase()))))
     );
   });
   return _serverMessages && _serverMessages.length > 0 ? _serverMessages[0].message : [];
