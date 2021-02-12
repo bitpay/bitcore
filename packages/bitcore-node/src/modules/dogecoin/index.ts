3e591986e9b65764b528cd0a4696cdc9a72461f3
Kenny Joseph <kjoseph@bitpay.com>
2021-02-12 16:40:49 -0500

    Doge support

---
+++ packages/bitcore-node/src/modules/dogecoin/index.ts
@@ -0,0 +1,14 @@
+import { BaseModule } from '..';
+import { DOGEStateProvider } from '../../providers/chain-state/doge/doge';
+import { DogecoinP2PWorker } from './p2p';
+import { VerificationPeer } from '../bitcoin/VerificationPeer';
+
+export default class DOGEModule extends BaseModule {
+  constructor(services) {
+    super(services);
+    services.Libs.register('DOGE', 'bitcore-lib', 'bitcore-p2p');
+    services.P2P.register('DOGE', DogecoinP2PWorker);
+    services.CSP.registerService('DOGE', new DOGEStateProvider());
+    services.Verification.register('DOGE', VerificationPeer);
+  }
+}
