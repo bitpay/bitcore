0609039373cebedc77ae775439a2b48a905077cb
Juan Pablo Abuin <juanpabloabuin@gmail.com>
2021-02-23 15:10:40 -0300

    Tests & doge config

--- packages/bitcore-lib-doge/lib/uri.js
+++ packages/bitcore-lib-doge/lib/uri.js
@@ -7,7 +7,7 @@ var Address = require('./address');
 var Unit = require('./unit');
 
 /**
- * Litecore URI
+ * Dogecore URI
  *
  * Instantiate an URI from a dogecoin URI String or an Object. An URI instance
  * can be created with a dogecoin uri string or an object. All instances of
