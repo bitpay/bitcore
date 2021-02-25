29dbbdd3772ad86125276cf6b276380b77bad9b2
sbhatnagar <sbhatnagar@bitpay.com>
2021-02-25 15:57:27 -0500

    derive to deriveChild

--- packages/crypto-wallet-core/src/derivation/btc/index.ts
+++ packages/crypto-wallet-core/src/derivation/btc/index.ts
@@ -14,7 +14,7 @@ export abstract class AbstractBitcoreLibDeriver implements IDeriver {
     const xpriv = new this.bitcoreLib.HDPrivateKey(xPriv, network);
     const changeNum = isChange ? 1 : 0;
     const path = `m/${changeNum}/${addressIndex}`;
-    const privKey = xpriv.derive(path).privateKey;
+    const privKey = xpriv.deriveChild(path).privateKey;
     const pubKey = privKey.publicKey;
     const address = this.bitcoreLib.Address(pubKey, network).toString();
     return { address, privKey: privKey.toString(), pubKey: pubKey.toString() };
