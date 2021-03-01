cc336f3f6428e51752d58f83d152da6af7fd1215
Gabriel <gabrielbazan89@gmail.com>
2021-03-01 12:12:53 -0300

    [REF] bitcore api

--- packages/bitcore-wallet-service/src/scripts/v8tool.ts
+++ packages/bitcore-wallet-service/src/scripts/v8tool.ts
@@ -33,7 +33,7 @@ const BASE = {
   BCH: `https://api.bitcore.io/api/${coin}/${network}`,
   ETH: `https://api-eth.bitcore.io/api/${coin}/${network}`,
   XRP: `https://api-xrp.bitcore.io/api/${coin}/${network}`,
-  DOGE: `https://api-doge.bitcore.io/api/${coin}/${network}`
+  DOGE: `https://api.bitcore.io/api/${coin}/${network}`
 };
 let baseUrl = BASE[coin];
 console.log('[v8tool.ts.37:baseUrl:]', baseUrl); // TODO
