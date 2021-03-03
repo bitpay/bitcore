e8407f1ca416432079b02392982233b9c579b48f
Bernard Snowden <bsnowden@bitpay.com>
2021-03-03 17:09:26 -0500

    new Buffer

--- packages/bitcore-lib-doge/lib/script/script.js
+++ packages/bitcore-lib-doge/lib/script/script.js
@@ -172,12 +172,12 @@ Script.fromASM = function(str) {
 };
 
 Script.fromHex = function(str) {
-  return new Script(new buffer.Buffer(str, 'hex'));
+  return new Script(Buffer.from(str, 'hex'));
 };
 
 Script.fromString = function(str) {
   if (JSUtil.isHexa(str) || str.length === 0) {
-    return new Script(new buffer.Buffer(str, 'hex'));
+    return new Script(Buffer.from(str, 'hex'));
   }
   var script = new Script();
   script.chunks = [];
@@ -1196,3 +1196,4 @@ Script.prototype.getSignatureOperationsCount = function(accurate) {
 };
 
 module.exports = Script;
+
