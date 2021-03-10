ff8eb21bab6406ee451ec1ec580f8ad1d2fc8c37
Kenny Joseph <kjoseph@bitpay.com>
2021-03-10 14:05:35 -0500

    Add doge-p2p to node

--- packages/bitcore-node/src/modules/dogecoin/index.ts
+++ packages/bitcore-node/src/modules/dogecoin/index.ts
@@ -6,7 +6,7 @@ import { DogecoinP2PWorker } from './p2p';
 export default class DOGEModule extends BaseModule {
   constructor(services) {
     super(services);
-    services.Libs.register('DOGE', 'bitcore-lib-doge', 'bitcore-p2p');
+    services.Libs.register('DOGE', 'bitcore-lib-doge', 'bitcore-p2p-doge');
     services.P2P.register('DOGE', DogecoinP2PWorker);
     services.CSP.registerService('DOGE', new DOGEStateProvider());
     services.Verification.register('DOGE', VerificationPeer);
