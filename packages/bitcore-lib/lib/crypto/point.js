91372fc25c348cc5f7448a22d4a6ad716a295fb5
Kondax <github@kondax.io>
2021-04-01 18:49:35 +0100

    Use prototype field in Object.create

--- packages/bitcore-lib/lib/crypto/point.js
+++ packages/bitcore-lib/lib/crypto/point.js
@@ -30,7 +30,7 @@ var Point = function Point(x, y, isRed) {
   return point;
 };
 
-Point.prototype = Object.create(ec.curve.point());
+Point.prototype = Object.create(ec.curve.point().prototype);
 
 /**
  *
