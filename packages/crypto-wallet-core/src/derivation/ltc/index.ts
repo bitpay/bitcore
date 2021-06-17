46328a695a52b0d84be8dfce33cbc194475dc1f5
sbhatnagar <sbhatnagar@bitpay.com>
2021-06-17 15:59:48 -0400

    adding tests and fixing ltc deriveAddress

--- packages/crypto-wallet-core/src/derivation/ltc/index.ts
+++ packages/crypto-wallet-core/src/derivation/ltc/index.ts
@@ -2,4 +2,11 @@ const BitcoreLibLtc = require('bitcore-lib-ltc');
 import { AbstractBitcoreLibDeriver } from '../btc';
 export class LtcDeriver extends AbstractBitcoreLibDeriver {
   bitcoreLib = BitcoreLibLtc;
+
+  deriveAddress(network, pubKey, addressIndex, isChange) {
+    const xpub = new this.bitcoreLib.HDPublicKey(pubKey, network);
+    const changeNum = isChange ? 1 : 0;
+    const path = `m/${changeNum}/${addressIndex}`;
+    return this.bitcoreLib.Address(xpub.deriveChild(path).publicKey, network).toString();
+  }
 }
