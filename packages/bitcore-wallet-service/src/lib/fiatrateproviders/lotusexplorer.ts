27222c88d9a9e73f8858c39debcfb11cf956bdc2
tung hoang <thtung2901@gmail.com>
2021-07-07 15:29:59 +0700

    refactor code

--- packages/bitcore-wallet-service/src/lib/fiatrateproviders/lotusexplorer.ts
+++ packages/bitcore-wallet-service/src/lib/fiatrateproviders/lotusexplorer.ts
@@ -3,14 +3,14 @@ const MINER_MARGIN = 1;
 const MINING_EFFICIENCY = 3.4;
 
 interface LotusData {
-    hashrate: string | number; 
-    difficulty: string | number; 
+  hashrate: string | number;
+  difficulty: string | number;
 }
 
 module.exports = {
   name: 'LotusExplorer',
   url: 'https://explorer.givelotus.org/ext/summary',
-  parseFn(raw: { data: LotusData[]; }) {
+  parseFn(raw: { data: LotusData[] }) {
     const currentDiff: number = +raw.data[0].difficulty;
     const hashRate: number = +raw.data[0].hashrate;
     const currentMinerReward = Math.round((Math.log2(currentDiff / 16) + 1) * 130 * 1000000) / 1000000;
