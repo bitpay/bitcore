895307fe013f3a64380f1ca9b8ef420696ae7d44
Kenny Joseph <kjoseph@bitpay.com>
2021-07-12 18:45:15 -0400

    Update blockchainexplorer default urls

--- packages/bitcore-wallet-service/src/lib/blockchainexplorer.ts
+++ packages/bitcore-wallet-service/src/lib/blockchainexplorer.ts
@@ -8,12 +8,12 @@ const Defaults = Common.Defaults;
 const PROVIDERS = {
   v8: {
     btc: {
-      livenet: 'https://api.bitpay.com',
-      testnet: 'https://api.bitpay.com'
+      livenet: 'https://api.bitcore.io',
+      testnet: 'https://api.bitcore.io'
     },
     bch: {
-      livenet: 'https://api.bitpay.com',
-      testnet: 'https://api.bitpay.com'
+      livenet: 'https://api.bitcore.io',
+      testnet: 'https://api.bitcore.io'
     },
     eth: {
       livenet: 'https://api-eth.bitcore.io',
@@ -24,12 +24,12 @@ const PROVIDERS = {
       testnet: 'https://api-xrp.bitcore.io'
     },
     doge: {
-      livenet: 'https://api.bitpay.com',
-      testnet: 'https://api.bitpay.com'
+      livenet: 'https://api.bitcore.io',
+      testnet: 'https://api.bitcore.io'
     },
     ltc: {
-      livenet: 'https://api.bitpay.com',
-      testnet: 'https://api.bitpay.com'
+      livenet: 'https://api.bitcore.io',
+      testnet: 'https://api.bitcore.io'
     }
   }
 };
