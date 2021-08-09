5d2c1814e6c90ea740d0c5059efe81b636d707b1
Ben Clabby <bclabby@bitpay.com>
2021-08-09 10:29:28 -0400

    Remove duplicate address index

--- packages/bitcore-wallet-service/src/lib/storage.ts
+++ packages/bitcore-wallet-service/src/lib/storage.ts
@@ -106,13 +106,6 @@ export class Storage {
       walletId: 1,
       createdOn: 1
     });
-
-    db.collection(collections.ADDRESSES).createIndex(
-      {
-        address: 1
-      },
-      { unique: true }
-    );
     db.collection(collections.ADDRESSES).createIndex({
       address: 1,
       beRegistered: 1
