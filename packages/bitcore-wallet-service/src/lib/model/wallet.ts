4fdf9047530072980e7abca94a91387c191f8563
Elliott Alexander <ealexander@bitpay.com>
2022-07-13 13:59:02 -0400

    added matic to a few more places

--- packages/bitcore-wallet-service/src/lib/model/wallet.ts
+++ packages/bitcore-wallet-service/src/lib/model/wallet.ts
@@ -16,6 +16,7 @@ const Bitcore = {
   btc: require('bitcore-lib'),
   bch: require('bitcore-lib-cash'),
   eth: require('bitcore-lib'),
+  matic: require('bitcore-lib'),
   xrp: require('bitcore-lib'),
   doge: require('bitcore-lib-doge'),
   ltc: require('bitcore-lib-ltc')
