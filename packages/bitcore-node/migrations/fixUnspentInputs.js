b43dc355317bc514ce53122616a4b902e769f711
Elliott Alexander <ealexander@bitpay.com>
2022-09-12 09:34:44 -0400

    migration script updates

--- packages/bitcore-node/migrations/fixUnspentInputs.js
+++ packages/bitcore-node/migrations/fixUnspentInputs.js
@@ -5,16 +5,21 @@
  *** are left in a pending state if they are not used by
  *** the new tx that is the replacement.
  ***
- *** This does a dry run by default. Use "--dryrun false" 
+ *** This does a dry run by default. Use "--dryrun false"
  *** to execute outside of dry run.
+ *** 
+ *** By default this will run for BTC on testnet. To change this
+ *** use the --chain [CHAIN] and --network [NETWORk] flags.
+ *** You must have valid RPC connection specified in bitcore.config.json.
  ********************************************/
-
-const { TransactionModel, TransactionStorage } = require('../build/src/models/transaction');
-const { CoinModel, CoinStorage } = require('../build/src/models/coin');
+const { CryptoRpc } = require('crypto-rpc');
+const { TransactionStorage } = require('../build/src/models/transaction');
+const { CoinStorage } = require('../build/src/models/coin');
 const fs = require('fs');
 const fsPromises = fs.promises;
 const { Storage } = require('../build/src/services/storage');
 const { wait } = require('../build/src/utils/wait');
+const Config = require('../build/src/config');
 
 const RBFEnabledUTXOChains = ['BTC', 'LTC', 'DOGE'];
 const networks = ['regtest', 'testnet', 'mainnet'];
@@ -37,7 +42,9 @@ class Migration {
 
   processArgs(argv) {
     let defaults = {
-      dryrun: true
+      dryrun: true,
+      chain: 'BTC',
+      network: 'testnet'
     };
     let args = argv.slice(2);
     const dryRunIdx = args.findIndex(i => i == '--dryrun');
@@ -49,51 +56,106 @@ class Migration {
           ? false
           : true;
     }
+    const chainIdx = args.findIndex(i => i == '--chain');
+    if (chainIdx >= 0) {
+      defaults.chain = args[chainIdx + 1] == undefined ? 'BTC' : args[chainIdx + 1].toUpperCase();
+    }
+
+    const networkIdx = args.findIndex(i => i == '--network');
+    if (networkIdx >= 0) {
+      defaults.network = args[networkIdx + 1] == undefined ? 'testnet' : args[networkIdx + 1].toLowerCase();
+    }
     return defaults;
   }
 
   async runScript(args) {
-    console.log('running script with these args: ', args);
+    console.log('Running script with these args: ', args);
     let output = {};
-    for await (const chain of RBFEnabledUTXOChains) {
-      for await (const network of networks) {
-        // using Set so that we don't have duplicates
-        console.log(`updating records for ${chain}:${network}`);
-        // Get array of all invalid txs for chain and network
-        const invalidTxs = await this.transactionModel.collection
-          .find({ chain, network, blockHeight: -3 }) // -3 is conflicted status
-          .toArray();
-        // Create array of tx ids
-        const spentTxids = invalidTxs.map(tx => tx.txid);
-        const recordsToUpdate = await this.coinModel.collection
-          .find(
-            { chain, network, spentTxid: { $in: spentTxids }, spentHeight: -1 } // -1 is pending status
-          )
-          .toArray();
-        // Set all coins that were pending to be spent by an invalid tx back to unspent
-        if (!args.dryrun) {
-          const task = await this.coinModel.collection.updateMany(
-            { chain, network, spentTxid: { $in: spentTxids }, spentHeight: -1 }, // -1 is pending status
-            { $set: { spentHeight: -2 } } // -2 is unspent status
-          );
-          console.log('modified count: ', task.modifiedCount);
+    const { chain, network, dryrun } = args;
+    console.log(`Checking records for ${chain}:${network}`);
+    // Get all pending coins from valid transactions (mintHeight should be valid block height)
+    const stream = this.coinModel.collection
+      .find(
+        { chain, network, mintHeight: { $gt: -1 }, spentHeight: -1 } // -1 is pending status
+      )
+      .addCursorFlag('noCursorTimeout', true);
+
+    // Initialize RPC connection
+    if (!Config.default.chains[chain]) {
+      console.error(`There is no config for chain '${chain}'`);
+      return;
+    }
+    if (!Config.default.chains[chain][network]) {
+      console.error(`There is no config for chain '${chain}' with network '${network}'`);
+      return;
+    }
+    const rpcConfig = Config.default.chains[chain][network].rpc;
+    const rpc = new CryptoRpc(
+      {
+        rpcPort: rpcConfig.port,
+        host: rpcConfig.host,
+        protocol: rpcConfig.protocol,
+        rpcUser: rpcConfig.username,
+        rpcPass: rpcConfig.password,
+        chain
+      },
+      {}
+    ).get(chain);
+
+    const handleStream = async data => {
+      let isUnspent = false;
+      // If spent (or in mempool) then this returns null with an error otherwise returns data on unspent output
+      try {
+        const coinData = await rpc.getTxOutputInfo({
+          txid: data.mintTxid,
+          vout: data.mintIndex
+        });
+        isUnspent = !!coinData;
+      } catch (e) {
+        // Coin must be spent or actually pending in mempool - do nothing
+      } finally {
+        if (isUnspent) {
+          // Log record
+          if (output[`${chain}-${network}`]) {
+            output[`${chain}-${network}`].push(data);
+          } else {
+            output[`${chain}-${network}`] = [data];
+          }
+
+          if (!dryrun) {
+            // Update record to be unspent (-2)
+            this.coinModel.collection.updateOne({ _id: data._id }, { $set: { spentHeight: -2 } }); // -2 is unspent status
+          }
         }
-        output[`${chain}-${network}`] = recordsToUpdate;
+        const nextData = await stream.next();
+        if (nextData) {
+          handleStream(nextData);
+        } else {
+          // End of data -
+          endProcess();
+        }
       }
-    }
-    const date = new Date().getTime();
-    const filename = `output-${date}.log`;
-    console.log(`writing output to ${filename}`);
-    try {
-      await fsPromises.writeFile(filename, JSON.stringify(output));
-    } catch (e) {
-      // write to stdout
-      console.log('failed to write output to file. Writing to stdout instead.');
-      console.log(output);
-    }
-    if (args.dryrun) {
-      console.log('run the script with "--dryrun false" to execute this operation on the returned results.');
-    }
+    };
+    // Handle incoming stream data
+    handleStream(await stream.next());
+
+    const endProcess = async () => {
+      console.log(`Finished updating records for ${chain}-${network}`);
+      const date = new Date().getTime();
+      const filename = `output-${chain}-${network}-${date}.log`;
+      console.log(`writing output to ${filename}`);
+      try {
+        await fsPromises.writeFile(filename, JSON.stringify(output));
+      } catch (e) {
+        // write to stdout
+        console.log('failed to write output to file. Writing to stdout instead.');
+        console.log(output);
+      }
+      if (args.dryrun) {
+        console.log('run the script with "--dryrun false" to execute this operation on the returned results.');
+      }
+      process.exit();
+    };
   }
 }
 
@@ -103,10 +165,7 @@ migration
   .connect()
   .then(() => {
     const args = migration.processArgs(process.argv);
-    migration.runScript(args).then(() => {
-      console.log('completed');
-      process.exit();
-    });
+    migration.runScript(args);
   })
   .catch(err => {
     console.error(err);
