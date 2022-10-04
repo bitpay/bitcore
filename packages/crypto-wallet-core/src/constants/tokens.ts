89fbc93cd4de8360558607e450e5cf9e0ec642db
juan <juanpabloabuin@gmail.com>
2022-10-04 17:19:12 +0200

    property name fix in eth tokens opts

--- packages/crypto-wallet-core/src/constants/tokens.ts
+++ packages/crypto-wallet-core/src/constants/tokens.ts
@@ -2,61 +2,61 @@ export let ETH_TOKEN_OPTS = {
   '0x4fabb145d64652a948d72533023f6e7a623c7c53': {
     name: 'Binance USD Coin',
     symbol: 'BUSD',
-    decimal: 18,
+    decimals: 18,
     address: '0x4fabb145d64652a948d72533023f6e7a623c7c53'
   },
   '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
     name: 'USD Coin',
     symbol: 'USDC',
-    decimal: 6,
+    decimals: 6,
     address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
   },
   '0x8e870d67f660d95d5be530380d0ec0bd388289e1': {
     name: 'Paxos Dollar',
     symbol: 'USDP',
-    decimal: 18,
+    decimals: 18,
     address: '0x8e870d67f660d95d5be530380d0ec0bd388289e1'
   },
   '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd': {
     name: 'Gemini Dollar',
     symbol: 'GUSD',
-    decimal: 2,
+    decimals: 2,
     address: '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd'
   },
   '0x6b175474e89094c44da98b954eedeac495271d0f': {
     name: 'Dai',
     symbol: 'DAI',
-    decimal: 18,
+    decimals: 18,
     address: '0x6b175474e89094c44da98b954eedeac495271d0f'
   },
   '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599': {
     name: 'Wrapped Bitcoin',
     symbol: 'WBTC',
-    decimal: 8,
+    decimals: 8,
     address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'
   },
   '0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce': {
     address: '0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce',
-    decimal: 18,
+    decimals: 18,
     name: 'SHIBA INU',
     symbol: 'SHIB'
   },
   '0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c': {
     name: 'Euro Coin',
     symbol: 'EUROC',
-    decimal: 6,
+    decimals: 6,
     address: '0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c'
   },
   '0x4d224452801aced8b2f0aebe155379bb5d594381': {
     name: 'ApeCoin',
     symbol: 'APE',
-    decimal: 18,
+    decimals: 18,
     address: '0x4d224452801aced8b2f0aebe155379bb5d594381'
   },
   '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0': {
     name: 'Polygon',
     symbol: 'MATIC',
-    decimal: 18,
+    decimals: 18,
     address: '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0'
   }
 };
