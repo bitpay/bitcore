1f1835811f76d4e8852234b771ee91906e596a45
Kenny Joseph <kjoseph@bitpay.com>
2022-10-27 14:49:33 -0400

    chain-granular conf cache threshold

--- packages/bitcore-wallet-service/src/lib/server.ts
+++ packages/bitcore-wallet-service/src/lib/server.ts
@@ -4021,8 +4021,13 @@ export class WalletService {
           // We have now TXs from 'tipHeight` to end in `lastTxs`.
           // Store hard confirmed TXs
           // confirmations here is bcHeight - tip + 1, so OK.
+          let CACHE_AT_CONFS = Defaults.CONFIRMATIONS_TO_START_CACHING;
+          if (Constants.CONFIRMATIONS_TO_START_CACHING[wallet.chain] != null) {
+            CACHE_AT_CONFS = Constants.CONFIRMATIONS_TO_START_CACHING[wallet.chain];
+          }
+
           txsToCache = _.filter(lastTxs, i => {
-            if (i.confirmations < Defaults.CONFIRMATIONS_TO_START_CACHING) {
+            if (i.confirmations < CACHE_AT_CONFS) {
               return false;
             }
             if (!cacheStatus.tipHeight) return true;
@@ -4035,7 +4040,7 @@ export class WalletService {
             return next();
           }
 
-          const updateHeight = bcHeight - Defaults.CONFIRMATIONS_TO_START_CACHING;
+          const updateHeight = bcHeight - CACHE_AT_CONFS;
           this.storage.storeTxHistoryCacheV8(walletCacheKey, cacheStatus.tipIndex, txsToCache, updateHeight, next);
         }
       ],
