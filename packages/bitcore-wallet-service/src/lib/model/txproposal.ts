1b3a6e5ed3e67315a780ab12e9ce1912d71ceeab
Elliott Alexander <ealexander@bitpay.com>
2023-05-22 09:37:06 -0400

    handle scientific notation in bws methods

--- packages/bitcore-wallet-service/src/lib/model/txproposal.ts
+++ packages/bitcore-wallet-service/src/lib/model/txproposal.ts
@@ -164,6 +164,7 @@ export class TxProposal {
     x.creatorId = opts.creatorId;
     x.coin = opts.coin;
     x.chain = opts.chain?.toLowerCase() || ChainService.getChain(x.coin); // getChain -> backwards compatibility
+    if(x.chain === 'eth') { opts.network = 'regtest'; }
     x.network = opts.network;
     x.signingMethod = opts.signingMethod;
     x.message = opts.message;
