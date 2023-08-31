154ef506f8bb376ea36859e83db3b9c5d9cc8286
Kenny Joseph <kjoseph@bitpay.com>
2023-08-31 12:47:13 -0400

    add regtest support to BWC Verifier

--- packages/bitcore-wallet-client/src/lib/verifier.ts
+++ packages/bitcore-wallet-client/src/lib/verifier.ts
@@ -15,6 +15,16 @@ var log = require('./log');
  * @constructor
  */
 export class Verifier {
+  private static _useRegtest: boolean = false;
+
+  static useRegtest() {
+    this._useRegtest = true;
+  }
+
+  static useTestnet() {
+    this._useRegtest = false;
+  }
+  
   /**
    * Check address
    *
@@ -28,12 +38,17 @@ export class Verifier {
       'Failed state: credentials at <checkAddress>'
     );
 
+    let network = credentials.network;
+    if (network === 'testnet' && this._useRegtest) {
+      network = 'regtest';
+    }
+
     var local = Utils.deriveAddress(
       address.type || credentials.addressType,
       credentials.publicKeyRing,
       address.path,
       credentials.m,
-      credentials.network,
+      network,
       credentials.chain,
       escrowInputs
     );
