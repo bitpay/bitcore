5efc793f58de1ce981d51672b7baadd5e7d52dea
Kenny Joseph <kjoseph@bitpay.com>
2023-10-03 13:30:03 -0400

    fix bch derivation

--- packages/crypto-wallet-core/src/derivation/bch/index.ts
+++ packages/crypto-wallet-core/src/derivation/bch/index.ts
@@ -2,4 +2,9 @@ const BitcoreLibCash = require('bitcore-lib-cash');
 import { AbstractBitcoreLibDeriver } from '../btc';
 export class BchDeriver extends AbstractBitcoreLibDeriver {
   bitcoreLib = BitcoreLibCash;
+
+  getAddress(network: string, pubKey, addressType: string) {
+    pubKey = new this.bitcoreLib.PublicKey(pubKey);
+    return pubKey.toAddress(network, addressType).toString(true);
+  }
 }
--- packages/crypto-wallet-core/src/derivation/btc/index.ts
+++ packages/crypto-wallet-core/src/derivation/btc/index.ts
@@ -18,14 +18,14 @@ export abstract class AbstractBitcoreLibDeriver implements IDeriver {
 
   deriveAddressWithPath(network: string, xpubKey: string, path: string, addressType: string) {
     const xpub = new this.bitcoreLib.HDPublicKey(xpubKey, network);
-    return xpub.derive(path).publicKey.toAddress(network, addressType).toString();
+    return this.getAddress(network, xpub.derive(path).publicKey, addressType);
   }
 
   derivePrivateKeyWithPath(network: string, xprivKey: string, path: string, addressType: string) {
     const xpriv = new this.bitcoreLib.HDPrivateKey(xprivKey, network);
     const privKey = xpriv.deriveChild(path).privateKey;
     const pubKey = privKey.publicKey;
-    const address = this.bitcoreLib.Address(pubKey, network, addressType).toString();
+    const address = this.getAddress(pubKey, network, addressType);
     return { address, privKey: privKey.toString(), pubKey: pubKey.toString() };
   }
 
