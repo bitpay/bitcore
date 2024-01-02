bad74f0dc3708f58f1b2082a91d3364980af4ff0
Kenny Joseph <kjoseph@bitpay.com>
2024-01-02 09:30:30 -0500

    rbf in wallet send; xrp in wallet send

--- packages/crypto-wallet-core/src/index.ts
+++ packages/crypto-wallet-core/src/index.ts
@@ -3,6 +3,7 @@ import * as BitcoreLibCash from 'bitcore-lib-cash';
 import * as BitcoreLibDoge from 'bitcore-lib-doge';
 import * as BitcoreLibLtc from 'bitcore-lib-ltc';
 import Web3 from 'web3';
+import xrpl from 'xrpl';
 import { Constants } from './constants';
 import Deriver from './derivation';
 import Transactions from './transactions';
@@ -16,5 +17,6 @@ export {
   Transactions,
   Validation,
   Web3,
-  Constants
+  Constants,
+  xrpl
 };
