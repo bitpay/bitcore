110e1d1a7fa1784bf8e9a231ac4d94661722807d
lyambo <lyambo@bitpay.com>
2024-04-09 18:27:56 -0400

    set priority fee minimums

--- packages/crypto-wallet-core/src/constants/index.ts
+++ packages/crypto-wallet-core/src/constants/index.ts
@@ -1,7 +1,11 @@
+import { EVM_CHAIN_ID_TO_CHAIN } from './chains';
+import { FEE_MINIMUMS } from './feeMinimums';
 import { ETH_TOKEN_OPTS, MATIC_TOKEN_OPTS } from './tokens';
 import { UNITS } from './units';
 export let Constants = {
   ETH_TOKEN_OPTS,
   MATIC_TOKEN_OPTS,
-  UNITS
+  UNITS,
+  FEE_MINIMUMS,
+  EVM_CHAIN_ID_TO_CHAIN
 };
