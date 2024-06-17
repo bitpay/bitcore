1378cfdd91ce48abf22fe3dd03b781127e05ffd5
lyambo <lyambo@bitpay.com>
2024-06-17 17:04:05 -0400

    import logger

--- packages/bitcore-wallet-service/src/lib/chain/index.ts
+++ packages/bitcore-wallet-service/src/lib/chain/index.ts
@@ -7,7 +7,7 @@ import { BchChain } from './bch';
 import { BtcChain } from './btc';
 import { DogeChain } from './doge';
 import { EthChain } from './eth';
-import logger from './logger';
+import logger from './../logger';
 import { LtcChain } from './ltc';
 import { MaticChain } from './matic';
 import { OpChain } from './op';
