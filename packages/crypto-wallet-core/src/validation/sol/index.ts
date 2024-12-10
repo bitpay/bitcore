141b798ff8e28e7caa5d9f871ad53e8cde38a169
lyambo <lyambo@bitpay.com>
2024-12-10 13:03:33 -0500

    sol validation

--- packages/crypto-wallet-core/src/validation/sol/index.ts
+++ packages/crypto-wallet-core/src/validation/sol/index.ts
@@ -1,36 +1,23 @@
 import baseX from 'base-x';
 import Bitcore from 'bitcore-lib';
+import * as Web3 from '@solana/web3.js'
 import { IValidation } from '..';
 
 const RIPPLE_ALPHABET = 'rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz';
 
 export class SolValidation implements IValidation {
+  regex: RegExp;
+
+  constructor() {
+    this.regex = /solana/i;
+  }
   validateAddress(_network: string, address: string): boolean {
-    // First ensure it matches regex
-    const RippleAddressRegex = new RegExp(/^r[rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz]{27,35}$/);
-    if (!address.match(RippleAddressRegex)) {
+    try {
+      new Web3.PublicKey(address);
+      return true;
+    } catch (error) {
       return false;
     }
-
-    // Then ensure it is a valid base58check encoding
-    const base58 = baseX(RIPPLE_ALPHABET);
-    const buffer = new Buffer(base58.decode(address));
-    let prefix = buffer.slice(0, 1);
-    let data = buffer.slice(1, -4);
-    let hash = Buffer.concat([prefix, data]);
-    hash = Bitcore.crypto.Hash.sha256(hash);
-    hash = Bitcore.crypto.Hash.sha256(hash);
-    let checksum = buffer.slice(-4).reduce((acc, check, index) => {
-      if (check !== hash[index]) {
-        // Invalid checksum
-        return 0;
-      } else return acc || 1;
-    });
-    if (checksum === 0) {
-      return false;
-    }
-
-    return true;
   }
 
   validateUri(addressUri: string): boolean {
@@ -38,13 +25,13 @@ export class SolValidation implements IValidation {
       return false;
     }
     const address = this.extractAddress(addressUri);
-    const ripplePrefix = /ripple/i.exec(addressUri);
-    return !!ripplePrefix && this.validateAddress('livenet', address);
+    const prefix = this.regex.exec(addressUri);
+    return !!prefix && this.validateAddress('livenet', address);
   }
 
   private extractAddress(data) {
     const prefix = /^[a-z]+:/i;
-    const params = /([\?\&](amount|dt)=(\d+([\,\.]\d+)?))+/i;
+    const params = /([\?\&](amount|fee)=(\d+([\,\.]\d+)?))+/i;
     return data.replace(prefix, '').replace(params, '');
   }
 }
