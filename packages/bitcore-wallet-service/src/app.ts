ef5a828fe9b84e3e1a518690d2b4e14d9218b80e
vince8x <vince.tran8x@gmail.com>
2025-02-27 00:21:39 +0700

    Fix locker not found

--- packages/bitcore-wallet-service/src/app.ts
+++ packages/bitcore-wallet-service/src/app.ts
@@ -4,7 +4,6 @@ var spawn = require('child_process').spawn;
 var async = require('async');
 
 var scripts = [
-  'locker/locker.js',
   'messagebroker/messagebroker.js',
   'bcmonitor/bcmonitor.js',
   'emailservice/emailservice.js',
