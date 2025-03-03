4d3fc08679a23c85b1a4edc294985f34cace3622
vince8x <vince.tran8x@gmail.com>
2025-03-03 10:34:48 +0700

    Revert locker

--- packages/bitcore-wallet-service/src/app.ts
+++ packages/bitcore-wallet-service/src/app.ts
@@ -4,6 +4,7 @@ var spawn = require('child_process').spawn;
 var async = require('async');
 
 var scripts = [
+  'locker/locker.js',
   'messagebroker/messagebroker.js',
   'bcmonitor/bcmonitor.js',
   'emailservice/emailservice.js',
