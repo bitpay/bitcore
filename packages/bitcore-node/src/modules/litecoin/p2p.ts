2195222fd01720b716c8dd2d5f6fdb1c1717b0ca
Kenny Joseph <kjoseph@bitpay.com>
2025-04-22 14:16:27 -0400

    DRY the doge and ltc p2p classes

--- packages/bitcore-node/src/modules/litecoin/p2p.ts
+++ packages/bitcore-node/src/modules/litecoin/p2p.ts
@@ -1,37 +1,10 @@
-import { EventEmitter } from 'events';
-import { BitcoinBlock, BitcoinBlockStorage } from '../../models/block';
-import { Libs } from '../../providers/libs';
+import { BitcoinBlockStorage } from '../../models/block';
 import { BitcoinP2PWorker } from '../bitcoin/p2p';
 
 export class LitecoinP2PWorker extends BitcoinP2PWorker {
-  protected bitcoreLib: any;
-  protected bitcoreP2p: any;
-  protected chainConfig: any;
-  protected messages: any;
-  protected connectInterval?: NodeJS.Timeout;
-  protected invCache: any;
-  protected invCacheLimits: any;
-  protected initialSyncComplete: boolean;
-  protected blockModel: BitcoinBlock;
-  protected pool: any;
-  public events: EventEmitter;
-  public isSyncing: boolean;
+
   constructor({ chain, network, chainConfig, blockModel = BitcoinBlockStorage }) {
     super({ chain, network, chainConfig, blockModel });
-    this.blockModel = blockModel;
-    this.chain = chain;
-    this.network = network;
-    this.bitcoreLib = Libs.get(this.chain).lib;
-    this.bitcoreP2p = Libs.get(this.chain).p2p;
-    this.chainConfig = chainConfig;
-    this.events = new EventEmitter();
-    this.isSyncing = false;
-    this.initialSyncComplete = false;
-    this.invCache = {};
-    this.invCacheLimits = {
-      [this.bitcoreP2p.Inventory.TYPE.BLOCK]: 100,
-      [this.bitcoreP2p.Inventory.TYPE.TX]: 100000
-    };
 
     if (this.network === 'regtest') {
       this.bitcoreLib.Networks.enableRegtest();
