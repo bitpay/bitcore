ac7d948157c45f0f44f73de1426e08d873aaebd5
lyambo <lyambo@bitpay.com>
2025-06-03 09:59:48 -0400

    make sol signatures consistently base58 encoded

--- packages/bitcore-wallet-service/src/lib/server.ts
+++ packages/bitcore-wallet-service/src/lib/server.ts
@@ -2650,7 +2650,7 @@ export class WalletService implements IWalletService {
                   return next();
                 },
                 async next => {
-                  if (Constants.SVM_CHAINS[wallet.chain.toUpperCase()] && !opts.blockHeight) { 
+                  if (Constants.SVM_CHAINS[wallet.chain.toUpperCase()] && (!opts.blockHeight || !opts.blockHash)) { 
                     this._getBlockchainHeight(wallet.chain, wallet.network, (err, height, hash) => {
                       if (err) return next(err);
                       opts.blockHeight = height;
