5daa800116a38e0498b6bd9bb4b710881f209789
MicahMaphet <micahmaphet@gmail.com>
2025-06-18 06:35:31 +0900

    merged changes adding scope to make button bigger

--- packages/insight/src/components/pill.tsx
+++ packages/insight/src/components/pill.tsx
@@ -1,19 +1,19 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 import CloseLightSvg from 'src/assets/images/close-light.svg'
-import {Black, Slate30, White} from '../assets/styles/colors';
+import {Black, White, Slate30} from '../assets/styles/colors';
 import { size } from 'src/utilities/constants';
 
 const PillBubble = styled.div`
   display: flex;
   align-items: center;
   height: 38px;
-  padding: 5.5px 7px;
+  padding-left: 5px;
   margin-right: 10px;
-
+  
   @media screen and (max-width: ${size.mobileL}) {
-    height: 30px;
-    padding: 6.5px 6px;
+    height: 25px;
+    padding-left: 6px;
     margin-right: 5px;
     margin-left: -3px;
   }
@@ -24,19 +24,42 @@ const PillBubble = styled.div`
 const PillCloseButtonCircle = styled.div`
   background-color: ${({theme: {dark}}) => dark ? '#888' : '#D1D4D7'};
   border-radius: 100%;
-  height: 100%;
+  height: 75%;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  aspect-ratio: 1;
   cursor: pointer;
 
+  @media screen and (max-width: ${size.mobileL}) {
+    height: 60%;
+  }
+`;
+
+const PillCloseButtonScope = styled.div`
+  background-color: transparent;
+  border-radius: 100%;
+  height: 100%;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  aspect-ratio: 1;
+  display: flex;
+
   &:hover {
-    background-color: #B1B4B7;
+    background-color: ${({theme: {dark}}) => dark ? '#222' : '#D8DBDD'};
+  }
+
+  &:hover ${PillCloseButtonCircle} {
+    background-color: ${({theme: {dark}}) => dark ? '#B1B4B7' : '#ECEFF2'};
   }
 `;
 
 const NetworkLabel = styled.span`
   text-transform: capitalize;
-  padding: 5px;
+  padding-left: 4px;
   color: ${({theme: {dark}}) => dark ? White : Black};
-  font-size: 16px
+  font-size: 16px;
 
   @media screen and (max-width: ${size.mobileL}) {
     font-size: 12px;
@@ -54,13 +77,15 @@ export const Pill: FC<PillProps> = ({ currency, network, onCloseClick }) => {
   return (
     currency ?
       <PillBubble>
-        <img src={`https://bitpay.com/img/icon/currencies/${currency}.svg`} alt={currency} style={{height: '100%'}} />
+        <img src={`https://bitpay.com/img/icon/currencies/${currency}.svg`} alt={currency} style={{height: isMobile() ? '60%' : '75%'}} />
         <NetworkLabel>
           {isMobile() ? (network === 'mainnet' ? 'main' : 'test') : network}
         </NetworkLabel>
-        <PillCloseButtonCircle onClick={onCloseClick}>
-          <img src={CloseLightSvg} alt='Close' style={{height: '100%', padding: '25%'}} />
-        </PillCloseButtonCircle>
+        <PillCloseButtonScope onClick={onCloseClick}>
+          <PillCloseButtonCircle>
+            <img src={CloseLightSvg} alt='Close' style={{height: '50%'}} />
+          </PillCloseButtonCircle>
+        </PillCloseButtonScope>
       </PillBubble>
     : null
   );
