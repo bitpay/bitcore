ec13fa59139ed5e1aabf9867172c67a9c80916e7
MicahMaphet <micahmaphet@gmail.com>
2025-07-28 22:18:07 -0400

    moved block list (now 10 blocks) to the right and linting

--- packages/insight/src/pages/chain.tsx
+++ packages/insight/src/pages/chain.tsx
@@ -1,63 +1,94 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
-import { useApi } from 'src/api/api';
+import {useEffect, useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {useNavigate, useParams} from 'react-router-dom';
+import {fetcher} from 'src/api/api';
 import ChainHeader from 'src/components/chain-header';
-import { changeCurrency, changeNetwork } from 'src/store/app.actions';
-import { getApiRoot, getDefaultRefreshInterval, normalizeParams } from 'src/utilities/helper-methods';
-import Blocks from './blocks';
+import {changeCurrency, changeNetwork} from 'src/store/app.actions';
+import {getApiRoot, getFormattedDate, normalizeParams, sleep} from 'src/utilities/helper-methods';
+import {BlocksType} from 'src/utilities/models';
+import nProgress from 'nprogress';
+import Info from 'src/components/info';
 
+const getBlocksUrl = (currency: string, network: string) => {
+  return `${getApiRoot(currency)}/${currency}/${network}/block?limit=10`;
+};
 
 export const ChainDetails = () => {
-    const params = useParams<{currency: string, network: string}>()
-    let {currency, network} = params;
+  const params = useParams<{currency: string; network: string}>();
+  const [blocksList, setBlocksList] = useState<BlocksType[]>();
+  const [error, setError] = useState('');
+  let {currency, network} = params;
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    if (currency) {
-        const apiRoot = getApiRoot(currency);
-        const refreshInterval = getDefaultRefreshInterval(currency);
-        const {data, error} = useApi(`${apiRoot}/${currency}/mainnet/block?limit=1`, {refreshInterval});
-        if (data) {
-            const {height, time, transactionCount, size} = data[0];
+  useEffect(() => {
+    if (!currency || !network) return;
+    nProgress.start();
+    const _normalizeParams = normalizeParams(currency, network);
 
-        }
+    currency = _normalizeParams.currency;
+    network = _normalizeParams.network;
 
-    }
+    dispatch(changeCurrency(currency));
+    dispatch(changeNetwork(network));
 
-    useEffect(() => {
-        if (!currency || !network) return;
+    Promise.all([fetcher(getBlocksUrl(currency, network)), sleep(500)])
+      .then(([data]) => {
+        setBlocksList(data);
+      })
+      .catch((e: any) => {
+        setError(e.message || 'Something went wrong. Please try again later.');
+      })
+      .finally(() => {
+        nProgress.done();
+      });
+  }, [currency, network]);
 
-        const normalizedParams = normalizeParams(currency, network);
-        currency = normalizedParams.currency;
-        network = normalizedParams.network;
-        dispatch(changeCurrency(currency));
-        dispatch(changeNetwork(network));
+  const gotoBlocks = async () => {
+    await navigate(`/${currency}/mainnet/blocks`);
+  };
 
-    }, [currency, network]);
+  const gotoSingleBlockDetailsView = async (hash: string) => {
+    await navigate(`/${currency}/${network}/block/${hash}`);
+  };
+
+  return (
+    <>
+      {error ? <Info type={'error'} message={error} /> : null}
     
-    const {data: priceDetails} = useApi(`https://bitpay.com/rates/${currency}/usd`);
-    const price = priceDetails?.data?.rate;
-    
-    const gotoBlocks = async () => {
-        await navigate(`/${currency}/mainnet/blocks`);
-    }
-
-    return (
-        <>
-            {currency && network && <ChainHeader currency={currency} network={network} />}
-            <div style={{display: 'flex', height: '500px'}}>
-                <div style={{width: '50%'}}>
-                    Fee
-                </div>
-                <div style={{width: '50%', height: '100%', overflow: 'auto'}}>
-                    <div style={{display: 'flex'}}>
-                        <div onClick={(gotoBlocks)}>Go to full block list</div>
-                    </div>
-                    <Blocks />
-                </div>
-            </div>
-        </>
-    );
-}
+      <div style={{display: 'flex'}}>
+        {currency && network && <ChainHeader currency={currency} network={network} />}
+        <div>
+          <div onClick={gotoBlocks}>Go to full block list</div>
+          <table>
+            <tr>
+              <th>Height</th>
+              <th>Timestamp</th>
+              <th>Transactions</th>
+              <th>Size</th>
+            </tr>
+            {
+              blocksList?.length ?
+              (<tbody>
+                {blocksList.map((block: BlocksType, index: number) => {
+                  const {height, hash, transactionCount, time, size} = block;
+                  return (
+                    <tr
+                      key={index}
+                      onClick={() => gotoSingleBlockDetailsView(hash)}>
+                      <td width='25%'>{height}</td>
+                      <td width='25%'>{getFormattedDate(time)}</td>
+                      <td width='25%'>{transactionCount}</td>
+                      <td width='25%'>{size}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+              ): null}
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
