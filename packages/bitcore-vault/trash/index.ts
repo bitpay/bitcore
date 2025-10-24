38391b8e9314980923e6f27a973bb2144377467c
Michael Jay <mjay@bitpay.com>
2025-10-24 08:01:53 -0400

    delete trash dir

--- packages/bitcore-vault/trash/index.ts
+++
@@ -1,9 +0,0 @@
-import { CredentialManager } from './CredentialManager';
-import { SecureVault } from './SecureVault';
-
-export {
-  SecureVault,
-  CredentialManager,
-};
-
-export * from './types'; 
--- packages/bitcore-vault/trash/secure-process.ts
+++
@@ -1,8 +0,0 @@
-/** ---------- Child process runtime ------------- */
-// Create RSA keypair
-
-import { VaultWallet } from './VaultWallet';
-
-// Set up listeners
-
-const wallets = new Map<string, { wallet: VaultWallet, passphrase: Buffer | undefined }>(); // keys are wallet names
