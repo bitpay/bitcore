1f678b33f483b1d8decfb709479074f49c123bb5
Micah Maphet <micahmaphet@gmail.com>
2025-11-04 10:34:44 -0500

    removed unnecessary container in chain page

--- packages/insight/src/pages/chain.tsx
+++ packages/insight/src/pages/chain.tsx
@@ -5,23 +5,10 @@ import {useParams} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
 import {changeCurrency, changeNetwork} from 'src/store/app.actions';
 import {getApiRoot, normalizeParams} from 'src/utilities/helper-methods';
-import styled from 'styled-components';
-import {size} from 'src/utilities/constants';
 import {fetcher} from 'src/api/api';
 import nProgress from 'nprogress';
 import {BitcoinBlockType} from 'src/utilities/models';
 import Info from 'src/components/info';
-
-const HeaderDataContainer = styled.div`
-  width: 100%;
-  min-width: 0;
-  align-items: center; /* center on mobile by default */
-
-  @media screen and (min-width: ${size.mobileL}) {
-    flex-direction: row;
-    align-items: flex-start;
-  }
-`;
 
 const Chain: React.FC = () => {
   let {currency, network} = useParams<{currency: string; network: string}>();
@@ -61,10 +48,8 @@ const Chain: React.FC = () => {
   return (
     <>
       {error ? <Info type={'error'} message={error} /> : null}
-      <HeaderDataContainer>
-        <ChainHeader currency={currency} network={network} blocks={blocksList}/>
-        { blocksList && <BlockSample currency={currency} network={network} blocks={blocksList} /> }
-      </HeaderDataContainer>
+      <ChainHeader currency={currency} network={network} blocks={blocksList}/>
+      { blocksList && <BlockSample currency={currency} network={network} blocks={blocksList} /> }
     </>
   );
 }
