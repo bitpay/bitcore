07b479e74b153eceb622a3455e34d6372df4ef37
Kenny Joseph <kjoseph@bitpay.com>
2025-11-06 15:47:13 -0500

    lint semicolon type endings

--- packages/crypto-wallet-core/src/transactions/eth/index.ts
+++ packages/crypto-wallet-core/src/transactions/eth/index.ts
@@ -167,12 +167,12 @@ export class ETHTxProvider {
     return this.applySignature({ tx, signature });
   }
 
-  transformSignatureObject(params: { obj: any; }) {
+  transformSignatureObject(params: { obj: any }) {
     const { obj } = params;
     return ethers.Signature.from(obj).serialized;
   }
 
-  getSighash(params: { tx: string; }): string {
+  getSighash(params: { tx: string }): string {
     const { tx } = params;
     return ethers.keccak256(tx).slice(2); // remove 0x prefix
   }
