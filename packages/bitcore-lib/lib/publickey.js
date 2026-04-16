01e89fa29fa74aa53a7a33e296a25dbb7039fff2
Michael Jay <mjay@bitpay.com>
2026-04-16 15:57:12 -0400

    [LIB] rm lodash from publickey.js

--- packages/bitcore-lib/lib/publickey.js
+++ packages/bitcore-lib/lib/publickey.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const _ = require('lodash');
 const BN = require('./crypto/bn');
 const Hash = require('./crypto/hash');
 const Point = require('./crypto/point');
@@ -70,7 +69,8 @@ function PublicKey(data, extra) {
 PublicKey.prototype._classifyArgs = function(data, extra) {
   /* jshint maxcomplexity: 10 */
   let info = {
-    compressed: _.isUndefined(extra.compressed) || extra.compressed
+    // Default true only if extra.compressed is left off
+    compressed: extra.compressed === undefined || extra.compressed
   };
 
   // detect type of data
@@ -88,7 +88,8 @@ PublicKey.prototype._classifyArgs = function(data, extra) {
     throw new TypeError('First argument is an unrecognized data format.');
   }
   if (!info.network) {
-    info.network = _.isUndefined(extra.network) ? undefined : Network.get(extra.network);
+    // Maintain undefined specifically
+    info.network = extra.network === undefined ? undefined : Network.get(extra.network);
   }
   return info;
 };
@@ -146,7 +147,7 @@ PublicKey._transformDER = function(buf, strict) {
   $.checkArgument(PublicKey._isBuffer(buf), 'Must be a hex buffer of DER encoded public key');
   let info = {};
 
-  strict = _.isUndefined(strict) ? true : strict;
+  strict = strict === undefined ? true : strict;
 
   let x;
   let y;
