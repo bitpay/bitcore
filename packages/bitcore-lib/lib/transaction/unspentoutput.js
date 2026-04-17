0c76529984c5dadef8e9b6ab0e2229f34bbc9364
Michael Jay <mjay@bitpay.com>
2026-04-17 15:48:14 -0400

    [LIB] lint transaction/unspentoutput.js

--- packages/bitcore-lib/lib/transaction/unspentoutput.js
+++ packages/bitcore-lib/lib/transaction/unspentoutput.js
@@ -1,12 +1,11 @@
 'use strict';
 
-var _ = require('lodash');
-var $ = require('../util/preconditions');
-var JSUtil = require('../util/js');
-
-var Script = require('../script');
-var Address = require('../address');
-var Unit = require('../unit');
+const _ = require('lodash');
+const Address = require('../address');
+const Script = require('../script');
+const Unit = require('../unit');
+const JSUtil = require('../util/js');
+const $ = require('../util/preconditions');
 
 /**
  * Represents an unspent output information: its script, associated amount and address,
@@ -31,22 +30,22 @@ function UnspentOutput(data) {
     return new UnspentOutput(data);
   }
   $.checkArgument(_.isObject(data), 'Must provide an object from where to extract data');
-  var address = data.address ? new Address(data.address) : undefined;
-  var txId = data.txid ? data.txid : data.txId;
+  const address = data.address ? new Address(data.address) : undefined;
+  const txId = data.txid ? data.txid : data.txId;
   if (!txId || !JSUtil.isHexaString(txId) || txId.length > 64) {
     // TODO: Use the errors library
     throw new Error('Invalid TXID in object', data);
   }
-  var outputIndex = _.isUndefined(data.vout) ? data.outputIndex : data.vout;
+  const outputIndex = _.isUndefined(data.vout) ? data.outputIndex : data.vout;
   if (!_.isNumber(outputIndex)) {
     throw new Error('Invalid outputIndex, received ' + outputIndex);
   }
   $.checkArgument(!_.isUndefined(data.scriptPubKey) || !_.isUndefined(data.script),
-                  'Must provide the scriptPubKey for that output!');
-  var script = new Script(data.scriptPubKey || data.script);
+    'Must provide the scriptPubKey for that output!');
+  const script = new Script(data.scriptPubKey || data.script);
   $.checkArgument(!_.isUndefined(data.amount) || !_.isUndefined(data.satoshis),
-                      'Must provide an amount for the output');
-  var amount = !_.isUndefined(data.amount) ? new Unit.fromBTC(data.amount).toSatoshis() : data.satoshis;
+    'Must provide an amount for the output');
+  const amount = !_.isUndefined(data.amount) ? new Unit.fromBTC(data.amount).toSatoshis() : data.satoshis;
   $.checkArgument(_.isNumber(amount), 'Amount must be a number');
   JSUtil.defineImmutable(this, {
     address: address,
