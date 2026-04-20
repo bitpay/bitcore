b6a581e603640c1e26896fcd4f084cc8b1435beb
Michael Jay <mjay@bitpay.com>
2026-04-20 10:42:05 -0400

    [LIB] lint test/transaction/unspentoutput.js

--- packages/bitcore-lib/test/transaction/unspentoutput.js
+++ packages/bitcore-lib/test/transaction/unspentoutput.js
@@ -1,23 +1,24 @@
 'use strict';
 
-var _ = require('lodash');
-var chai = require('chai');
-var should = chai.should();
-var expect = chai.expect;
+const _ = require('lodash');
+const chai = require('chai');
 
-var bitcore = require('../..');
-var UnspentOutput = bitcore.Transaction.UnspentOutput;
+const expect = chai.expect;
 
-describe('UnspentOutput', function() {
+const bitcore = require('../..');
 
-  var sampleData1 = {
+const UnspentOutput = bitcore.Transaction.UnspentOutput;
+
+describe.only('UnspentOutput', function() {
+
+  const sampleData1 = {
     'address': 'mszYqVnqKoQx4jcTdJXxwKAissE3Jbrrc1',
     'txId': 'a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458',
     'outputIndex': 0,
     'script': 'OP_DUP OP_HASH160 20 0x88d9931ea73d60eaf7e5671efc0552b912911f2a OP_EQUALVERIFY OP_CHECKSIG',
     'satoshis': 1020000
   };
-  var sampleData2 = {
+  const sampleData2 = {
     'txid': 'e42447187db5a29d6db161661e4bc66d61c3e499690fe5ea47f87b79ca573986',
     'vout': 1,
     'address': 'mgBCJAsvzgT2qNNeXsoECg2uPKrUsZ76up',
@@ -40,7 +41,7 @@ describe('UnspentOutput', function() {
   });
 
   it('fails if vout is not a number', function() {
-    var sample = _.cloneDeep(sampleData2);
+    const sample = _.cloneDeep(sampleData2);
     sample.vout = '1';
     expect(function() {
       return new UnspentOutput(sample);
@@ -48,18 +49,18 @@ describe('UnspentOutput', function() {
   });
 
   it('displays nicely on the console', function() {
-    var expected = '<UnspentOutput: a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458:0' +
+    const expected = '<UnspentOutput: a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458:0' +
                    ', satoshis: 1020000, address: mszYqVnqKoQx4jcTdJXxwKAissE3Jbrrc1>';
     expect(new UnspentOutput(sampleData1).inspect()).to.equal(expected);
   });
 
   describe('checking the constructor parameters', function() {
-    var notDefined = {
+    const notDefined = {
       'txId': 'a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458',
       'outputIndex': 0,
       'script': 'OP_DUP OP_HASH160 20 0x88d9931ea73d60eaf7e5671efc0552b912911f2a OP_EQUALVERIFY OP_CHECKSIG',
     };
-    var zero = {
+    const zero = {
       'txId': 'a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458',
       'outputIndex': 0,
       'script': 'OP_DUP OP_HASH160 20 0x88d9931ea73d60eaf7e5671efc0552b912911f2a OP_EQUALVERIFY OP_CHECKSIG',
@@ -78,17 +79,17 @@ describe('UnspentOutput', function() {
   });
 
   it('toString returns txid:vout', function() {
-    var expected = 'a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458:0';
+    const expected = 'a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458:0';
     expect(new UnspentOutput(sampleData1).toString()).to.equal(expected);
   });
 
   it('to/from JSON roundtrip', function() {
-    var utxo = new UnspentOutput(sampleData2);
-    var obj = UnspentOutput.fromObject(utxo.toJSON()).toObject();
+    const utxo = new UnspentOutput(sampleData2);
+    const obj = UnspentOutput.fromObject(utxo.toJSON()).toObject();
     expect(obj).to.deep.equal(sampleData2);
-    var str = JSON.stringify(UnspentOutput.fromObject(obj));
+    const str = JSON.stringify(UnspentOutput.fromObject(obj));
     expect(JSON.parse(str)).to.deep.equal(sampleData2);
-    var str2 = JSON.stringify(new UnspentOutput(JSON.parse(str)));
+    const str2 = JSON.stringify(new UnspentOutput(JSON.parse(str)));
     expect(JSON.parse(str2)).to.deep.equal(sampleData2);
   });
 });
