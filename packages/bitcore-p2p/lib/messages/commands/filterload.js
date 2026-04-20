b4cca6ebee0262c2027c92b5f97be8b158878d3a
Michael Jay <mjay@bitpay.com>
2026-04-20 15:23:58 -0400

    rm lodash - messages/commands/filterload.js

--- packages/bitcore-p2p/lib/messages/commands/filterload.js
+++ packages/bitcore-p2p/lib/messages/commands/filterload.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const Message = require('../message');
 const inherits = require('util').inherits;
 const bitcore = require('@bitpay-labs/bitcore-lib');
 const BloomFilter = require('../../bloomfilter');
+const Message = require('../message');
 
 const BufferUtil = bitcore.util.buffer;
 const $ = bitcore.util.preconditions;
-const _ = bitcore.deps._;
 
 /**
  * Request peer to send inv messages based on a bloom filter
@@ -20,7 +19,7 @@ function FilterloadMessage(arg, options) {
   Message.call(this, options);
   this.command = 'filterload';
   $.checkArgument(
-    _.isUndefined(arg) || arg instanceof BloomFilter,
+    arg == null || arg instanceof BloomFilter,
     'An instance of BloomFilter or undefined is expected'
   );
   this.filter = arg;
@@ -32,7 +31,7 @@ FilterloadMessage.prototype.setPayload = function(payload) {
 };
 
 FilterloadMessage.prototype.getPayload = function() {
-  if(this.filter) {
+  if (this.filter) {
     return this.filter.toBuffer();
   } else {
     return BufferUtil.EMPTY_BUFFER;
