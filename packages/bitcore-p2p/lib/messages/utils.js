74a276f4fde4023a451d783e375d49df04ccc69c
Michael Jay <mjay@bitpay.com>
2026-04-20 13:50:59 -0400

    lint message/utils.js

--- packages/bitcore-p2p/lib/messages/utils.js
+++ packages/bitcore-p2p/lib/messages/utils.js
@@ -17,7 +17,7 @@ module.exports = utils = {
     );
   },
   checkFinished: function checkFinished(parser) {
-    if(!parser.finished()) {
+    if (!parser.finished()) {
       throw new Error('Data still available after parsing');
     }
   },
@@ -25,17 +25,17 @@ module.exports = utils = {
     return bitcore.crypto.Random.getRandomBuffer(8);
   },
   writeIP: function writeIP(ip, bw) {
-    var words = ip.v6.split(':').map(function(s) {
+    const words = ip.v6.split(':').map(function(s) {
       return Buffer.from(s, 'hex');
     });
-    for (var i = 0; i < words.length; i++) {
-      var word = words[i];
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
       bw.write(word);
     }
   },
   writeAddr: function writeAddr(addr, bw) {
     if (_.isUndefined(addr)) {
-      var pad = Buffer.from(Array(26));
+      const pad = Buffer.from(Array(26));
       bw.write(pad);
       return;
     }
@@ -52,10 +52,10 @@ module.exports = utils = {
     });
   },
   parseIP: function parseIP(parser) {
-    var ipv6 = [];
-    var ipv4 = [];
-    for (var a = 0; a < 8; a++) {
-      var word = parser.read(2);
+    let ipv6 = [];
+    let ipv4 = [];
+    for (let a = 0; a < 8; a++) {
+      const word = parser.read(2);
       ipv6.push(word.toString('hex'));
       if (a >= 6) {
         ipv4.push(word[0]);
@@ -70,9 +70,9 @@ module.exports = utils = {
     };
   },
   parseAddr: function parseAddr(parser) {
-    var services = parser.readUInt64LEBN();
-    var ip = utils.parseIP(parser);
-    var port = parser.readUInt16BE();
+    const services = parser.readUInt64LEBN();
+    const ip = utils.parseIP(parser);
+    const port = parser.readUInt16BE();
     return {
       services: services,
       ip: ip,
@@ -83,8 +83,8 @@ module.exports = utils = {
     /* jshint maxcomplexity: 10 */
     /* jshint maxstatements: 20 */
     $.checkArgument(_.isUndefined(obj.starts) || _.isArray(obj.starts));
-    var starts = obj.starts;
-    var stop = obj.stop;
+    let starts = obj.starts;
+    let stop = obj.stop;
     if (starts) {
       starts = starts.map(function(hash) {
         if (_.isString(hash)) {
@@ -97,7 +97,7 @@ module.exports = utils = {
       starts = [];
     }
 
-    for (var i = 0; i < starts.length; i++) {
+    for (let i = 0; i < starts.length; i++) {
       if (starts[i].length !== 32) {
         throw new Error('Invalid hash ' + i + ' length: ' + starts[i].length);
       }
