0cde8e4f1ad7f9352ea12d4471827779f3e9f05b
Michael Jay <mjay@bitpay.com>
2026-04-24 10:14:38 -0400

    fix bad test

--- packages/bitcore-p2p/integration/bitcoind.js
+++ packages/bitcore-p2p/integration/bitcoind.js
@@ -63,8 +63,8 @@ describe('Integration with ' + network.name + ' bitcoind', function() {
   it('connects', function(cb) {
     connect(function(peer) {
       peer.version.should.be.above(70000);
-      (typeof peer.subversion).should.be(true);
-      (typeof peer.bestHeight).should.be(true);
+      (typeof peer.subversion === 'string').should.be(true);
+      (typeof peer.bestHeight === 'number').should.be(true);
       cb();
     });
   });
