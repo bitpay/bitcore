d46a5d31efa354902e68733635ea3b5272b72dfa
Micah Maphet <micahmaphet@gmail.com>
2026-05-19 19:09:59 -0400

    error handling

--- bitcore-sh/src/index.ts
+++ bitcore-sh/src/index.ts
@@ -34,33 +34,37 @@ rl.on('line', async (line) => {
   }
   args = [...context, ...args];
 
-  const chain = args[0].toUpperCase();
-  const network = args[1];
-  const command = args[2];
-  args.slice(0, 3);
+  try {
+    const chain = args[0].toUpperCase();
+    const network = args[1];
+    const command = args[2];
+    args.slice(0, 3);
 
-  const rpcArgs = {};
-  for (let i = 0; i < args.length; i++) {
-    if (!args[i].startsWith('--'))
-      continue;
-    rpcArgs[args[i].slice(2)] = args[i + 1];
-    args.splice(i, 2);
-    i--;
+    const rpcArgs = {};
+    for (let i = 0; i < args.length; i++) {
+      if (!args[i].startsWith('--'))
+        continue;
+      rpcArgs[args[i].slice(2)] = args[i + 1];
+      args.splice(i, 2);
+      i--;
+    }
+
+    const networkConfig = config.chains[chain][network];
+    const rpcConfig = networkConfig.rpc || networkConfig.providers[0];
+
+    const rpc = new CryptoRpc({
+      chain,
+      protocol: rpcConfig.protocol || 'http',
+      host: rpcConfig.host,
+      port: rpcConfig.port,
+      user: rpcConfig.username,
+      pass: rpcConfig.password
+    }).get(chain);
+
+    console.log(await rpc[command](rpcArgs));
+  } catch (e) {
+    console.log(e);
   }
-
-  const networkConfig = config.chains[chain][network];
-  const rpcConfig = networkConfig.rpc || networkConfig.providers[0];
-
-  const rpc = new CryptoRpc({
-    chain,
-    protocol: rpcConfig.protocol || 'http',
-    host: rpcConfig.host,
-    port: rpcConfig.port,
-    user: rpcConfig.username,
-    pass: rpcConfig.password
-  }).get(chain);
-
-  console.log(await rpc[command](rpcArgs));
   end();
 });
 
