3529170ddccaa0c5c65e56dc80df48687452e268
Micah Maphet <micahmaphet@gmail.com>
2026-05-20 14:23:49 -0400

    refactored completer out of object instantiation

--- packages/bitcore-sh/src/index.ts
+++ packages/bitcore-sh/src/index.ts
@@ -2,40 +2,48 @@ import readline from 'readline';
 import config from './config';
 import { getRpc, getRpcMethodParams, rpcMethods } from './rpc';
 
+// all the commands prepended by the use command
 const context: string[] = [];
 
+// complete the chain, network, rpcCommand, and paramers
+const completer = (line: string) => {
+  let args = [...context, ...line.split(' ')];
+  if (args.includes('use'))
+    args = args.filter(arg => arg !== 'use');
+
+  const completions: string[] = line.includes(' ') ? [] : ['use'];
+  let hits: string[] = [];
+
+  if (args.length <= 1) {
+    completions.push(...Object.keys(config));
+    hits = completions.filter(c => c.toLowerCase().startsWith(args[0].toLowerCase()));
+  } else if (args.length === 2) {
+    if (Object.keys(config).includes(args[0].toUpperCase())) {
+      completions.push(...Object.keys(config[args[0].toUpperCase()]));
+      hits = completions.filter(c => c.startsWith(args[1]));
+    }
+  } else if (args.length === 3) {
+    const rpc = getRpc(args[0].toUpperCase(), args[1]);
+    if (rpc) {
+      completions.push(...rpcMethods);
+      hits = completions.filter(c => c.startsWith(args[2]));
+    }
+  } else if (args.length === 4) {
+    completions.push(...getRpcMethodParams(args[0], args[1], args[2]));
+    hits = completions.filter(c => c.startsWith(args[3]));
+  }
+  return [hits.length ? hits : completions, args[args.length - 1]];
+}
+
+// repl environment
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  completer: (line: string) => {
-    let args = [...context, ...line.split(' ')];
-    if (args.includes('use'))
-      args = args.filter(arg => arg !== 'use');
-    const completions: string[] = line.includes(' ') ? [] : ['use'];
-    let hits: string[] = [];
-    if (args.length <= 1) {
-      completions.push(...Object.keys(config));
-      hits = completions.filter(c => c.toLowerCase().startsWith(args[0].toLowerCase()));
-    } else if (args.length === 2) {
-      if (Object.keys(config).includes(args[0].toUpperCase())) {
-        completions.push(...Object.keys(config[args[0].toUpperCase()]));
-        hits = completions.filter(c => c.startsWith(args[1]));
-      }
-    } else if (args.length === 3) {
-      const rpc = getRpc(args[0].toUpperCase(), args[1]);
-      if (rpc) {
-        completions.push(...rpcMethods);
-        hits = completions.filter(c => c.startsWith(args[2]));
-      }
-    } else if (args.length === 4) {
-      completions.push(...getRpcMethodParams(args[0], args[1], args[2]));
-      hits = completions.filter(c => c.startsWith(args[3]));
-    }
-    return [hits.length ? hits : completions, args[args.length - 1]];
-  }
+  completer
 });
 nextCommand();
 
+// handle all user commands
 rl.on('line', async (line) => {
   let args = line.split(' ');
   if (args[0] === 'use') {
