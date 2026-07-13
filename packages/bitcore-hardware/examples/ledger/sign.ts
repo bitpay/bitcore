b6632e32296421d313da8fbcbdd335e104ab7ca5
Micah Maphet <micahmaphet@gmail.com>
2026-07-13 11:59:05 -0400

    signing demo for ledger sign method

--- packages/bitcore-hardware/examples/ledger/sign.ts
+++ packages/bitcore-hardware/examples/ledger/sign.ts
@@ -2,6 +2,6 @@ import Ledger from '../../src/ledger.js';
 
 const ledger = new Ledger();
 await ledger.connect();
-console.log(await ledger.getMasterKeyFingerprint());
+console.log(await ledger.sign());
 await ledger.disconnect();
 process.exit(0);
--- packages/bitcore-hardware/src/ledger.ts
+++ packages/bitcore-hardware/src/ledger.ts
@@ -1,8 +1,11 @@
 import { createRequire } from 'module';
+import BIP32Factory from 'bip32';
+import { Psbt, payments } from 'bitcoinjs-lib';
 import {
   Observable,
   lastValueFrom
 } from 'rxjs';
+import * as ecc from 'tiny-secp256k1';
 import { Base } from './base.js';
 import { dmk } from './dmk.js';
 import { BaseParams } from './types/paramTypes.js';
@@ -12,7 +15,12 @@ const {
   CloseAppCommand,
   GetOsVersionCommand
 } = require('@ledgerhq/device-management-kit');
-const { SignerBtcBuilder } = require('@ledgerhq/device-signer-kit-bitcoin');
+const {
+  DefaultWallet,
+  SignerBtcBuilder
+} = require('@ledgerhq/device-signer-kit-bitcoin');
+
+const bip32 = BIP32Factory(ecc);
 
 export default class Ledger implements Base {
   device: any;
@@ -90,6 +98,45 @@ export default class Ledger implements Base {
   }
 
   async sign() {
-    return 'not implemented';
+    const psbt = new Psbt();
+
+    // Derive the specific child key for external chain (0), address index (0)
+    const childNode = bip32.fromBase58(await this.getPublicKey({ index: 0 })).derive(0).derive(0);
+    const pubkey = Buffer.from(childNode.publicKey);
+
+    // Compute the P2WPKH scriptPubKey from the derived child key
+    const p2wpkh = payments.p2wpkh({ pubkey: pubkey });
+    const script = p2wpkh.output!;
+
+    const masterFingerprint = await this.getMasterKeyFingerprint();
+    psbt.addInput({
+      hash: '78519a191327dfdc0c2ea64a04d09d87c3909ce8365d0e0c0dbd0bc80d0405b4',
+      index: 0,
+      witnessUtxo: {
+        script,
+        value: 1000,
+      },
+      bip32Derivation: [{
+        masterFingerprint: Buffer.from(masterFingerprint.buffer, masterFingerprint.byteOffset, masterFingerprint.byteLength),
+        pubkey,
+        path: "m/84'/0'/0'/0/0",
+      }],
+    });
+    psbt.addOutput({ address: 'bc1qj86hpgprdudkks84y52vdenz86kd26stkssrcq', value: 1000 });
+
+    const ob: Observable<any> = this.signer.signTransaction(
+      new DefaultWallet("84'/0'/0'", 'wpkh(@0/**)'),
+      psbt
+    ).observable;
+
+    const result = await lastValueFrom(ob);
+    return result.output;
+  }
+
+  async getMasterKeyFingerprint(): Promise<Uint8Array> {
+    const ob: Observable<any> = this.signer.getMasterFingerprint().observable;
+    const result = await lastValueFrom(ob);
+
+    return result.output.masterFingerprint;
   }
 }
