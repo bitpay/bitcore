fe720719b0ee29f0931a97e49a2501ed2d0d106a
Micah Maphet <micahmaphet@gmail.com>
2026-08-12 23:06:33 -0400

    created a signing test for burner

--- packages/bitcore-hardware/src/burner.ts
+++ packages/bitcore-hardware/src/burner.ts
@@ -108,7 +108,7 @@ export default class Burner implements Base {
 
     const response: any = await this.awaitResponse();
 
-    const publicKey = new PublicKey('04161f62f9778a44bd3d07009b1f2e9df7ab1dc57e74665db7ed8baa95780452ab39f4975099f3ae36f7b6a874b46edd79d1d88573a325b9976fd8f120bed704aa');
+    const publicKey = new PublicKey(response.publicKey);
     const signature = crypto.Signature.fromString(response.signature.der);
     signature.pubKey = publicKey;
 
---
+++ packages/bitcore-hardware/test/burner/sign.test.ts
@@ -0,0 +1,69 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import CWC, { BitcoreLib } from '@bitpay-labs/crypto-wallet-core';
+import Burner from '../../src/burner.js';
+
+const { PrivateKey, crypto, Transaction, Script } = BitcoreLib;
+
+describe('Burner Signing', function () {
+  const sandbox = sinon.createSandbox();
+  const burner = new Burner('BTC');
+  const privateKey = new PrivateKey();
+  const publicKey = privateKey.toPublicKey();
+
+  before(function () {
+    this.timeout(5_000);
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it('should sign transactions', async function () {
+    const address = publicKey.toAddress();
+    
+    const utxos = [{
+      txId: '115e8f72f39fad874cfab0deed11a80f24f967a84079fb56ddf53ea02e308986',
+      outputIndex: 0,
+      address: address.toString(),
+      script: Script.buildWitnessV0Out(address),
+      satoshis: 1100
+    }];
+    
+    const tx = CWC.Transactions.create({
+      chain: 'BTC',
+      recipients: [{ address: '1Gokm82v6DmtwKEB8AiVhm82hyFSsEvBDK', amount: 1000 }],
+      utxos
+    });
+
+    const signRequest: Promise<string> = burner.sign({
+      tx,
+      utxos,
+      password: 'password not relevant to test',
+      index: 0
+    });
+
+    const hash = CWC.Transactions.getSighash({
+      chain: 'BTC',
+      tx,
+      utxos,
+      index: 0
+    });
+    
+    const signature = crypto.ECDSA.sign(Buffer.from(hash, 'hex'), privateKey);
+    
+    burner.response = {
+      input: (burner.command as any).keyNo,
+      digest: (burner.command as any).digest,
+      signature: {
+        raw: {},
+        der: signature.toString(),
+      },
+      publicKey: publicKey.toString()
+    };
+    
+    const signedTransaction = new Transaction(await signRequest);
+    expect(signedTransaction.inputs[0].witnesses[0].length).to.be.greaterThan(70).and.lessThan(73);
+    expect(signedTransaction.inputs[0].witnesses[1].length).to.equal(33);
+  });
+});
