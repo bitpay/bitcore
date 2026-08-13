eb9c14221ee1da3da0dbf4982bb3eb16d8cbb936
Micah Maphet <micahmaphet@gmail.com>
2026-08-13 00:52:42 -0400

    added signature verification test and refactored test suite for bitcore-hardware

--- packages/bitcore-hardware/test/burner/sign.test.ts
+++ packages/bitcore-hardware/test/burner/sign.test.ts
@@ -1,5 +1,6 @@
 import sinon from 'sinon';
 import { expect } from 'chai';
+import { describe } from 'mocha';
 import CWC, { BitcoreLib } from '@bitpay-labs/crypto-wallet-core';
 import Burner from '../../src/burner.js';
 
@@ -15,6 +16,11 @@ describe('Burner Signing', function () {
     this.timeout(5_000);
   });
 
+  this.beforeEach(function () {
+    burner.responses = [];
+    burner.commandQueue = [];
+  });
+  
   afterEach(function () {
     sandbox.restore();
   });
@@ -65,5 +71,58 @@ describe('Burner Signing', function () {
     const signedTransaction = new Transaction(await signRequest);
     expect(signedTransaction.inputs[0].witnesses[0].length).to.be.greaterThan(70).and.lessThan(73);
     expect(signedTransaction.inputs[0].witnesses[1].length).to.equal(33);
+  });
+
+  it('should throw error if signature does not match public key', async function () {
+    const address = publicKey.toAddress();
+    
+    const utxos = [{
+      txId: '115e8f72f39fad874cfab0deed11a80f24f967a84079fb56ddf53ea02e308986',
+      outputIndex: 0,
+      address: address.toString(),
+      script: Script.buildWitnessV0Out(address),
+      satoshis: 1100
+    }];
+    
+    const tx = CWC.Transactions.create({
+      chain: 'BTC',
+      recipients: [{ address: '1Gokm82v6DmtwKEB8AiVhm82hyFSsEvBDK', amount: 1000 }],
+      utxos
+    });
+
+    const hash = CWC.Transactions.getSighash({
+      chain: 'BTC',
+      tx,
+      utxos,
+      index: 0
+    });
+    
+    const signature = crypto.ECDSA.sign(Buffer.from(hash, 'hex'), privateKey);
+
+    const signRequest: Promise<string> = burner.sign({
+      tx,
+      utxos,
+      password: 'password not relevant to test',
+      index: 0
+    });
+    
+    burner.responses = [{
+      input: (burner.commandQueue[0] as any).keyNo,
+      digest: (burner.commandQueue[0] as any).digest,
+      signature: {
+        raw: {},
+        der: signature.toString(),
+      },
+      // random, incorrect public key
+      publicKey: new PrivateKey().toPublicKey().toString()
+    }];
+
+    let error = false;
+    try {
+      await signRequest;
+    } catch {
+      error = true;
+    }
+    expect(error, 'did not throw error with incorrect public key').to.be.true;
   });
 });
--- packages/bitcore-hardware/test/test.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-
-describe('Placeholder Test', function() {
-  it('should have a test that runs', function() {
-    expect(true).to.be.true;
-  });
-});
