cc3284bbd1d6ef550176c467717a8fd23402d9fc
Micah Maphet <micahmaphet@gmail.com>
2026-09-12 14:52:56 -0400

    add ledger button pressing in tests

--- packages/bitcore-hardware/test/ledger/test.ts
+++ packages/bitcore-hardware/test/ledger/test.ts
@@ -1,10 +1,20 @@
 import 'source-map-support/register.js';
+import { createRequire } from 'module';
 import Ledger from '../../src/ledger/wallet.js';
+
+const require = createRequire(import.meta.url);
+const { deviceControllerClientFactory } = require('@ledgerhq/speculos-device-controller');
+
+const deviceClient = deviceControllerClientFactory('http://localhost:5000');
+
+const deviceButtons = deviceClient.buttonFactory();
+
+await deviceButtons.right();
 
 const ledger = new Ledger();
 await ledger.connect();
 
-console.log('Awaiting approval from your ledger...');
-console.log(await ledger.getVersion());
+console.log('Currently actions crash speculos...');
+// console.log(await ledger.getVersion());
 await ledger.disconnect();
 process.exit(0);
